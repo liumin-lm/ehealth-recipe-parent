d6cb2f167632c31142d2ad3bb282e8da1b218c40
yuyun <yuy@ngarihealth.com>
2018-07-03 10:40:53 +0800

    医院处方修改

--- ehealth-recipe-prescription/src/main/java/recipe/prescription/HisprescriptionService.java
+++ ehealth-recipe-prescription/src/main/java/recipe/prescription/HisprescriptionService.java
@@ -9,6 +9,7 @@ import ctd.persistence.DAOFactory;
 import ctd.persistence.exception.DAOException;
 import ctd.util.JSONUtils;
 import ctd.util.annotation.RpcBean;
+import ctd.util.annotation.RpcService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import recipe.dao.HisprescriptionDAO;
@@ -16,27 +17,33 @@ import recipe.dao.HisprescriptionDAO;
 /**
  * @author： 0184/yu_yun
  * @date： 2018/6/28
- * @description： TODO
+ * @description： 医院处方
  * @version： 1.0
  */
 
 @RpcBean("remoteHisprescriptionService")
 public class HisprescriptionService extends BaseService<HisprescriptionTO> implements IHisprescriptionService {
 
-    /** logger */
+    /**
+     * logger
+     */
     private static final Logger LOGGER = LoggerFactory.getLogger(HisprescriptionService.class);
 
+    @RpcService
     @Override
     public HisprescriptionTO get(Object id) {
-        return null;
+        HisprescriptionDAO hisprescriptionDAO = DAOFactory.getDAO(HisprescriptionDAO.class);
+        Hisprescription recipe = hisprescriptionDAO.get(id);
+        return getBean(recipe, HisprescriptionTO.class);
     }
 
+    @RpcService
     @Override
     public RecipeCommonResTO createPrescription(HisprescriptionTO hisprescription) {
         HisprescriptionDAO hisprescriptionDAO = DAOFactory.getDAO(HisprescriptionDAO.class);
         RecipeCommonResTO response = new RecipeCommonResTO();
         response.setCode(RecipeCommonResTO.FAIL);
-        if(null != hisprescription) {
+        if (null != hisprescription) {
             try {
                 hisprescriptionDAO.save(getBean(hisprescription, Hisprescription.class));
                 response.setCode(RecipeCommonResTO.SUCCESS);
@@ -44,7 +51,7 @@ public class HisprescriptionService extends BaseService<HisprescriptionTO> imple
                 response.setMsg("存储失败");
                 LOGGER.warn("createPrescription error. hisprescription={}", JSONUtils.toString(hisprescription), e);
             }
-        }else{
+        } else {
             response.setMsg("对象为空");
         }
         return response;
