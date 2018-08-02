09c1ee485209823a7c7a19fed0a39dd2e629857e
yuyun <yuy@ngarihealth.com>
2018-08-02 09:42:34 +0800

    修复因为无法准确获取支付完成后挂号记录生成的时间点，前端定时查询会获取不到挂号记录的问题

--- ehealth-recipe-core/src/main/java/recipe/serviceprovider/his/service/RemoteRecipeToHisService.java
+++ ehealth-recipe-core/src/main/java/recipe/serviceprovider/his/service/RemoteRecipeToHisService.java
@@ -153,7 +153,9 @@ public class RemoteRecipeToHisService implements IRecipeToHisService {
             }
             response.setMsg(hosrelationBean.getMemo());
         }else{
-            response.setMsg("由于系统原因，请稍后再试，咨询已自动取消");
+            //-1 会让前端重复查询
+            response.setCode(-1);
+//            response.setMsg("由于系统原因，请稍后再试，咨询已自动取消");
         }
         LOGGER.info("visitRegistSuccess consultId={}, response={}", consultId, JSONUtils.toString(response));
         return response;
