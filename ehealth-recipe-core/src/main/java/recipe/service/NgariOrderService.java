f3fa0ade200796d6b76406bb58eb8d8591de1a2d
yuanb <yuanb@ngarihealth.com>
2018-09-19 15:37:31 +0800

    扫码下单接口

---
+++ ehealth-recipe-core/src/main/java/recipe/service/NgariOrderService.java
@@ -0,0 +1,63 @@
+package recipe.service;
+
+import com.ngari.recipe.entity.RecipeOrder;
+import com.ngari.wxpay.service.INgariPayService;
+import com.ngari.wxpay.service.IUnifiedPayService;
+import ctd.persistence.DAOFactory;
+import ctd.persistence.exception.DAOException;
+import ctd.util.annotation.RpcBean;
+import ctd.util.annotation.RpcService;
+import eh.base.constant.ErrorCode;
+import eh.entity.bus.pay.BusTypeEnum;
+import org.slf4j.LoggerFactory;
+import recipe.ApplicationUtils;
+import recipe.dao.RecipeOrderDAO;
+
+import java.util.Map;
+
+/**
+ * 扫码下单相关接口
+ * Created by yuanb on 2018/9/19.
+ * @author yuanb
+ */
+@RpcBean("ngariOrderService")
+public class NgariOrderService {
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(NgariOrderService.class);
+
+    /**
+     *
+     * @param payway 支付宝扫码支付  32
+     * @param orderId
+     * @return
+     */
+    @RpcService
+    public String ScanCodePayment(String payway ,Integer orderId , String appId){
+        //检查业务单状态，如果已经支付成功，直接返回
+        RecipeOrderDAO recipeOrderDAO = DAOFactory.getDAO(RecipeOrderDAO.class);
+        RecipeOrder recipeOrder = recipeOrderDAO.getOrderByRecipeId(orderId);
+        Integer payFlag = recipeOrder.getPayFlag();
+        String outTradeNo = recipeOrder.getOutTradeNo();
+        if(payFlag .equals(1)){
+            throw new DAOException(ErrorCode.SERVICE_ERROR,"订单已支付，请勿重复下单");
+        }
+        //判断业务单的商户订单号 ，是否已经下过的呢。如果已经下过单，需要先取消上一个订单
+        if(outTradeNo == null || outTradeNo.trim().equals("")){
+            IUnifiedPayService unifiedPayService = ApplicationUtils.getBaseService(IUnifiedPayService.class);
+            Map<String, Object> cancelmap = unifiedPayService.orderCancel(orderId, BusTypeEnum.RECIPE.getCode());
+            String result = (String) cancelmap.get("code");
+            String action = (String) cancelmap.get("action");
+            logger.info("result:{}, action:{}", result, action);
+        }
+
+        // 获取支付的二维码
+        INgariPayService payService = ApplicationUtils.getBaseService(INgariPayService.class);
+        Map<String, Object> map = payService.appOrder(appId, payway, BusTypeEnum.MEETCLINIC.getCode(), String.valueOf(orderId), "");
+        if (map == null || map.get("qr_code") == null) {
+            logger.info("payService qr_code is required,or map is null...orderId=" + orderId + ",payway=" + payway + ",appId=" + appId);
+            throw new DAOException(ErrorCode.SERVICE_ERROR, "生成二维码失败，请重试");
+        }
+        String url = (String) map.get("qr_code");
+        logger.info("处方订单生成二维码成功：[{}]",url);
+        return url;
+    }
+}
