06b6fc06654b17720043c2e2c2b0a6b8a97a544d
yuyun <yuy@ngarihealth.com>
2018-11-02 14:08:58 +0800

    格式问题

--- ehealth-recipe-common/src/main/java/recipe/constant/ErrorCode.java
+++ ehealth-recipe-common/src/main/java/recipe/constant/ErrorCode.java
@@ -1,59 +1,59 @@
 package recipe.constant;
+
 /**
  * 错误代码定义
- * @author wnw
  *
+ * @author wnw
  */
-public class ErrorCode
-{
+public class ErrorCode {
 
-	/**
-	 * 服务异常，提示消息
-	 */
-	public static final int SERVICE_ERROR=609;
+    /**
+     * 服务异常，提示消息
+     */
+    public static final int SERVICE_ERROR = 609;
 
-	/**
-	 * 提示消息，单次确认
-	 */
-	public static final int SERVICE_CONFIRM=908;
+    /**
+     * 提示消息，单次确认
+     */
+    public static final int SERVICE_CONFIRM = 908;
 
-	/**
-	 * 提示消息，二次确认
-	 */
-	public static final int SERVICE_DOUBLE_CONFIRM=909;
+    /**
+     * 提示消息，二次确认
+     */
+    public static final int SERVICE_DOUBLE_CONFIRM = 909;
 
     /**
      * 有待支付的咨询单，不能发起新咨询
      */
     public static final int CONSULT_PENDING = 611;
 
-	/**
-	 * 优惠券不能使用时，弹框一秒显示“亲，该优惠券已过期”
-	 */
-	public static final int COUPON_EXPIRED = 612;
+    /**
+     * 优惠券不能使用时，弹框一秒显示“亲，该优惠券已过期”
+     */
+    public static final int COUPON_EXPIRED = 612;
 
-	/**
-	 * 当前患者和医生已存在进行中业务
-	 */
-	public static final int REQUEST_MODE_EXISTS = 613;
+    /**
+     * 当前患者和医生已存在进行中业务
+     */
+    public static final int REQUEST_MODE_EXISTS = 613;
 
-	/**
-	 * 患者注册时身份证号有问题
-	 */
-	public static final int PATIENT_IDCARD_ERROR = 615;
+    /**
+     * 患者注册时身份证号有问题
+     */
+    public static final int PATIENT_IDCARD_ERROR = 615;
 
-	/**
-	 * 患者注册时手机号有问题
-	 */
-	public static final int PATIENT_MOBILE_ERROR = 616;
+    /**
+     * 患者注册时手机号有问题
+     */
+    public static final int PATIENT_MOBILE_ERROR = 616;
 
-	/**
-	 * 重复发起支付
-	 */
-	public static final int REPEAT_ORDER = 617;
+    /**
+     * 重复发起支付
+     */
+    public static final int REPEAT_ORDER = 617;
 
-	/**
-	 * 合理用药建议
-	 */
-	public static final int RATIONAL_DRUG_USE = 618;
+    /**
+     * 合理用药建议
+     */
+    public static final int RATIONAL_DRUG_USE = 618;
 }
