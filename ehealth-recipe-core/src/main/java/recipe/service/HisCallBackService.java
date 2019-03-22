18d56dd567263895d1e4f70e48b342cd71149ac1
yuyun <yuy@ngarihealth.com>
2019-03-22 11:20:50 +0800

    task#11924 【后台】根据机构参数是否支持补充库存走相应流程

--- ehealth-recipe-core/src/main/java/recipe/service/HisCallBackService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/HisCallBackService.java
@@ -5,6 +5,7 @@ import com.ngari.home.asyn.model.BussCreateEvent;
 import com.ngari.home.asyn.service.IAsynDoBussService;
 import com.ngari.patient.utils.ObjectCopyUtils;
 import com.ngari.recipe.common.RecipeResultBean;
+import com.ngari.recipe.entity.DrugsEnterprise;
 import com.ngari.recipe.entity.Recipe;
 import com.ngari.recipe.entity.RecipeOrder;
 import com.ngari.recipe.entity.Recipedetail;
@@ -21,9 +22,11 @@ import recipe.ApplicationUtils;
 import recipe.bean.RecipeCheckPassResult;
 import recipe.bussutil.RecipeUtil;
 import recipe.constant.*;
+import recipe.dao.DrugsEnterpriseDAO;
 import recipe.dao.RecipeDAO;
 import recipe.dao.RecipeDetailDAO;
 import recipe.dao.RecipeOrderDAO;
+import recipe.drugsenterprise.RemoteDrugEnterpriseService;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -166,6 +169,17 @@ public class HisCallBackService {
                 throw new DAOException(ErrorCode.SERVICE_ERROR, "更改取药方式失败，错误:" + result1.getError());
             }
         }
+
+        //武昌需求处理，推送无库存的处方至医院补充库存药企
+        DrugsEnterpriseDAO enterpriseDAO = DAOFactory.getDAO(DrugsEnterpriseDAO.class);
+        List<DrugsEnterprise> enterpriseList = enterpriseDAO.findByOrganIdAndHosInteriorSupport(recipe.getClinicOrgan());
+        if(CollectionUtils.isNotEmpty(enterpriseList)){
+            RemoteDrugEnterpriseService service = ApplicationUtils.getRecipeService(RemoteDrugEnterpriseService.class);
+            for (DrugsEnterprise dep : enterpriseList) {
+                service.pushSingleRecipeInfoWithDepId(recipe.getRecipeId(), dep.getId());
+            }
+        }
+
     }
 
     /**
