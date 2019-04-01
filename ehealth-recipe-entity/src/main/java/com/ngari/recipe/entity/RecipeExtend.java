353cb03e7d3119565526e2f25f903a2edf57175e
yuyun <yuy@ngarihealth.com>
2019-04-01 09:58:08 +0800

    task#12774 【后台】新增电子病历字段处理

---
+++ ehealth-recipe-entity/src/main/java/com/ngari/recipe/entity/RecipeExtend.java
@@ -0,0 +1,95 @@
+package com.ngari.recipe.entity;
+
+import ctd.schema.annotation.ItemProperty;
+import ctd.schema.annotation.Schema;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * cdr_recipe扩展表
+ */
+@Entity
+@Schema
+@Table(name = "cdr_recipe_ext")
+@Access(AccessType.PROPERTY)
+public class RecipeExtend implements Serializable {
+
+
+    @ItemProperty(alias = "处方ID")
+    private Integer recipeId;
+
+    @ItemProperty(alias = "主诉")
+    private String mainDieaseDescribe;
+
+    @ItemProperty(alias = "现病史")
+    private String currentMedical;
+
+    @ItemProperty(alias = "既往史")
+    private String histroyMedical;
+
+    @ItemProperty(alias = "过敏史")
+    private String allergyMedical;
+
+    @ItemProperty(alias = "发病日期")
+    private Date onsetDate;
+
+    public RecipeExtend() {
+    }
+
+    @Id
+    @Column(name = "recipeId", unique = true, nullable = false)
+    public Integer getRecipeId() {
+        return recipeId;
+    }
+
+    public void setRecipeId(Integer recipeId) {
+        this.recipeId = recipeId;
+    }
+
+    @Column(name = "mainDieaseDescribe")
+    public String getMainDieaseDescribe() {
+        return mainDieaseDescribe;
+    }
+
+    public void setMainDieaseDescribe(String mainDieaseDescribe) {
+        this.mainDieaseDescribe = mainDieaseDescribe;
+    }
+
+    @Column(name = "currentMedical")
+    public String getCurrentMedical() {
+        return currentMedical;
+    }
+
+    public void setCurrentMedical(String currentMedical) {
+        this.currentMedical = currentMedical;
+    }
+
+    @Column(name = "histroyMedical")
+    public String getHistroyMedical() {
+        return histroyMedical;
+    }
+
+    public void setHistroyMedical(String histroyMedical) {
+        this.histroyMedical = histroyMedical;
+    }
+
+    @Column(name = "allergyMedical")
+    public String getAllergyMedical() {
+        return allergyMedical;
+    }
+
+    public void setAllergyMedical(String allergyMedical) {
+        this.allergyMedical = allergyMedical;
+    }
+
+    @Column(name = "onsetDate")
+    public Date getOnsetDate() {
+        return onsetDate;
+    }
+
+    public void setOnsetDate(Date onsetDate) {
+        this.onsetDate = onsetDate;
+    }
+}
