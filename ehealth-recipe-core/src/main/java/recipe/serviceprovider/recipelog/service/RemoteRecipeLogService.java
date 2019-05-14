7a1b17fb6b34dfccb947c40df81c83439d3dbffe
zhangxq <zhangxq@ngarihealth.com>
2019-05-14 20:47:24 +0800

    TASK#14757 opbase合并

--- ehealth-recipe-core/src/main/java/recipe/serviceprovider/recipelog/service/RemoteRecipeLogService.java
+++ ehealth-recipe-core/src/main/java/recipe/serviceprovider/recipelog/service/RemoteRecipeLogService.java
@@ -9,6 +9,8 @@ import recipe.ApplicationUtils;
 import recipe.service.RecipeLogService;
 import recipe.serviceprovider.BaseService;
 
+import java.util.List;
+
 /**
  * company: ngarihealth
  * @author: 0184/yu_yun
@@ -30,5 +32,12 @@ public class RemoteRecipeLogService extends BaseService<RecipeLogBean> implement
     public void saveRecipeLog(int recipeId, int beforeStatus, int afterStatus, String memo) {
         RecipeLogService service = ApplicationUtils.getRecipeService(RecipeLogService.class);
         service.saveRecipeLog(recipeId, beforeStatus, afterStatus, memo);
+    }
+
+    @Override
+    @RpcService
+    public List<RecipeLogBean> findByRecipeId(Integer recipeId){
+        RecipeLogService service = ApplicationUtils.getRecipeService(RecipeLogService.class);
+        return service.findByRecipeId(recipeId);
     }
 }
