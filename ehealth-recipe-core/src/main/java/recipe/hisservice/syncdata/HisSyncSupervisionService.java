c095b83ade09a82a9732cee645fbdb8dbb50c08d
shiyuping <shiyp@ngarihealth.com>
2019-06-03 17:04:51 +0800

    修改版本号

---
+++ ehealth-recipe-core/src/main/java/recipe/hisservice/syncdata/HisSyncSupervisionService.java
@@ -0,0 +1,16 @@
+package recipe.hisservice.syncdata;
+
+import com.ngari.recipe.entity.Recipe;
+import recipe.common.response.CommonResponse;
+
+import java.util.List;
+
+/**
+ * created by shiyuping on 2019/6/3
+ */
+public class HisSyncSupervisionService implements ICommonSyncSupervisionService {
+    @Override
+    public CommonResponse uploadRecipeIndicators(List<Recipe> recipeList) {
+        return null;
+    }
+}
