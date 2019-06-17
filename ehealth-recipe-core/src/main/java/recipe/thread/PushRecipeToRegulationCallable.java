cb7458361a67c7ac75e3490825e0518e097debc9
shiyuping <shiyp@ngarihealth.com>
2019-06-17 14:08:59 +0800

    task#17676 江苏省监管平台处方数据处理

--- ehealth-recipe-core/src/main/java/recipe/thread/PushRecipeToRegulationCallable.java
+++ ehealth-recipe-core/src/main/java/recipe/thread/PushRecipeToRegulationCallable.java
@@ -1,8 +1,11 @@
 package recipe.thread;
 
 import com.google.common.collect.ImmutableMap;
+import com.ngari.base.serviceconfig.mode.ServiceConfigResponseTO;
+import com.ngari.base.serviceconfig.service.IHisServiceConfigService;
 import com.ngari.recipe.entity.Recipe;
 import ctd.persistence.DAOFactory;
+import ctd.spring.AppDomainContext;
 import ctd.util.JSONUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +18,7 @@ import recipe.hisservice.syncdata.HisSyncSupervisionService;
 import recipe.service.RecipeLogService;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.Callable;
 
 /**
@@ -23,6 +27,7 @@ import java.util.concurrent.Callable;
 public class PushRecipeToRegulationCallable implements Callable<String> {
 
     private Logger logger = LoggerFactory.getLogger(PushRecipeToRegulationCallable.class);
+    private static final String REGULATION_JS = "jssjgpt";
 
     private Integer recipeId;
 
@@ -39,16 +44,24 @@ public class PushRecipeToRegulationCallable implements Callable<String> {
         Recipe recipe = recipeDAO.getByRecipeId(recipeId);
         HisSyncSupervisionService service = ApplicationUtils.getRecipeService(HisSyncSupervisionService.class);
         CommonResponse response = null;
+        //获取所有监管平台机构列表
+        IHisServiceConfigService configService = AppDomainContext.getBean("his.hisServiceConfig", IHisServiceConfigService.class);
+        List<ServiceConfigResponseTO> list = configService.findAllRegulationOrgan();
         try {
-            if (RecipeStatusConstant.CHECK_PASS_YS == recipe.getStatus()){
-                response = service.uploadRecipeAuditIndicators(Arrays.asList(recipe));
-                if (CommonConstant.SUCCESS.equals(response.getCode())){
-                    response = service.uploadRecipeCirculationIndicators(Arrays.asList(recipe));
-                } else{
-                    logger.warn("uploadRecipeAuditIndicators rpc execute error. recipe={}", JSONUtils.toString(recipe));
+            for (ServiceConfigResponseTO serviceConfigResponseTO : list){
+                if (REGULATION_JS.equals(serviceConfigResponseTO.getRegulationAppDomainId())
+                        && (serviceConfigResponseTO.getOrganid().equals(recipe.getClinicOrgan()))){
+                    if (RecipeStatusConstant.CHECK_PASS_YS == recipe.getStatus()){
+                        response = service.uploadRecipeAuditIndicators(Arrays.asList(recipe));
+                        if (CommonConstant.SUCCESS.equals(response.getCode())){
+                            response = service.uploadRecipeCirculationIndicators(Arrays.asList(recipe));
+                        } else{
+                            logger.warn("uploadRecipeAuditIndicators rpc execute error. recipe={}", JSONUtils.toString(recipe));
+                        }
+                    }else {
+                        response = service.uploadRecipeIndicators(Arrays.asList(recipe));
+                    }
                 }
-            }else {
-                response = service.uploadRecipeIndicators(Arrays.asList(recipe));
             }
         } catch (Exception e) {
             logger.warn("uploadRecipeIndicators exception recipe={}", JSONUtils.toString(recipe), e);
