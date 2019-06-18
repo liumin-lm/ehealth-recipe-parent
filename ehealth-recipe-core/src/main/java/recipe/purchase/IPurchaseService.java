1056b46b15be41fc410e668609ebd87a934137ee
yuyun <yuy@ngarihealth.com>
2019-06-18 14:35:40 +0800

    taks#18277 购药方式流程改造，增加顶层接口

--- ehealth-recipe-core/src/main/java/recipe/purchase/IPurchaseService.java
+++ ehealth-recipe-core/src/main/java/recipe/purchase/IPurchaseService.java
@@ -27,4 +27,6 @@ public interface IPurchaseService {
 
     
     Integer getPayMode();
+
+    String getServiceName();
 }
---
+++ ehealth-recipe-core/src/main/java/recipe/purchase/PurchaseConfigure.java
@@ -0,0 +1,25 @@
+package recipe.purchase;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author： 0184/yu_yun
+ * @date： 2019/6/18
+ * @description： 购药配置信息
+ * @version： 1.0
+ */
+
+@Configuration
+public class PurchaseConfigure {
+
+    @Bean
+    public IPurchaseService payModeOnlineService(){
+        return new PayModeOnline();
+    }
+
+    @Bean
+    public IPurchaseService payModeTFDSService(){
+        return new PayModeTFDS();
+    }
+}
