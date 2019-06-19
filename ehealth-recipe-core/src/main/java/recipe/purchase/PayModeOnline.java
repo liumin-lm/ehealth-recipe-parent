6d5ad3052ce8122809b9a9800275c8d65a8d79f8
yuyun <yuy@ngarihealth.com>
2019-06-19 15:09:42 +0800

    taks#18277 购药方式流程改造，增加顶层接口

--- ehealth-recipe-core/src/main/java/recipe/purchase/PayModeOnline.java
+++ ehealth-recipe-core/src/main/java/recipe/purchase/PayModeOnline.java
@@ -164,7 +164,7 @@ public class PayModeOnline implements IPurchaseService {
     }
 
     @Override
-    public RecipeResultBean order(Recipe dbRecipe) {
+    public RecipeResultBean order(Recipe dbRecipe, Map<String, String> extInfo) {
         return null;
     }
 
