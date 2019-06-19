43057aa1ebbee91209e87aac2f405c2ed6915927
shiyuping <shiyp@ngarihealth.com>
2019-06-19 16:54:46 +0800

    task#18232 江苏省监管平台处方数据处理

--- ehealth-recipe-core/src/main/java/recipe/thread/PushRecipeToRegulationCallable.java
+++ ehealth-recipe-core/src/main/java/recipe/thread/PushRecipeToRegulationCallable.java
@@ -57,7 +57,9 @@ public class PushRecipeToRegulationCallable implements Callable<String> {
                     if (status == 2){
                         response = service.uploadRecipeAuditIndicators(Arrays.asList(recipe));
                         if (CommonConstant.SUCCESS.equals(response.getCode())){
-                            response = service.uploadRecipeCirculationIndicators(Arrays.asList(recipe));
+                            if (RecipeStatusConstant.CHECK_PASS_YS==recipe.getStatus()){
+                                response = service.uploadRecipeCirculationIndicators(Arrays.asList(recipe));
+                            }
                         } else{
                             logger.warn("uploadRecipeAuditIndicators rpc execute error. recipe={}", JSONUtils.toString(recipe));
                         }
