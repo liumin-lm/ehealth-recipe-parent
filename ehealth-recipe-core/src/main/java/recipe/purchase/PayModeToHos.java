a6ee7e6c325e6b0cad4138b8fea8a71470ebac8c
yuyun <yuy@ngarihealth.com>
2019-06-21 10:52:34 +0800

    taks#18277 购药方式流程改造，增加到院取药

--- ehealth-recipe-core/src/main/java/recipe/purchase/PayModeToHos.java
+++ ehealth-recipe-core/src/main/java/recipe/purchase/PayModeToHos.java
@@ -2,9 +2,15 @@ package recipe.purchase;
 
 import com.ngari.recipe.common.RecipeResultBean;
 import com.ngari.recipe.entity.Recipe;
+import com.ngari.recipe.entity.Recipedetail;
 import com.ngari.recipe.recipeorder.model.OrderCreateResult;
+import ctd.persistence.DAOFactory;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import recipe.constant.RecipeBussConstant;
+import recipe.dao.RecipeDetailDAO;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -17,13 +23,29 @@ public class PayModeToHos implements IPurchaseService{
 
     @Override
     public RecipeResultBean findSupportDepList(Recipe dbRecipe, Map<String, String> extInfo) {
-        return null;
+        RecipeDetailDAO detailDAO = DAOFactory.getDAO(RecipeDetailDAO.class);
+
+        RecipeResultBean resultBean = RecipeResultBean.getSuccess();
+        Integer recipeId = dbRecipe.getRecipeId();
+        List<Recipedetail> detailList = detailDAO.findByRecipeId(recipeId);
+        StringBuilder sb = new StringBuilder();
+
+        if(CollectionUtils.isNotEmpty(detailList)){
+            String pharmNo = detailList.get(0).getPharmNo();
+            if(StringUtils.isNotEmpty(pharmNo)){
+                sb.append("到院自取需去医院取药窗口取药："+pharmNo);
+            }else {
+                sb.append("选择到院自取后，需去医院取药窗口取药");
+            }
+        }
+
+        resultBean.setMsg(sb.toString());
+        return resultBean;
     }
 
     @Override
     public OrderCreateResult order(Recipe dbRecipe, Map<String, String> extInfo) {
-        
-        return null;
+       return null;
     }
 
     @Override
