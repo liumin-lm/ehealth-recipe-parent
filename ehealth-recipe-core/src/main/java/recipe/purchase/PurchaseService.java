c3079022a36f932ee10b63e2e634c393bef4fc32
yinsheng <yins@ngarihealth.com>
2019-06-27 20:07:22 +0800

    TASK#18335 子 【后台】患者购药流程改造，药店取药 / 获取药店列表接口改造-对接钥世圈

--- ehealth-recipe-core/src/main/java/recipe/purchase/PurchaseService.java
+++ ehealth-recipe-core/src/main/java/recipe/purchase/PurchaseService.java
@@ -125,7 +125,7 @@ public class PurchaseService {
         }
 
         //处方单状态不是待处理 or 处方单已被处理
-        boolean dealFlag = checkRecipeIsDeal(dbRecipe, resultBean);
+        boolean dealFlag = checkRecipeIsUser(dbRecipe, resultBean);
         if(dealFlag){
             return resultBean;
         }
@@ -279,6 +279,28 @@ public class PurchaseService {
                 } else if (RecipeBussConstant.PAYMODE_ONLINE.equals(dbRecipe.getPayMode())) {
                     result.setCode(4);
                     result.setMsg(dbRecipe.getOrderCode());
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 检查处方是否已被处理
+     * @param dbRecipe
+     * @param result
+     * @return true 已被处理
+     */
+    private boolean checkRecipeIsUser(Recipe dbRecipe, RecipeResultBean result){
+        if (RecipeStatusConstant.CHECK_PASS != dbRecipe.getStatus()
+                || 1 == dbRecipe.getChooseFlag()) {
+            result.setCode(RecipeResultBean.FAIL);
+            result.setMsg("处方单已被处理");
+            //判断是否已到院取药，查看 HisCallBackService *RecipesFromHis 方法处理
+            if (Integer.valueOf(1).equals(dbRecipe.getPayFlag())) {
+                if (RecipeBussConstant.PAYMODE_TO_HOS.equals(dbRecipe.getPayMode())) {
+                    result.setMsg("您已到院自取药品，无法选择其他购药方式");
                 }
             }
             return true;
