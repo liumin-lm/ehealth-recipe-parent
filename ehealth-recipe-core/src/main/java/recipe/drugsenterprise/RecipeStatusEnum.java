3e516fa55c847692bae23e33c8905b282aed2bb4
wuyijing <wuyijing@ngarihealth.com>
2019-09-24 09:20:46 +0800

    O2O天猫对接

--- ehealth-recipe-core/src/main/java/recipe/drugsenterprise/RecipeStatusEnum.java
+++ ehealth-recipe-core/src/main/java/recipe/drugsenterprise/RecipeStatusEnum.java
@@ -12,19 +12,32 @@ public enum RecipeStatusEnum {
     //使用中
     USING("22", "USING"),
     //已使用
-    USED("6", "USED");
+    USED("6", "USED"),
+    //已过期
+    EXPIRED("20", "EXPIRED"),
+    //已退回
+    RETURNED("6", "RETURNED");
 
     private String key;
     private String value;
 
-    RecipeStatusEnum(String name, String value) {
-        this.key = name;
+    RecipeStatusEnum(String key, String value) {
+        this.key = key;
         this.value = value;
     }
     public static String getKey(String value){
         for(RecipeStatusEnum c:RecipeStatusEnum.values()){
             if(c.getValue().equals(value)){
                 return c.getKey();
+            }
+        }
+        return null;
+    }
+
+    public static String getValue(String key){
+        for(RecipeStatusEnum c:RecipeStatusEnum.values()){
+            if(c.getKey().equals(key)){
+                return c.getValue();
             }
         }
         return null;
