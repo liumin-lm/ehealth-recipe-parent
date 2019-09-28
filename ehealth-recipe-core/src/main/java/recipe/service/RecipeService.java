8084ed0bc58a73f456fd4955daa9f561b2392c4c
shiyuping <shiyp@ngarihealth.com>
2019-09-28 17:34:05 +0800

    task#27651 原二次审方的机构医生收不到审核不通过消息，推送消息在判断是否二次前处理

--- ehealth-recipe-core/src/main/java/recipe/service/RecipeService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/RecipeService.java
@@ -1132,7 +1132,6 @@ public class RecipeService extends RecipeBaseService{
         if (!effective) {
             return;
         }
-        RecipeHisService hisService = ApplicationUtils.getRecipeService(RecipeHisService.class);
         RecipeOrderService orderService = ApplicationUtils.getRecipeService(RecipeOrderService.class);
         //相应订单处理
         orderService.cancelOrderByRecipeId(recipe.getRecipeId(), OrderStatusConstant.CANCEL_NOT_PASS);
@@ -1142,25 +1141,21 @@ public class RecipeService extends RecipeBaseService{
             //线上支付
             //微信退款
             wxPayRefundForRecipe(2, recipe.getRecipeId(), null);
-            if (RecipeBussConstant.FROMFLAG_PLATFORM.equals(recipe.getFromflag())) {
+            /*if (RecipeBussConstant.FROMFLAG_PLATFORM.equals(recipe.getFromflag())) {
                 RecipeMsgService.batchSendMsg(recipe.getRecipeId(), RecipeStatusConstant.CHECK_NOT_PASSYS_PAYONLINE);
-            }
+            }*/
         } else if (RecipeBussConstant.PAYMODE_COD.equals(recipe.getPayMode()) || RecipeBussConstant.PAYMODE_TFDS.equals(recipe.getPayMode())) {
             //货到付款 | 药店取药
-            if (RecipeBussConstant.FROMFLAG_PLATFORM.equals(recipe.getFromflag())) {
+           /* if (RecipeBussConstant.FROMFLAG_PLATFORM.equals(recipe.getFromflag())) {
                 RecipeMsgService.batchSendMsg(recipe, RecipeStatusConstant.CHECK_NOT_PASSYS_REACHPAY);
-            }
+            }*/
         }
 
-        if (RecipeBussConstant.FROMFLAG_HIS_USE.equals(recipe.getFromflag())) {
+        /*if (RecipeBussConstant.FROMFLAG_HIS_USE.equals(recipe.getFromflag())) {
             //发送审核不成功消息
             //${sendOrgan}：抱歉，您的处方未通过药师审核。如有收取费用，款项将为您退回，预计1-5个工作日到账。如有疑问，请联系开方医生或拨打${customerTel}联系小纳。
             RecipeMsgService.sendRecipeMsg(RecipeMsgEnum.RECIPE_YS_CHECKNOTPASS_4HIS, recipe);
-        }
-        //HIS消息发送
-        //审核不通过 往his更新状态（已取消）
-        hisService.recipeStatusUpdate(recipe.getRecipeId());
-        RecipeLogService.saveRecipeLog(recipe.getRecipeId(), recipe.getStatus(), recipe.getStatus(), "审核不通过处理完成");
+        }*/
     }
 
     /**
