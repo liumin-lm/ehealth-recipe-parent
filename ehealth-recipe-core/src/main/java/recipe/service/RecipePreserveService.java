51b0f0a90ecb44008f9a613a72499f7d090c0f99
shiyuping <shiyp@ngarihealth.com>
2019-10-15 16:25:55 +0800

    task#28666 武昌九州通外延处方项目改造

--- ehealth-recipe-core/src/main/java/recipe/service/RecipePreserveService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/RecipePreserveService.java
@@ -448,7 +448,7 @@ public class RecipePreserveService {
         String organCode = organService.getOrganizeCodeByOrganId(sourceOrgan);
         //批量同步
         int start = 0;
-        int limit = 200;
+        int limit = 500;
         int total = 0;
         while (true){
             List<DrugList> drugs = dao.findDrugListBySourceOrgan(sourceOrgan,start,limit);
