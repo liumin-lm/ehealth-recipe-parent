a95b40cadb3085e097af1a3d7de76a3a2f4c04f4
yinsheng <yins@ngarihealth.com>
2019-10-15 15:51:27 +0800

    BUG#33371 【电子处方】药店取药审核通过后没有收到取药通知模板消息

--- ehealth-recipe-core/src/main/java/recipe/audit/auditmode/AuditPostMode.java
+++ ehealth-recipe-core/src/main/java/recipe/audit/auditmode/AuditPostMode.java
@@ -48,6 +48,23 @@ public class AuditPostMode extends AbstractAuidtMode {
     @Override
     public void afterCheckPassYs(Recipe recipe) {
         recipeService.afterCheckPassYs(recipe);
+        //此处增加药店取药消息推送
+        RemoteDrugEnterpriseService remoteDrugService = ApplicationUtils.getRecipeService(RemoteDrugEnterpriseService.class);
+        DrugsEnterpriseDAO drugsEnterpriseDAO = DAOFactory.getDAO(DrugsEnterpriseDAO.class);
+        if (recipe.getEnterpriseId() == null) {
+            LOGGER.info("审方后置-药店取药-药企为空");
+        } else {
+            DrugsEnterprise drugsEnterprise = drugsEnterpriseDAO.getById(recipe.getEnterpriseId());
+            boolean scanFlag = remoteDrugService.scanStock(recipe.getRecipeId(), drugsEnterprise);
+            LOGGER.info("AuditPostMode afterCheckPassYs scanFlag:{}.", scanFlag);
+            if (scanFlag) {
+                //表示需要进行库存校验并且有库存
+                RecipeMsgService.sendRecipeMsg(RecipeMsgEnum.RECIPE_DRUG_HAVE_STOCK, recipe);
+            } else if (drugsEnterprise.getCheckInventoryFlag() == 2) {
+                //表示无库存但是药店可备货
+                RecipeMsgService.sendRecipeMsg(RecipeMsgEnum.RECIPE_DRUG_NO_STOCK_READY, recipe);
+            }
+        }
     }
 
     @Override
@@ -117,23 +134,6 @@ public class AuditPostMode extends AbstractAuidtMode {
                     status = RecipeStatusConstant.READY_CHECK_YS;
                 }
                 memo = "药店取药-到店取药成功";
-                //此处增加药店取药消息推送
-                RemoteDrugEnterpriseService remoteDrugService = ApplicationUtils.getRecipeService(RemoteDrugEnterpriseService.class);
-                DrugsEnterpriseDAO drugsEnterpriseDAO = DAOFactory.getDAO(DrugsEnterpriseDAO.class);
-                if (dbRecipe.getEnterpriseId() == null) {
-                    LOGGER.info("审方后置-药店取药-药企为空");
-                } else {
-                    DrugsEnterprise drugsEnterprise = drugsEnterpriseDAO.getById(dbRecipe.getEnterpriseId());
-                    boolean scanFlag = remoteDrugService.scanStock(dbRecipe.getRecipeId(), drugsEnterprise);
-                    if (scanFlag) {
-                        //表示需要进行库存校验并且有库存
-                        RecipeMsgService.sendRecipeMsg(RecipeMsgEnum.RECIPE_DRUG_HAVE_STOCK, dbRecipe);
-                    } else if (drugsEnterprise.getCheckInventoryFlag() == 2) {
-                        //表示无库存但是药店可备货
-                        RecipeMsgService.sendRecipeMsg(RecipeMsgEnum.RECIPE_DRUG_NO_STOCK_READY, dbRecipe);
-                    }
-                }
-
             }else if (RecipeBussConstant.GIVEMODE_DOWNLOAD_RECIPE.equals(giveMode)){
                 if(PayConstant.PAY_FLAG_PAY_SUCCESS == payFlag){
                     status = RecipeStatusConstant.READY_CHECK_YS;
