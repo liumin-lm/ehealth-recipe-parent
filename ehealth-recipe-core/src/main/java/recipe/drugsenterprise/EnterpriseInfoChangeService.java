f927c0fdc704b9d6aa715ebc31cdb448f150b88c
yinsheng <yins@ngarihealth.com>
2019-11-15 17:17:05 +0800

    TASK #32974 【后台】【岳阳市一人民医院】便民药房项目开处方数量不能大于库存数量

--- ehealth-recipe-core/src/main/java/recipe/drugsenterprise/EnterpriseInfoChangeService.java
+++ ehealth-recipe-core/src/main/java/recipe/drugsenterprise/EnterpriseInfoChangeService.java
@@ -1,6 +1,8 @@
 package recipe.drugsenterprise;
 
 import com.alibaba.fastjson.JSONObject;
+import com.ngari.recipe.entity.DrugsEnterprise;
+import com.ngari.recipe.entity.SaleDrugList;
 import ctd.persistence.DAOFactory;
 import ctd.util.annotation.RpcBean;
 import ctd.util.annotation.RpcService;
@@ -14,10 +16,13 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import recipe.dao.DrugsEnterpriseDAO;
 import recipe.dao.RecipeParameterDao;
+import recipe.dao.SaleDrugListDAO;
 import recipe.drugsenterprise.bean.StoreInventoryResponse;
 import recipe.util.DigestUtil;
 
+import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -36,6 +41,8 @@ public class EnterpriseInfoChangeService {
     public void updateStoreInventory(){
         try{
             RecipeParameterDao recipeParameterDao = DAOFactory.getDAO(RecipeParameterDao.class);
+            SaleDrugListDAO saleDrugListDAO = DAOFactory.getDAO(SaleDrugListDAO.class);
+            DrugsEnterpriseDAO drugsEnterpriseDAO = DAOFactory.getDAO(DrugsEnterpriseDAO.class);
             String time = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
             String roledata = recipeParameterDao.getByName("yy-roledata");
             String password = recipeParameterDao.getByName("yy-password");
@@ -52,10 +59,22 @@ public class EnterpriseInfoChangeService {
             CloseableHttpResponse response = httpClient.execute(httpPost);
             HttpEntity httpEntity = response.getEntity();
             String responseStr = EntityUtils.toString(httpEntity);
+            LOGGER.info("EnterpriseInfoChangeService-updateStoreInventory responseStr:{}.", responseStr);
             List<StoreInventoryResponse> storeResponses = JSONObject.parseArray(responseStr, StoreInventoryResponse.class);
             LOGGER.info("EnterpriseInfoChangeService-updateStoreInventory storeResponses:{}.", storeResponses);
             if (storeResponses != null && storeResponses.size() > 0) {
-
+                for (StoreInventoryResponse storeInventoryResponse : storeResponses) {
+                    String hwarecode = storeInventoryResponse.getHwarecode();
+                    double storeqty = storeInventoryResponse.getStoreqty();
+                    List<DrugsEnterprise> drugsEnterprises = drugsEnterpriseDAO.findAllDrugsEnterpriseByName("岳阳-钥世圈");
+                    SaleDrugList saleDrugList = saleDrugListDAO.getByOrganIdAndDrugCode(drugsEnterprises.get(0).getId(), hwarecode);
+                    if (saleDrugList != null) {
+                        if (saleDrugList.getInventory() != null && saleDrugList.getInventory().doubleValue() != storeqty) {
+                            saleDrugList.setInventory(new BigDecimal(storeqty));
+                            saleDrugListDAO.save(saleDrugList);
+                        }
+                    }
+                }
             }
         }catch (Exception e){
             LOGGER.info("EnterpriseInfoChangeService-updateStoreInventory 更新药店药品库存失败.");
