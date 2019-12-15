b482433ef5b820acae8b5b449bff23d41499571a
shiyuping <shiyp@ngarihealth.com>
2019-12-15 21:06:42 +0800

    update

--- ehealth-recipe-core/src/main/java/recipe/service/hospitalrecipe/PrescribeService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/hospitalrecipe/PrescribeService.java
@@ -320,7 +320,7 @@ public class PrescribeService {
             Integer clinicOrgan = null;
             Recipe dbRecipe;
             //是否更新处方号 此时必须要有平台处方id
-            if (request.getUpdateRecipeCodeFlag()){
+            if (request.getUpdateRecipeCodeFlag() != null && request.getUpdateRecipeCodeFlag()){
                 if (StringUtils.isEmpty(request.getPlatRecipeID())){
                     result.setMsg("平台处方Id为空");
                     return result;
