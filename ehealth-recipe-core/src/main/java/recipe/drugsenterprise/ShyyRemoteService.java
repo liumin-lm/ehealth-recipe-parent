89f68cc8c97f824abbfc1ca7bc070c81e94d5fe8
yinsheng <yins@ngarihealth.com>
2020-03-04 17:23:05 +0800

    上药对接

--- ehealth-recipe-core/src/main/java/recipe/drugsenterprise/ShyyRemoteService.java
+++ ehealth-recipe-core/src/main/java/recipe/drugsenterprise/ShyyRemoteService.java
@@ -1,12 +1,21 @@
 package recipe.drugsenterprise;
 
 import com.ngari.recipe.entity.DrugsEnterprise;
+import com.ngari.recipe.entity.Recipe;
+import com.ngari.recipe.entity.Recipedetail;
+import com.ngari.recipe.entity.SaleDrugList;
 import com.ngari.recipe.hisprescription.model.HospitalRecipeDTO;
+import ctd.persistence.DAOFactory;
+import ctd.util.JSONUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import recipe.bean.DrugEnterpriseResult;
 import recipe.constant.DrugEnterpriseConstant;
+import recipe.dao.RecipeDAO;
+import recipe.dao.RecipeDetailDAO;
+import recipe.dao.SaleDrugListDAO;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
@@ -29,7 +38,23 @@ public class ShyyRemoteService  extends AccessDrugEnterpriseService {
 
     @Override
     public DrugEnterpriseResult pushRecipeInfo(List<Integer> recipeIds, DrugsEnterprise enterprise) {
+        LOGGER.info("ShyyRemoteService.pushRecipeInfo recipeIds:{}.", JSONUtils.toString(recipeIds));
+        //上海益友此处进行减库存的操作
+        updateEnterpriseInventory(recipeIds.get(0), enterprise);
         return DrugEnterpriseResult.getSuccess();
+    }
+
+    private void updateEnterpriseInventory(Integer recipeId, DrugsEnterprise drugsEnterprise) {
+        RecipeDetailDAO recipeDetailDAO = DAOFactory.getDAO(RecipeDetailDAO.class);
+        List<Recipedetail> recipedetails = recipeDetailDAO.findByRecipeId(recipeId);
+        SaleDrugListDAO saleDrugListDAO = DAOFactory.getDAO(SaleDrugListDAO.class);
+        for (Recipedetail recipedetail : recipedetails) {
+            Integer drugId = recipedetail.getDrugId();
+            Double useTotalDose = recipedetail.getUseTotalDose();
+            BigDecimal totalDose = new BigDecimal(useTotalDose);
+            LOGGER.info("ShyyRemoteService.updateEnterpriseInventory 更新库存成功,更新药品:{},更新数量:{},处方单号：{}.", drugId, totalDose, recipeId);
+            saleDrugListDAO.updateInventoryByOrganIdAndDrugId(drugsEnterprise.getId(), drugId, totalDose);
+        }
     }
 
     @Override
@@ -39,12 +64,26 @@ public class ShyyRemoteService  extends AccessDrugEnterpriseService {
 
     @Override
     public String getDrugInventory(Integer drugId, DrugsEnterprise drugsEnterprise) {
-        return "";
+        SaleDrugListDAO saleDrugListDAO = DAOFactory.getDAO(SaleDrugListDAO.class);
+        SaleDrugList saleDrugList = saleDrugListDAO.getByDrugIdAndOrganId(drugId, drugsEnterprise.getId());
+        return saleDrugList.getInventory()+"";
     }
 
     @Override
     public DrugEnterpriseResult scanStock(Integer recipeId, DrugsEnterprise drugsEnterprise) {
-        return DrugEnterpriseResult.getSuccess();
+        DrugEnterpriseResult drugEnterpriseResult = DrugEnterpriseResult.getSuccess();
+        RecipeDetailDAO recipeDetailDAO = DAOFactory.getDAO(RecipeDetailDAO.class);
+        List<Recipedetail> recipedetails = recipeDetailDAO.findByRecipeId(recipeId);
+        SaleDrugListDAO saleDrugListDAO = DAOFactory.getDAO(SaleDrugListDAO.class);
+        for (Recipedetail recipedetail : recipedetails) {
+            SaleDrugList saleDrugList = saleDrugListDAO.getByDrugIdAndOrganId(recipedetail.getDrugId(), drugsEnterprise.getId());
+            if (saleDrugList.getInventory().doubleValue() < recipedetail.getUseTotalDose()) {
+                //说明开药比较多
+                drugEnterpriseResult.setCode(DrugEnterpriseResult.FAIL);
+                return drugEnterpriseResult;
+            }
+        }
+        return drugEnterpriseResult;
     }
 
     @Override
