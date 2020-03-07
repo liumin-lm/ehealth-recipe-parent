20854ef74962638831983162fc6c85648672d12c
yushijun <yusj@ngarihealth.com>
2020-03-07 17:05:51 +0800

    task # 44256 上海六院CA对接开发

--- ehealth-recipe-core/src/main/java/recipe/ca/factory/CommonCAFactory.java
+++ ehealth-recipe-core/src/main/java/recipe/ca/factory/CommonCAFactory.java
@@ -15,15 +15,25 @@ import recipe.ca.impl.ShanxiCAImpl;
 public class CommonCAFactory {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CommonCAFactory.class);
+    /**
+     * 陕西CA
+     */
+    private static final String CA_TYPE_SHANXI= "shanxiCA";
+    /**
+     * 上海CA
+     */
+    private static final String CA_TYPE_SHANGHAI= "shanghaiCA";
 
     public CAInterface useCAFunction(Integer organId) {
         LOGGER.info("useCAFunction start in organId={}", organId);
         IConfigurationCenterUtilsService configurationService = BaseAPI.getService(IConfigurationCenterUtilsService.class);
         String thirdCASign = (String) configurationService.getConfiguration(organId, "thirdCASign");
         LOGGER.info("useCAFunction thirdCASign={}", thirdCASign);
-        if ("shanxiCA".equals(thirdCASign)) {
+        //陕西CA
+        if (CA_TYPE_SHANXI.equals(thirdCASign)) {
             return new ShanxiCAImpl();
-        } else if ("shanghaiCA".equals(thirdCASign)) {
+        //上海CA
+        } else if (CA_TYPE_SHANGHAI.equals(thirdCASign)) {
             return new ShanghaiCAImpl();
         } else {
             LOGGER.info("没有找到对应的CA配置，请检查运营平台的配置是否正确thirdCASign=", thirdCASign);
