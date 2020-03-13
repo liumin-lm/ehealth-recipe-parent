41157d83855ee74fdfb1f841fd22e984ec748c13
yinsheng <yins@ngarihealth.com>
2020-03-13 13:37:17 +0800

    Task#his线下处方流转线上

--- ehealth-recipe-core/src/main/java/recipe/service/HisRecipeService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/HisRecipeService.java
@@ -195,6 +195,7 @@ public class HisRecipeService {
         recipe.setCreateDate(hisRecipe.getCreateDate());
         recipe.setOrganDiseaseName(hisRecipe.getDiseaseName());
         recipe.setOrganDiseaseId(hisRecipe.getDisease());
+        recipe.setTotalMoney(hisRecipe.getTotalAmount());
         recipe.setPayFlag(0);
         recipe.setStatus(2);
         recipe.setReviewType(0);
