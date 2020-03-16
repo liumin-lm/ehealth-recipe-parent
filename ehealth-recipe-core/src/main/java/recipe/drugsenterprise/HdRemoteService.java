59adc79cec639a8707a0f4e312362b5be73fc967
yinsheng <yins@ngarihealth.com>
2020-03-16 23:33:02 +0800

    BUG#39250 【实施】-【浙大四院】-【A类】-对接华东药企大药房

--- ehealth-recipe-core/src/main/java/recipe/drugsenterprise/HdRemoteService.java
+++ ehealth-recipe-core/src/main/java/recipe/drugsenterprise/HdRemoteService.java
@@ -576,9 +576,12 @@ public class HdRemoteService extends AccessDrugEnterpriseService {
         String hdStores = recipeParameterDao.getByName("hd_store_payonline");
         String storeOrganName = nowRecipe.getClinicOrgan() + "_" + "hd_organ_store";
         String organStore = recipeParameterDao.getByName(storeOrganName);
+        String hdStorePayOnlineFlag = recipeParameterDao.getByName("hdStorePayOnlineFlag");
         if (StringUtils.isNotEmpty(hdStores) && hdStores.contains(nowRecipe.getClinicOrgan().toString())) {
             sendHdRecipe.setGiveMode("4");
-            sendHdRecipe.setPharmacyCode(organStore);
+            if ("1".equals(hdStorePayOnlineFlag)) {
+                sendHdRecipe.setPharmacyCode(organStore);
+            }
         }
     }
 
