4afdba1393b70174d3d21f6b5fa03636285a9642
wuzhentao <1228764596@qq,com>
2020-03-18 22:50:04 +0800

    commit

--- ehealth-recipe-dao/src/main/java/recipe/dao/RecipeDAO.java
+++ ehealth-recipe-dao/src/main/java/recipe/dao/RecipeDAO.java
@@ -2131,7 +2131,7 @@ public abstract class RecipeDAO extends HibernateSupportDelegateDAO<Recipe> {
         HibernateStatelessResultAction<List<Object[]>> action = new AbstractHibernateStatelessResultAction<List<Object[]>>() {
             @Override
             public void execute(StatelessSession ss) throws Exception {
-                StringBuilder hql = new StringBuilder("select sum(TotalMoney),Depart from Recipe where CreateDate between :startDate and :endDate and ClinicOrgan=: organId and payFlag=1 GROUP BY Depart");
+                StringBuilder hql = new StringBuilder("select sum(TotalMoney),Depart from Recipe where CreateDate between :startDate and :endDate and ClinicOrgan=: organId and PayFlag = 1 GROUP BY Depart");
                 Query query = ss.createQuery(hql.toString());
                 query.setParameter("organId",organId);
                 query.setParameter("startDate",startDate);
@@ -2141,7 +2141,7 @@ public abstract class RecipeDAO extends HibernateSupportDelegateDAO<Recipe> {
             }
         };
 
-        HibernateSessionTemplate.instance().execute(action);
+        HibernateSessionTemplate.instance().executeReadOnly(action);
         return action.getResult();
     }
 }
