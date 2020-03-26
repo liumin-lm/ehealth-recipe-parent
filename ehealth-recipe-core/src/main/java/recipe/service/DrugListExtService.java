d212f2b68d675bfe4e734afe2a298868216a5b8b
yuwx <yuwx@ngarihealth.com>
2020-03-26 11:36:34 +0800

    BUG #39826 【在线复诊】既往用药接口搜索无结果

--- ehealth-recipe-core/src/main/java/recipe/service/DrugListExtService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/DrugListExtService.java
@@ -2,7 +2,6 @@ package recipe.service;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-import com.google.inject.internal.cglib.proxy.$MethodProxy;
 import com.ngari.base.searchcontent.model.SearchContentBean;
 import com.ngari.base.searchcontent.service.ISearchContentService;
 import com.ngari.base.searchservice.model.DrugSearchTO;
@@ -16,7 +15,6 @@ import ctd.controller.exception.ControllerException;
 import ctd.dictionary.DictionaryController;
 import ctd.dictionary.DictionaryItem;
 import ctd.persistence.DAOFactory;
-import ctd.persistence.exception.DAOException;
 import ctd.util.AppContextHolder;
 import ctd.util.JSONUtils;
 import ctd.util.annotation.RpcBean;
@@ -36,7 +34,6 @@ import recipe.serviceprovider.BaseService;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
