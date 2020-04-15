41353b53860aa646c392e8a107b8e5c67be735be
shiyuping <shiyp@ngarihealth.com>
2020-04-15 13:56:08 +0800

    task#50522 处方扩展信息新增接口更新人脸识别token字段保存

--- ehealth-recipe-core/src/main/java/recipe/service/RecipeExtendService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/RecipeExtendService.java
@@ -1,9 +1,11 @@
 package recipe.service;
 
+import com.google.common.collect.Maps;
 import ctd.persistence.DAOFactory;
-import ctd.util.JSONUtils;
+import ctd.persistence.exception.DAOException;
 import ctd.util.annotation.RpcBean;
 import ctd.util.annotation.RpcService;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import recipe.dao.RecipeExtendDAO;
@@ -13,13 +15,24 @@ import java.util.Map;
 /**
  * created by shiyuping on 2020/4/14
  */
+@RpcBean("recipeExtendService")
 public class RecipeExtendService {
-   /* private static final Logger LOGGER = LoggerFactory.getLogger(RecipeExtendService.class);
-
+    private final Logger LOGGER = LoggerFactory.getLogger(RecipeExtendService.class);
+    /**
+     * 更新处方扩展信息人脸识别认证成功的bizToken
+     * @param recipeId
+     * @param bizToken 人脸识别认证成功bizToken
+     * @return
+     */
     @RpcService
-    public Boolean updateRecipeExInfoByRecipeId(int recipeId,Map<String, ?> changeAttr){
+    public Boolean updateBizTokenByRecipeId(int recipeId,String bizToken){
+        LOGGER.info("updateBizTokenByRecipeId recipeId={},bizToken={}",recipeId, bizToken);
+        if (StringUtils.isEmpty(bizToken)){
+            throw new DAOException(DAOException.VALUE_NEEDED, "bizToken is require");
+        }
         RecipeExtendDAO recipeExtendDAO = DAOFactory.getDAO(RecipeExtendDAO.class);
-        LOGGER.info("updateRecipeExInfoByRecipeId recipeId={},changeAttr={}",recipeId, JSONUtils.toString(changeAttr));
+        Map<String, Object> changeAttr = Maps.newHashMap();
+        changeAttr.put("medicalSettleData",bizToken);
         return recipeExtendDAO.updateRecipeExInfoByRecipeId(recipeId,changeAttr);
-    }*/
+    }
 }
