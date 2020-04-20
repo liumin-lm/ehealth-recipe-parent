0af2f6fb6fd3247b398514801301cfd0b3d025c6
yinsheng <yins@ngarihealth.com>
2020-04-20 21:19:57 +0800

    TASK#51072 【后台】40158 【实施】【上海市胸科医院】【A】上药接口对接医院ESB接口

--- ehealth-recipe-core/src/main/java/recipe/drugsenterprise/EbsRemoteService.java
+++ ehealth-recipe-core/src/main/java/recipe/drugsenterprise/EbsRemoteService.java
@@ -1,13 +1,19 @@
 package recipe.drugsenterprise;
 
+import com.ngari.patient.dto.DepartmentDTO;
+import com.ngari.patient.dto.OrganDTO;
+import com.ngari.patient.dto.PatientDTO;
 import com.ngari.patient.service.*;
 import com.ngari.recipe.entity.DrugsEnterprise;
 import com.ngari.recipe.entity.Recipe;
 import com.ngari.recipe.entity.RecipeOrder;
 import com.ngari.recipe.hisprescription.model.HospitalRecipeDTO;
+import ctd.controller.exception.ControllerException;
+import ctd.dictionary.DictionaryController;
 import ctd.util.JSONUtils;
 import ctd.util.annotation.RpcBean;
 import ctd.util.annotation.RpcService;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.CollectionUtils;
@@ -72,10 +78,47 @@ public class EbsRemoteService extends AccessDrugEnterpriseService {
             Recipe recipe = recipeList.get(0);
             RecipeOrder recipeOrder = recipeOrderDAO.getByOrderCode(recipe.getOrderCode());
 
+            OrganDTO organDTO = organService.getByOrganId(recipe.getClinicOrgan());
+            DepartmentDTO departmentDTO = departmentService.get(recipe.getDepart());
+            PatientDTO patientDTO = patientService.getByMpiId(recipe.getMpiid());
+
             EbsBean ebsBean = new EbsBean();
             ebsBean.setPrescripNo(recipe.getRecipeCode());
             ebsBean.setPrescribeDate(recipe.getSignDate().getTime());
-            //ebsBean.setHospitalCode();
+            ebsBean.setHospitalCode(organDTO.getOrganizeCode());
+            ebsBean.setHospitalName(organDTO.getName());
+            ebsBean.setDepartment(departmentDTO.getName());
+            ebsBean.setDoctorName(recipe.getDoctorName());
+            ebsBean.setName(recipe.getPatientName());
+            if (patientDTO != null && new Integer(1).equals(patientDTO.getPatientSex())) {
+                ebsBean.setSex(1);
+            } else {
+                ebsBean.setSex(0);
+            }
+            ebsBean.setAge(patientDTO.getAge());
+            ebsBean.setMobile(patientDTO.getMobile());
+            ebsBean.setIdCard(patientDTO.getCertificate());
+            ebsBean.setSocialSecurityCard("");
+            ebsBean.setAddress("");
+            if (recipeOrder != null && new Integer(0).equals(recipeOrder.getOrderType())) {
+                ebsBean.setFeeType(0);
+            } else {
+                ebsBean.setFeeType(1);
+            }
+            ebsBean.setDistrictName(recipe.getOrganDiseaseName());
+            if (recipeOrder != null) {
+                ebsBean.setTotalAmount(recipeOrder.getTotalFee());
+                ebsBean.setReceiver(recipeOrder.getReceiver());
+                ebsBean.setReceiverMobile(recipeOrder.getRecMobile());
+                String province = getAddressDic(recipeOrder.getAddress1());
+                String city = getAddressDic(recipeOrder.getAddress2());
+                String district = getAddressDic(recipeOrder.getAddress3());
+                ebsBean.setProvinceName(province);
+                ebsBean.setCityName(city);
+                ebsBean.setDistrictName(district);
+            }
+
+
         }
         return null;
     }
@@ -120,5 +163,21 @@ public class EbsRemoteService extends AccessDrugEnterpriseService {
         LOGGER.info(msg);
         result.setCode(DrugEnterpriseResult.FAIL);
         return result;
+    }
+
+    /**
+     * 获取区域文本
+     * @param area 区域
+     * @return     区域文本
+     */
+    private String getAddressDic(String area) {
+        if (StringUtils.isNotEmpty(area)) {
+            try {
+                return DictionaryController.instance().get("eh.base.dictionary.AddrArea").getText(area);
+            } catch (ControllerException e) {
+                LOGGER.error("getAddressDic 获取地址数据类型失败*****area:" + area);
+            }
+        }
+        return "";
     }
 }
