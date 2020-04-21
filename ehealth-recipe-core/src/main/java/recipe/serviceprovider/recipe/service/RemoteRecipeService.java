2b4d41a71b18ea326c8847c90c837f781f42ba59
zhangxq <zhangxq@ngarihealth.com>
2020-04-21 13:44:05 +0800

    处方查询优化

--- ehealth-recipe-core/src/main/java/recipe/serviceprovider/recipe/service/RemoteRecipeService.java
+++ ehealth-recipe-core/src/main/java/recipe/serviceprovider/recipe/service/RemoteRecipeService.java
@@ -186,9 +186,9 @@ public class RemoteRecipeService extends BaseService<RecipeBean> implements IRec
     public QueryResult<Map> findRecipesByInfo(Integer organId, Integer status,
                                               Integer doctor, String patientName, Date bDate, Date eDate, Integer dateType,
                                               Integer depart, int start, int limit, List<Integer> organIds, Integer giveMode,Integer fromflag,Integer recipeId,
-            Integer enterpriseId,Integer checkStatus,Integer payFlag) {
+            Integer enterpriseId,Integer checkStatus,Integer payFlag,Integer orderType) {
         RecipeDAO recipeDAO = DAOFactory.getDAO(RecipeDAO.class);
-        return recipeDAO.findRecipesByInfo(organId, status, doctor, patientName, bDate, eDate, dateType, depart, start, limit, organIds, giveMode,fromflag,recipeId,enterpriseId,checkStatus,payFlag);
+        return recipeDAO.findRecipesByInfo(organId, status, doctor, patientName, bDate, eDate, dateType, depart, start, limit, organIds, giveMode,fromflag,recipeId,enterpriseId,checkStatus,payFlag,orderType);
     }
 
     @RpcService
@@ -314,9 +314,9 @@ public class RemoteRecipeService extends BaseService<RecipeBean> implements IRec
     @RpcService
     public List<Map> findRecipesByInfoForExcel(final Integer organId, final Integer status, final Integer doctor, final String patientName, final Date bDate,
                                         final Date eDate, final Integer dateType, final Integer depart, List<Integer> organIds, Integer giveMode,
-                                        Integer fromflag,Integer recipeId,Integer enterpriseId,Integer checkStatus,Integer payFlag){
+                                        Integer fromflag,Integer recipeId,Integer enterpriseId,Integer checkStatus,Integer payFlag,Integer orderType){
         RecipeDAO recipeDAO = DAOFactory.getDAO(RecipeDAO.class);
-        return recipeDAO.findRecipesByInfoForExcel(organId,status,doctor,patientName,bDate,eDate,dateType,depart,organIds,giveMode,fromflag,recipeId,enterpriseId,checkStatus,payFlag);
+        return recipeDAO.findRecipesByInfoForExcel(organId,status,doctor,patientName,bDate,eDate,dateType,depart,organIds,giveMode,fromflag,recipeId,enterpriseId,checkStatus,payFlag,orderType);
     }
 
     /**
