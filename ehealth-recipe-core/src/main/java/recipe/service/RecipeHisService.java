a2b13f08adfad7596be8aa306c2a9e18668a4bb7
wuzhentao <1228764596@qq,com>
2020-05-12 13:52:41 +0800

    出参打印

--- ehealth-recipe-core/src/main/java/recipe/service/RecipeHisService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/RecipeHisService.java
@@ -1345,7 +1345,7 @@ public class RecipeHisService extends RecipeBaseService {
         LOGGER.info("调用杭州逸曜合理用药queryHzyyRationalUserDurg,入参 = {}，idNO = {}",JSONUtils.toString(reqTO), reqTO.getPatient().getIdNo());
         IRecipeHisService iRecipeHisService = AppContextHolder.getBean("his.iRecipeHisService", IRecipeHisService.class);
         HisResponseTO<List<HzyyRationalUseDrugResTO>> hisResponseTO = iRecipeHisService.queryHzyyRationalUserDurg(reqTO);
-        LOGGER.info("调用杭州逸曜合理用药queryHzyyRationalUserDurg,出参 = {}, idNO = {}", JSONUtils.toString(reqTO), reqTO.getPatient().getIdNo());
+        LOGGER.info("调用杭州逸曜合理用药queryHzyyRationalUserDurg,出参 = {}, idNO = {}", JSONUtils.toString(hisResponseTO), reqTO.getPatient().getIdNo());
         if(hisResponseTO == null || !hisResponseTO.getMsgCode().equals("200")){
             return Collections.EMPTY_LIST;
         }
