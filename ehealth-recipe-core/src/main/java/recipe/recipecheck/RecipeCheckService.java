15fa82dd7718e67ea125be31c1157d82322692bd
隋晓宇 <xy18043124001@gmail.com>
2020-05-12 09:51:09 +0800

    身份证脱敏

--- ehealth-recipe-core/src/main/java/recipe/recipecheck/RecipeCheckService.java
+++ ehealth-recipe-core/src/main/java/recipe/recipecheck/RecipeCheckService.java
@@ -614,7 +614,7 @@ public class RecipeCheckService {
         if (null != apothecaryId && !apothecaryId.equals(0)) {
             DoctorDTO doctorDTO = doctorService.get(apothecaryId);
             if (null != doctorDTO) {
-                apothecaryVO.setCheckApothecaryIdCard(doctorDTO.getIdNumber());
+                apothecaryVO.setCheckApothecaryIdCard(hideIdCard(doctorDTO.getIdNumber()));
                 apothecaryVO.setCheckApothecaryName(doctorDTO.getName());
             }
         }
@@ -871,12 +871,16 @@ public class RecipeCheckService {
         if (StringUtils.isEmpty(idCard)) {
             return "";
         }
-        //显示前1-3位
-        String str1 = idCard.substring(0, 3);
-        //显示后15-18位
-        String str2 = idCard.substring(14, 18);
-        idCard = str1 + "***********" + str2;
-        return idCard;
+        try {
+            //显示前1-3位
+            String str1 = idCard.substring(0, 3);
+            //显示后15-18位
+            String str2 = idCard.substring(14, 18);
+            idCard = str1 + "***********" + str2;
+            return idCard;
+        } catch (Exception e) {
+            return "";
+        }
     }
 
     /**
