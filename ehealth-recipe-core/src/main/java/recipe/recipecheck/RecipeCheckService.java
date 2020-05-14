6027a1cdf4e9778424359a279e04f473c24c03b5
shiyuping <shiyp@ngarihealth.com>
2020-05-14 13:51:59 +0800

    bug#43146 【处方】“审方签名失败”的处方单，药师进入该处方单的审核页，底部按钮问题

--- ehealth-recipe-core/src/main/java/recipe/recipecheck/RecipeCheckService.java
+++ ehealth-recipe-core/src/main/java/recipe/recipecheck/RecipeCheckService.java
@@ -526,7 +526,8 @@ public class RecipeCheckService {
         }
         //药师能否撤销标识
         Boolean cancelRecipeFlag = false;
-        if (checkerId != null && checkerId.equals(recipe.getChecker())){
+        //只有审核通过的才有标识
+        if (checkerId != null && checkerId.equals(recipe.getChecker()) && new Integer(1).equals(checkResult)){
             cancelRecipeFlag = true;
         }
         map.put("cancelRecipeFlag", cancelRecipeFlag);
