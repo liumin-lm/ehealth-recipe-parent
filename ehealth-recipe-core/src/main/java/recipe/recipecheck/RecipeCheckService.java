1cbd77bdedf6dcd349fbc1336d48c7d308f49198
jiangrk <jiangrk@ngarihealth.com>
2020-05-14 17:22:45 +0800

    BUG# 药师签名失败按钮

--- ehealth-recipe-core/src/main/java/recipe/recipecheck/RecipeCheckService.java
+++ ehealth-recipe-core/src/main/java/recipe/recipecheck/RecipeCheckService.java
@@ -1276,7 +1276,8 @@ public class RecipeCheckService {
         String recipeS = null;
         try {
             RecipeCheck recipeCheck = recipeCheckDAO.getByRecipeIdAndCheckStatus(recipeId);
-            if (null == recipeCheck || recipeCheck.getGrabOrderStatus().equals(GrabOrderStatusConstant.GRAB_ORDER_NO)) {
+            List<RecipeCheck> recipeCheckeds = recipeCheckDAO.findByRecipeId(recipeId);
+            if ((null == recipeCheck && CollectionUtils.isEmpty(recipeCheckeds)) || recipeCheck.getGrabOrderStatus().equals(GrabOrderStatusConstant.GRAB_ORDER_NO)) {
                 resultMap.put("grabOrderStatus", GrabOrderStatusConstant.GRAB_ORDER_NO);
                 resultMap.put("lockLimitTime", 10); //未抢单默认返回10
                 return resultMap;
