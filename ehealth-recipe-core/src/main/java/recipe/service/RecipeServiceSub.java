363b676fbfbd9c18a07ece1841898f3b16754cdc
jiangrk <jiangrk@ngarihealth.com>
2020-05-14 10:16:34 +0800

    BUG# 医生端撤销不展示签名状态

--- ehealth-recipe-core/src/main/java/recipe/service/RecipeServiceSub.java
+++ ehealth-recipe-core/src/main/java/recipe/service/RecipeServiceSub.java
@@ -1341,6 +1341,12 @@ public class RecipeServiceSub {
                         && recipe.getStatus() != RecipeStatusConstant.RECIPE_MEDICAL_FAIL
                         && recipe.getStatus() != RecipeStatusConstant.CHECKING_HOS
                         && recipe.getStatus() != RecipeStatusConstant.NO_MEDICAL_INSURANCE_RETURN
+                        //date 2020/05/14
+                        //将签名失败和审核失败的
+                        && recipe.getStatus() != RecipeStatusConstant.SIGN_ERROR_CODE_PHA
+                        && recipe.getStatus() != RecipeStatusConstant.SIGN_ERROR_CODE_DOC
+                        && recipe.getStatus() != RecipeStatusConstant.SIGN_ING_CODE_DOC
+                        && recipe.getStatus() != RecipeStatusConstant.SIGN_ING_CODE_PHA
                         && !Integer.valueOf(1).equals(recipe.getChooseFlag())) {
                     cancelFlag = true;
                 }
