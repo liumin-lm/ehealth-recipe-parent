91b3db2c75645c9d8ab3a91b2771ce96a44bdb8b
shiyuping <shiyp@ngarihealth.com>
2020-06-01 11:10:53 +0800

    update

--- ehealth-recipe-core/src/main/java/recipe/service/RecipeOrderService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/RecipeOrderService.java
@@ -1946,8 +1946,8 @@ public class RecipeOrderService extends RecipeBaseService {
         }
 
         //配送费
-        //有配送费并且配送费支付方式为在线支付时才计入支付金额
-        if (null != order.getExpressFee() && !(new Integer(2).equals(order.getExpressFeePayWay()))) {
+        //有配送费并且配送费支付方式为不等于第三方支付时才计入支付金额
+        if (null != order.getExpressFee() && !(new Integer(3).equals(order.getExpressFeePayWay()))) {
             full = full.add(order.getExpressFee());
         }
 
