2c827b866ab6abf7a374cde2d488429f8a37ed40
yinsheng <yins@ngarihealth.com>
2020-06-03 21:43:19 +0800

    同步代码

--- ehealth-recipe-api/src/main/java/com/ngari/recipe/drugsenterprise/model/ThirdResultBean.java
+++ ehealth-recipe-api/src/main/java/com/ngari/recipe/drugsenterprise/model/ThirdResultBean.java
@@ -1,12 +1,6 @@
 package com.ngari.recipe.drugsenterprise.model;
 
-import com.ngari.recipe.common.RecipeResultBean;
-
-public class ThirdResultBean extends RecipeResultBean {
-
-    public static final Integer SUCCESS = 1;
-
-    public static final Integer FAIL = 0;
+public class ThirdResultBean {
 
     private Integer code;
 
@@ -16,42 +10,34 @@ public class ThirdResultBean extends RecipeResultBean {
 
     private String error;
 
-    @Override
     public Integer getCode() {
         return code;
     }
 
-    @Override
     public void setCode(Integer code) {
         this.code = code;
     }
 
-    @Override
     public String getMsg() {
         return msg;
     }
 
-    @Override
     public void setMsg(String msg) {
         this.msg = msg;
     }
 
-    @Override
     public Integer getBusId() {
         return busId;
     }
 
-    @Override
     public void setBusId(Integer busId) {
         this.busId = busId;
     }
 
-    @Override
     public String getError() {
         return error;
     }
 
-    @Override
     public void setError(String error) {
         this.error = error;
     }
