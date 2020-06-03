63d3b1d69f22e2db793e4c015e91dec37dc2eef6
zuoxiaoxi <zuoxx@ngarihealth.com>
2020-06-03 19:18:39 +0800

    bug#44560 【运营平台】机构添加药品日志不正确

--- ehealth-recipe-core/src/main/java/recipe/service/OrganDrugListService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/OrganDrugListService.java
@@ -289,7 +289,7 @@ public class OrganDrugListService implements IOrganDrugListService {
             req.setNotifyTime(System.currentTimeMillis()-1000);
             req.setOrganId(saveOrganDrugList.getOrganId());
             iRegulationService.notifyData(saveOrganDrugList.getOrganId(),req);
-            busActionLogService.recordBusinessLogRpcNew("机构药品管理","","OrganDrugList","【"+organDTO.getMinkeUnitCretditCode()+"】新增药品【"+saveOrganDrugList.getOrganDrugId()+"-"+saveOrganDrugList.getDrugName()+"】",organDTO.getName());
+            busActionLogService.recordBusinessLogRpcNew("机构药品管理","","OrganDrugList","【"+organDTO.getName()+"】新增药品【"+saveOrganDrugList.getOrganDrugId()+"-"+saveOrganDrugList.getDrugName()+"】",organDTO.getName());
             return ObjectCopyUtils.convert(saveOrganDrugList, OrganDrugListDTO.class);
         } else {
             logger.info("修改机构药品服务[updateOrganDrugList]:" + JSONUtils.toString(organDrugList));
