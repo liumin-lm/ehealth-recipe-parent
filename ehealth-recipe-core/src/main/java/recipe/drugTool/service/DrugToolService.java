a76bd7637153b0a9324a5fc9685fbb2c00424318
shiyuping <shiyp@ngarihealth.com>
2020-06-15 10:10:41 +0800

    task#60727 【后台】医生APP端的用药频次与用药途径，将没有进行对照的过滤掉，前端页面不展示 / 平台新接口改造

--- ehealth-recipe-core/src/main/java/recipe/drugTool/service/DrugToolService.java
+++ ehealth-recipe-core/src/main/java/recipe/drugTool/service/DrugToolService.java
@@ -1078,7 +1078,7 @@ public class DrugToolService implements IDrugToolService {
      * 获取用药频率和用药途径--新
      */
     @RpcService
-    public Map<String, Object> findUsingRateAndUsePathwayWithOutKey(Integer organId) {
+    public Map<String, Object> findUsingRateAndUsePathwayNew(Integer organId) {
         Map<String, Object> result = Maps.newHashMap();
         IUsingRateService usingRateService = BaseAPI.getService(IUsingRateService.class);
         IUsePathwaysService usePathwaysService = BaseAPI.getService(IUsePathwaysService.class);
