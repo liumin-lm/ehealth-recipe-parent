8e7df589d8863d59176b0ac7ce8564deae1cb46a
隋晓宇 <xy18043124001@gmail.com>
2020-06-29 14:19:40 +0800

    修改异常捕获

--- ehealth-recipe-core/src/main/java/recipe/thread/UpdateTotalRecipePdfRunable.java
+++ ehealth-recipe-core/src/main/java/recipe/thread/UpdateTotalRecipePdfRunable.java
@@ -1,7 +1,6 @@
 package recipe.thread;
 
 import com.google.common.collect.ImmutableMap;
-import com.itextpdf.text.DocumentException;
 import com.ngari.recipe.entity.Recipe;
 import ctd.persistence.DAOFactory;
 import org.apache.commons.lang3.StringUtils;
@@ -10,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import recipe.bussutil.CreateRecipePdfUtil;
 import recipe.dao.RecipeDAO;
 
-import java.io.IOException;
 import java.math.BigDecimal;
 
 /**
@@ -64,7 +62,7 @@ public class UpdateTotalRecipePdfRunable implements Runnable {
             if (StringUtils.isNotEmpty(newPfd) && StringUtils.isNotEmpty(key)) {
                 recipeDAO.updateRecipeInfoByRecipeId(recipeId, ImmutableMap.of(key, newPfd));
             }
-        } catch (DocumentException | IOException e) {
+        } catch (Exception e) {
             logger.error("UpdateTotalRecipePdfRunable error recipeId={},e=", recipeId, e);
         }
     }
