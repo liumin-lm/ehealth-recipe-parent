94636bb4b662dd4a396f01b412e9da71000d1b89
刘敏 <liu_m@winning.com.cn>
2020-07-01 14:08:24 +0800

    task #46135 【上海市胸科医院】【实施】【A】添加药品时，选择剂量单位时有误

--- ehealth-recipe-core/src/main/java/recipe/service/CommonRecipeService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/CommonRecipeService.java
@@ -250,9 +250,11 @@ public class CommonRecipeService extends BaseService<CommonRecipeDTO> {
                             }
                             //设置医生端每次剂量和剂量单位联动关系
                             useDoseAndUnitRelationList = Lists.newArrayList();
-                            useDoseAndUnitRelationList.add(new UseDoseAndUnitRelationBean(organDrug.getRecommendedUseDose(),organDrug.getUseDoseUnit(),organDrug.getUseDose()));
-                            if (StringUtils.isNotEmpty(organDrug.getUseDoseSmallestUnit())
-                                    ||organDrug.getDefaultSmallestUnitUseDose()!= null){
+                            //用药单位不为空时才返回给前端
+                            if (StringUtils.isNotEmpty(organDrug.getUseDoseUnit())){
+                                useDoseAndUnitRelationList.add(new UseDoseAndUnitRelationBean(organDrug.getRecommendedUseDose(),organDrug.getUseDoseUnit(),organDrug.getUseDose()));
+                            }
+                            if (StringUtils.isNotEmpty(organDrug.getUseDoseSmallestUnit())){
                                 useDoseAndUnitRelationList.add(new UseDoseAndUnitRelationBean(organDrug.getDefaultSmallestUnitUseDose(),organDrug.getUseDoseSmallestUnit(),organDrug.getSmallestUnitUseDose()));
                             }
                             try {
@@ -295,9 +297,11 @@ public class CommonRecipeService extends BaseService<CommonRecipeDTO> {
                             }
                             //设置医生端每次剂量和剂量单位联动关系
                             useDoseAndUnitRelationList = Lists.newArrayList();
-                            useDoseAndUnitRelationList.add(new UseDoseAndUnitRelationBean(organDrug.getRecommendedUseDose(),organDrug.getUseDoseUnit(),organDrug.getUseDose()));
-                            if (StringUtils.isNotEmpty(organDrug.getUseDoseSmallestUnit())
-                                    ||organDrug.getDefaultSmallestUnitUseDose()!= null){
+                            //用药单位不为空时才返回给前端
+                            if (StringUtils.isNotEmpty(organDrug.getUseDoseUnit())){
+                                useDoseAndUnitRelationList.add(new UseDoseAndUnitRelationBean(organDrug.getRecommendedUseDose(),organDrug.getUseDoseUnit(),organDrug.getUseDose()));
+                            }
+                            if (StringUtils.isNotEmpty(organDrug.getUseDoseSmallestUnit())){
                                 useDoseAndUnitRelationList.add(new UseDoseAndUnitRelationBean(organDrug.getDefaultSmallestUnitUseDose(),organDrug.getUseDoseSmallestUnit(),organDrug.getSmallestUnitUseDose()));
                             }
                             commonRecipeDrug.setUseDoseAndUnitRelation(useDoseAndUnitRelationList);
