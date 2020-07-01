c8995d85afc5164f09108aa5263ad28a6fa17c1f
shiyuping <shiyp@ngarihealth.com>
2020-07-01 14:08:37 +0800

    task# 新增处方报表服务

---
+++ ehealth-recipe-core/src/main/java/recipe/service/recipereportforms/RecipeReportFormsService.java
@@ -0,0 +1,11 @@
+package recipe.service.recipereportforms;
+
+import ctd.util.annotation.RpcBean;
+
+/**
+ * created by shiyuping on 2020/7/1
+ * 处方报表服务
+ */
+@RpcBean("recipeReportFormsService")
+public class RecipeReportFormsService {
+}
