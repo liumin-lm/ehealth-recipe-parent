d6c3c0360dede490a829004e1beed6e19c8379c4
yuanb <yuanb@ngarihealth.com>
2020-07-10 10:11:16 +0800

    Merge remote-tracking branch 'remotes/origin/toC-hotfix-20200629'
    # Conflicts:
    #	ehealth-recipe-api/pom.xml
    #	ehealth-recipe-audit/pom.xml
    #	ehealth-recipe-common/pom.xml
    #	ehealth-recipe-core/pom.xml
    #	ehealth-recipe-dao/pom.xml
    #	ehealth-recipe-entity/pom.xml
    #	ehealth-recipe-prescription/pom.xml
    #	ehealth-recipe-purchase/pom.xml
    #	pom.xml

--- ehealth-recipe-core/src/main/java/recipe/service/RecipeServiceSub.java
+++ ehealth-recipe-core/src/main/java/recipe/service/RecipeServiceSub.java
@@ -70,7 +70,6 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.sql.Timestamp;
 import java.text.DecimalFormat;
-import java.util.*;
 import java.util.stream.Collectors;
 
 /**
