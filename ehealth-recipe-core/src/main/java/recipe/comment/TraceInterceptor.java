c81c2ced1baebbb2c13e790fe0483b693a4c3159
隋晓宇 <xy18043124001@gmail.com>
2020-07-16 13:41:05 +0800

    增加traceId

--- ehealth-recipe-core/src/main/java/recipe/comment/TraceInterceptor.java
+++ ehealth-recipe-core/src/main/java/recipe/comment/TraceInterceptor.java
@@ -2,6 +2,7 @@ package recipe.comment;
 
 
 import org.slf4j.MDC;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,11 +13,17 @@ import java.util.UUID;
  * @author fuzi
  */
 public class TraceInterceptor extends HandlerInterceptorAdapter {
+    private static final String THREAD_ID = "traceId";
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         // "traceId"
-        MDC.put("traceId", UUID.randomUUID().toString());
+        MDC.put(THREAD_ID, UUID.randomUUID().toString());
         return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
+        MDC.clear();
     }
 }
