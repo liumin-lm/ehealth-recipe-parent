abfeefcf0d3e52207c40b7f8fa96475fcefd7dfe
yinsheng <yins@ngarihealth.com>
2020-07-27 15:46:08 +0800

    TASK#67906 子 【后台】【实施】【北京市互联网医院】处方流转个性化流程 / 根据HIS返回配送地址的个性化处理

--- ehealth-recipe-core/src/main/java/recipe/service/RecipeOrderService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/RecipeOrderService.java
@@ -710,7 +710,7 @@ public class RecipeOrderService extends RecipeBaseService {
                     if (hisRecipe != null && StringUtils.isNotEmpty(hisRecipe.getSendAddr())) {
                         order.setReceiver(hisRecipe.getReceiverName());
                         order.setRecMobile(hisRecipe.getReceiverTel());
-                        order.setAddressCanSend(true);
+                        order.setAddressCanSend(false);
                         order.setAddress4(hisRecipe.getSendAddr());
                     }
                 }
