2fbf4e606ef531c10ad640ca6424150a32963b6b
xy <xy18043124001@gmail.com>
2020-08-04 15:50:59 +0800

    task# 69113【昆明医科大学第一附属医院】【A级】昆明医科大学第一附属医院电子发票接入

--- ehealth-recipe-core/src/main/java/recipe/hisservice/EleInvoiceService.java
+++ ehealth-recipe-core/src/main/java/recipe/hisservice/EleInvoiceService.java
@@ -7,10 +7,13 @@ import com.ngari.consult.common.service.IConsultExService;
 import com.ngari.his.recipe.mode.EleInvoiceReqTo;
 import com.ngari.his.recipe.service.IRecipeHisService;
 import com.ngari.patient.dto.PatientDTO;
-import com.ngari.patient.service.BasicAPI;
 import com.ngari.patient.service.PatientService;
+import com.ngari.patient.utils.ObjectCopyUtils;
+import com.ngari.platform.recipe.mode.*;
+import com.ngari.recipe.entity.Recipe;
 import com.ngari.recipe.entity.RecipeExtend;
-import ctd.persistence.DAOFactory;
+import com.ngari.recipe.entity.RecipeOrder;
+import com.ngari.recipe.entity.Recipedetail;
 import ctd.persistence.exception.DAOException;
 import ctd.spring.AppDomainContext;
 import ctd.util.JSONUtils;
@@ -19,12 +22,16 @@ import ctd.util.annotation.RpcService;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import recipe.ApplicationUtils;
 import recipe.bean.EleInvoiceDTO;
+import recipe.dao.RecipeDAO;
+import recipe.dao.RecipeDetailDAO;
 import recipe.dao.RecipeExtendDAO;
+import recipe.dao.RecipeOrderDAO;
 import recipe.util.DateConversion;
 
-import java.util.ArrayList;
+import javax.annotation.Resource;
 import java.util.Arrays;
 import java.util.List;
 
@@ -39,55 +46,56 @@ import java.util.List;
 public class EleInvoiceService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(EleInvoiceService.class);
-
+    @Autowired
+    private PatientService patientService;
+    @Autowired
+    private RecipeDAO recipeDAO;
+    @Autowired
+    private RecipeExtendDAO recipeExtendDAO;
+    @Resource
+    private RecipeOrderDAO recipeOrderDAO;
+    @Autowired
+    private RecipeDetailDAO recipeDetailDAO;
 
     @RpcService
-    public List<String> findEleInvoice(EleInvoiceDTO eleInvoiceDTO){
-        LOGGER.info("EleInvoiceService.findEleInvoice 入参eleInvoiceDTO=[{}]",JSONUtils.toString(eleInvoiceDTO));
+    public List<String> findEleInvoice(EleInvoiceDTO eleInvoiceDTO) {
+        LOGGER.info("EleInvoiceService.findEleInvoice 入参eleInvoiceDTO=[{}]", JSONUtils.toString(eleInvoiceDTO));
         validateParam(eleInvoiceDTO);
-        if("0".equals(eleInvoiceDTO.getType())){
+        PatientDTO patientDTO = patientService.get(eleInvoiceDTO.getMpiid());
+
+        if ("0".equals(eleInvoiceDTO.getType())) {
             IConsultExService iConsultExService = AppDomainContext.getBean("consult.consultExService", IConsultExService.class);
             ConsultExDTO consultExDTO = iConsultExService.getByConsultId(eleInvoiceDTO.getId());
-            if(StringUtils.isNotBlank(consultExDTO.getRegisterNo())){
+            if (StringUtils.isNotBlank(consultExDTO.getRegisterNo())) {
                 eleInvoiceDTO.setGhxh(consultExDTO.getRegisterNo());
             }
-            if(StringUtils.isNotBlank(consultExDTO.getCardId())){
+            if (StringUtils.isNotBlank(consultExDTO.getCardId())) {
                 eleInvoiceDTO.setCardId(consultExDTO.getCardId());
             }
-            if(StringUtils.isNotBlank(consultExDTO.getCardType())){
+            if (StringUtils.isNotBlank(consultExDTO.getCardType())) {
                 eleInvoiceDTO.setCardType(consultExDTO.getCardType());
             }
-
         }
-        if("1".equals(eleInvoiceDTO.getType())){
-            RecipeExtendDAO recipeExtendDAO = DAOFactory.getDAO(RecipeExtendDAO.class);
-            RecipeExtend recipeExtend = recipeExtendDAO.getByRecipeId(eleInvoiceDTO.getId());
-            if(StringUtils.isNotBlank(recipeExtend.getRegisterID())){
-                eleInvoiceDTO.setGhxh(recipeExtend.getRegisterID());
-            }
-            if(StringUtils.isNotBlank(recipeExtend.getCardType())){
-                eleInvoiceDTO.setCardType(recipeExtend.getCardType());
-            }
-            if(StringUtils.isNotBlank(recipeExtend.getCardNo())){
-                eleInvoiceDTO.setCardId(recipeExtend.getCardNo());
-            }
-        }
-
-        PatientService patientService = BasicAPI.getService(PatientService.class);
-        PatientDTO patientDTO = patientService.get(eleInvoiceDTO.getMpiid());
         EleInvoiceReqTo eleInvoiceReqTo = new EleInvoiceReqTo();
+        //处方数据
+        if ("1".equals(eleInvoiceDTO.getType())) {
+            RecipeDTO recipeDTO = setRecipeDTO(eleInvoiceDTO);
+            recipeDTO.setPatientDTO(patientDTO);
+            eleInvoiceReqTo.setRecipeDTO(recipeDTO);
+        }
+
         eleInvoiceReqTo.setOrganId(eleInvoiceDTO.getOrganId());
-        if(StringUtils.isNotBlank(patientDTO.getPatientName())){
+        if (StringUtils.isNotBlank(patientDTO.getPatientName())) {
             eleInvoiceReqTo.setHzxm(patientDTO.getPatientName());
         }
-        if(StringUtils.isNotBlank(patientDTO.getGuardianCertificate())){
+        if (StringUtils.isNotBlank(patientDTO.getGuardianCertificate())) {
             eleInvoiceReqTo.setLxrzjh(patientDTO.getGuardianCertificate());
         }
-        if(StringUtils.isNotBlank(patientDTO.getMobile())){
+        if (StringUtils.isNotBlank(patientDTO.getMobile())) {
             eleInvoiceReqTo.setLxrdh(patientDTO.getMobile());
             eleInvoiceReqTo.setPhone(patientDTO.getMobile());
         }
-        if(StringUtils.isNotBlank(patientDTO.getCertificate())){
+        if (StringUtils.isNotBlank(patientDTO.getCertificate())) {
             eleInvoiceReqTo.setSfzh(patientDTO.getCertificate());
         }
         eleInvoiceReqTo.setBlh(null);
@@ -101,92 +109,112 @@ public class EleInvoiceService {
 
         eleInvoiceReqTo.setKsrq(DateConversion.getPastDate(7));
         eleInvoiceReqTo.setJsrq(DateConversion.getToDayDate());
-        if("0".equals(eleInvoiceDTO.getType())){
+        if ("0".equals(eleInvoiceDTO.getType())) {
             eleInvoiceReqTo.setCzybz("0");
         }
-        if("1".equals(eleInvoiceDTO.getType())){
+        if ("1".equals(eleInvoiceDTO.getType())) {
             eleInvoiceReqTo.setCzybz(null);
         }
-        if(StringUtils.isNotBlank(eleInvoiceDTO.getGhxh())){
+        if (StringUtils.isNotBlank(eleInvoiceDTO.getGhxh())) {
             eleInvoiceReqTo.setGhxh(eleInvoiceDTO.getGhxh());
-        }else{
-            throw new DAOException(609,"ghxh is null,无法获取对应电子发票");
+        } else {
+            throw new DAOException(609, "ghxh is null,无法获取对应电子发票");
         }
         IRecipeHisService hisService = AppDomainContext.getBean("his.iRecipeHisService", IRecipeHisService.class);
-        HisResponseTO<String> hisResponseTO = null;
-
         LOGGER.info("EleInvoiceService.findEleInvoice 待推送数据:eleInvoiceReqTo:[{}]", JSONUtils.toString(eleInvoiceReqTo));
-        hisResponseTO = hisService.queryEleInvoice(eleInvoiceReqTo);
-        if(hisResponseTO != null){
-            if(hisResponseTO.getMsgCode().equals("200")){
-                String result = hisResponseTO.getData();
-                if(StringUtils.isNotBlank(result)){
-                    LOGGER.info("EleInvoiceService.findEleInvoice :result={}",result);
-                   return stringToList(result);
-                }else{
-                    throw new DAOException(609,"当前系统繁忙，请稍后再试");
-                }
-            }else{
-                LOGGER.info("EleInvoiceService.findEleInvoice 请求his失败，返回信息:msg={}",hisResponseTO.getMsg());
-                throw new DAOException(609,hisResponseTO.getMsg());
-            }
-        }else{
-            LOGGER.info("EleInvoiceService.findEleInvoice 请求his失败,hisResponseTo is null");
-            throw new DAOException(609,"当前系统繁忙，请稍后再试");
-        }
+        HisResponseTO<String> hisResponse = hisService.queryEleInvoice(eleInvoiceReqTo);
+        return stringToList(hisResponse);
+
 
     }
 
-    private void validateParam(EleInvoiceDTO eleInvoiceDTO){
-        if(eleInvoiceDTO.getId() == null){
-            throw new DAOException(609,"id is null");
+    private void validateParam(EleInvoiceDTO eleInvoiceDTO) {
+        if (eleInvoiceDTO.getId() == null) {
+            throw new DAOException(609, "id is null");
         }
-        if(StringUtils.isBlank(eleInvoiceDTO.getMpiid())){
-            throw new DAOException(609,"mpiid is null");
+        if (StringUtils.isBlank(eleInvoiceDTO.getMpiid())) {
+            throw new DAOException(609, "mpiid is null");
         }
-        if(eleInvoiceDTO.getOrganId() == null){
-            throw new DAOException(609,"organId is null");
+        if (eleInvoiceDTO.getOrganId() == null) {
+            throw new DAOException(609, "organId is null");
         }
-        if(StringUtils.isBlank(eleInvoiceDTO.getType())){
-            throw new DAOException(609,"type is null");
+        if (StringUtils.isBlank(eleInvoiceDTO.getType())) {
+            throw new DAOException(609, "type is null");
         }
+    }
+
+    /**
+     * 处方发票号入参
+     * 组织处方相关数据
+     *
+     * @param eleInvoiceDTO
+     * @param
+     */
+    private RecipeDTO setRecipeDTO(EleInvoiceDTO eleInvoiceDTO) {
+        RecipeDTO recipeDTO = new RecipeDTO();
+
+
+        Integer recipeId = eleInvoiceDTO.getId();
+        RecipeExtend recipeExtend = recipeExtendDAO.getByRecipeId(recipeId);
+        if (StringUtils.isNotBlank(recipeExtend.getRegisterID())) {
+            eleInvoiceDTO.setGhxh(recipeExtend.getRegisterID());
+        }
+        if (StringUtils.isNotBlank(recipeExtend.getCardType())) {
+            eleInvoiceDTO.setCardType(recipeExtend.getCardType());
+        }
+        if (StringUtils.isNotBlank(recipeExtend.getCardNo())) {
+            eleInvoiceDTO.setCardId(recipeExtend.getCardNo());
+        }
+        RecipeExtendBean recipeExtendBean = ObjectCopyUtils.convert(recipeExtend, RecipeExtendBean.class);
+        recipeDTO.setRecipeExtendBean(recipeExtendBean);
+        Recipe recipe = recipeDAO.getByRecipeId(recipeId);
+        RecipeBean recipeBean = ObjectCopyUtils.convert(recipe, RecipeBean.class);
+        recipeDTO.setRecipeBean(recipeBean);
+        RecipeOrder recipeOrder = recipeOrderDAO.getOrderByRecipeId(recipeId);
+        RecipeOrderBean recipeOrderBean = ObjectCopyUtils.convert(recipeOrder, RecipeOrderBean.class);
+        recipeDTO.setRecipeOrderBean(recipeOrderBean);
+        List<Recipedetail> recipeDetailList = recipeDetailDAO.findByRecipeId(recipeId);
+        List<RecipeDetailBean> recipeDetails = ObjectCopyUtils.convert(recipeDetailList, RecipeDetailBean.class);
+        recipeDTO.setRecipeDetails(recipeDetails);
+        return recipeDTO;
     }
 
     @RpcService
-    public String getEleInvoiceEnable(Integer organId,String type){
-        if(organId == null){
-            throw new DAOException(609,"organId is null");
+    public String getEleInvoiceEnable(Integer organId, String type) {
+        if (organId == null) {
+            throw new DAOException(609, "organId is null");
         }
-        if(StringUtils.isBlank(type)){
-            throw new DAOException(609,"type is null");
+        if (StringUtils.isBlank(type)) {
+            throw new DAOException(609, "type is null");
         }
         IConfigurationCenterUtilsService configurationCenterUtils = ApplicationUtils.getBaseService(IConfigurationCenterUtilsService.class);
         String result = "";
-        if("0".equals(type)){
-            result = (String)configurationCenterUtils.getConfiguration(organId, "EleInvoiceFzSwitchNew");
+        if ("0".equals(type)) {
+            result = (String) configurationCenterUtils.getConfiguration(organId, "EleInvoiceFzSwitchNew");
         }
-        if("1".equals(type)){
-            result = (String)configurationCenterUtils.getConfiguration(organId, "EleInvoiceCfSwitch");
+        if ("1".equals(type)) {
+            result = (String) configurationCenterUtils.getConfiguration(organId, "EleInvoiceCfSwitch");
         }
-        if(StringUtils.isBlank(result)){
-           result = "0";
+        if (StringUtils.isBlank(result)) {
+            result = "0";
         }
         return result;
     }
 
-    private List<String> stringToList(String str){
-        List<String> result2 = new ArrayList<String>();
-        if(StringUtils.isNotBlank(str)){
-            Arrays.asList(str.split(","));
-            Arrays.asList(StringUtils.split(str, ","));
-            String[] strings = str.split(",");
-            for (String string : strings) {
-                result2.add(string);
-            }
+    private List<String> stringToList(HisResponseTO<String> hisResponse) {
+        LOGGER.info("EleInvoiceService.stringToList  hisResponseTO={}", JSONUtils.toString(hisResponse));
+        if (null == hisResponse) {
+            LOGGER.info("EleInvoiceService.findEleInvoice 请求his失败,hisResponseTo is null");
+            throw new DAOException(609, "当前系统繁忙，请稍后再试");
         }
-        return  result2;
+        if (!"200".equals(hisResponse.getMsgCode())) {
+            LOGGER.info("EleInvoiceService.findEleInvoice 请求his失败，返回信息:msg={}", hisResponse.getMsg());
+            throw new DAOException(609, hisResponse.getMsg());
+        }
+        String result = hisResponse.getData();
+        if (StringUtils.isBlank(result)) {
+            throw new DAOException(609, "当前系统繁忙，请稍后再试");
+        }
+        return Arrays.asList(result.split(","));
     }
-
-
-
 }
