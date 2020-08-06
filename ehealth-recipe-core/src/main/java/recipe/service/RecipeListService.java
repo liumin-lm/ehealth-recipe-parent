65c764278da47b82f0acd1824fd6700244e1b885
刘敏 <liu_m@winning.com.cn>
2020-08-06 09:22:08 +0800

    task #44711/44709/长春中医药新增字段推送给his

--- ehealth-recipe-core/src/main/java/recipe/service/RecipeListService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/RecipeListService.java
@@ -1122,6 +1122,8 @@ public class RecipeListService extends RecipeBaseService{
             //如果运营平台-配置管理 中药是否隐方的配置项, 选择隐方后,患者在支付成功处方费用后才可以显示中药明细，否则就隐藏掉对应的中药明细。
             IConfigurationCenterUtilsService configService = BaseAPI.getService(IConfigurationCenterUtilsService.class);
             Object isHiddenRecipeDetail = configService.getConfiguration(organId, "isHiddenRecipeDetail");
+            LOGGER.info("isReturnRecipeDetail 是否是中药：{} 是否隐方：{} 支付状态是否为已支付（1）：{} "
+                    ,RecipeUtil.isTcmType(recipeType),(boolean)isHiddenRecipeDetail,payFlag);
             if (RecipeUtil.isTcmType(recipeType)//中药
                     &&(boolean)isHiddenRecipeDetail==true//隐方
                     &&(PayConstant.PAY_FLAG_PAY_SUCCESS != payFlag) //支付状态为非已支付
@@ -1131,6 +1133,7 @@ public class RecipeListService extends RecipeBaseService{
         }catch (Exception e){
             LOGGER.error("isReturnRecipeDetail error:{}",e);
         }
+
         return isReturnRecipeDetail;
     }
     /**
