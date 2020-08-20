0dec897225a5ea051131bba084e037b56b323452
xy <xy18043124001@gmail.com>
2020-08-20 14:54:00 +0800

    task# 71464 【后台】【重庆医科大学附属第二医院】【A级】医生端开具处方可以根据线下历史处方复制生成新处方

--- ehealth-recipe-core/src/main/java/recipe/service/RecipeService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/RecipeService.java
@@ -780,6 +780,10 @@ public class RecipeService extends RecipeBaseService {
             }
             // 添加机构id
             dataMap.put("organId", recipe.getClinicOrgan());
+            Object footerRemark = configService.getConfiguration(recipe.getClinicOrgan(), "recipeDetailRemark");
+            if (null != footerRemark) {
+                dataMap.put("footerRemark", footerRemark.toString());
+            }
             Map<String, Object> backMap = esignService.signForRecipe(false, checker, dataMap);
             LOGGER.info("reviewRecipe  esignService backMap:{} ,e=============", JSONUtils.toString(backMap));
             //0表示成功
@@ -1286,6 +1290,10 @@ public class RecipeService extends RecipeBaseService {
         }
         //上传阿里云
         String memo = "";
+        Object footerRemark = configService.getConfiguration(recipe.getClinicOrgan(), "recipeDetailRemark");
+        if (null != footerRemark) {
+            paramMap.put("footerRemark", footerRemark.toString());
+        }
         Map<String, Object> backMap = esignService.signForRecipe(true, recipe.getDoctor(), paramMap);
         String imgFileId = MapValueUtil.getString(backMap, "imgFileId");
         Map<String, Object> attrMapimg = Maps.newHashMap();
