db4aba1901a7761e840a13ca38cba0f5db1cf701
刘敏 <liu_m@winning.com.cn>
2020-08-21 11:19:25 +0800

    task #P0-39515[电子处方]患者端处方确认页面\订单详情页优化 药店取药方式返回线上/线下支付----【代码还原】

--- ehealth-recipe-entity/src/main/java/com/ngari/recipe/entity/RecipeOrder.java
+++ ehealth-recipe-entity/src/main/java/com/ngari/recipe/entity/RecipeOrder.java
@@ -301,8 +301,6 @@ public class RecipeOrder implements Serializable {
     @ItemProperty(alias = "支付用户类型:0平台，1机构，2药企")
     private Integer payeeCode;
 
-    @ItemProperty(alias = "0:不支付药品费用，1:全部支付 【 1线上支付  非1就是线下支付】")
-    private Integer storePayFlag;
 
     @Column(name = "cancelReason")
     public String getCancelReason() {
@@ -1048,12 +1046,4 @@ public class RecipeOrder implements Serializable {
         this.sendType = sendType;
     }
 
-    @Transient
-    public Integer getStorePayFlag() {
-        return storePayFlag;
-    }
-
-    public void setStorePayFlag(Integer storePayFlag) {
-        this.storePayFlag = storePayFlag;
-    }
 }
