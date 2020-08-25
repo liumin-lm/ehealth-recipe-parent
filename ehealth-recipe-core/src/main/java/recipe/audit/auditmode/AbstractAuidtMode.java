7eb3d89a66573576e68bc542b1e5aa240f496b6e
wenzhicheng <w_zc@ngarihealth.com>
2020-08-25 14:28:20 +0800

    task#72076  对接卫宁审方中心 / 卫宁审方代码编写

--- ehealth-recipe-core/src/main/java/recipe/audit/auditmode/AbstractAuidtMode.java
+++ ehealth-recipe-core/src/main/java/recipe/audit/auditmode/AbstractAuidtMode.java
@@ -5,10 +5,14 @@ import com.ngari.home.asyn.service.IAsynDoBussService;
 import com.ngari.patient.utils.ObjectCopyUtils;
 import com.ngari.recipe.common.RecipeResultBean;
 import com.ngari.recipe.entity.Recipe;
+import com.ngari.recipe.entity.Recipedetail;
 import com.ngari.recipe.recipe.model.RecipeBean;
+import com.ngari.recipe.recipe.model.RecipeDetailBean;
 import ctd.persistence.DAOFactory;
+import ctd.util.AppContextHolder;
 import eh.base.constant.BussTypeConstant;
 import eh.cdr.constant.RecipeStatusConstant;
+import eh.recipeaudit.api.IRecipeAuditService;
 import eh.wxpay.constant.PayConstant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,6 +28,7 @@ import recipe.service.RecipeService;
 import recipe.service.RecipeServiceSub;
 import recipe.util.MapValueUtil;
 
+import java.util.List;
 import java.util.Map;
 
 import static ctd.persistence.DAOFactory.getDAO;
@@ -31,18 +36,23 @@ import static ctd.persistence.DAOFactory.getDAO;
 /**
  * created by shiyuping on 2019/9/3
  */
-public abstract class AbstractAuidtMode implements IAuditMode{
+public abstract class AbstractAuidtMode implements IAuditMode {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AbstractAuidtMode.class);
+
     @Override
-    public void afterHisCallBackChange(Integer status, Recipe recipe,String memo) {
+    public void afterHisCallBackChange(Integer status, Recipe recipe, String memo) {
         RecipeDetailDAO detailDAO = DAOFactory.getDAO(RecipeDetailDAO.class);
         //发送卡片
         RecipeServiceSub.sendRecipeTagToPatient(recipe, detailDAO.findByRecipeId(recipe.getRecipeId()), null, true);
-        saveStatusAndSendMsg(status,recipe,memo);
+        saveStatusAndSendMsg(status, recipe, memo);
+        Integer checkMode = recipe.getCheckMode();
+        if (new Integer(3).equals(checkMode)) {
+            winningRecipeAudit(recipe);
+        }
     }
 
-    protected void saveStatusAndSendMsg(Integer status, Recipe recipe,String memo){
+    protected void saveStatusAndSendMsg(Integer status, Recipe recipe, String memo) {
         //生成文件成功后再去更新处方状态
         RecipeDAO recipeDAO = DAOFactory.getDAO(RecipeDAO.class);
         recipeDAO.updateRecipeInfoByRecipeId(recipe.getRecipeId(), status, null);
@@ -51,13 +61,17 @@ public abstract class AbstractAuidtMode implements IAuditMode{
 
         //平台处方进行消息发送等操作
         if (1 == recipe.getFromflag()) {
-            //发送消息--待审核或者待处理消息
-            RecipeMsgService.batchSendMsg(recipe.getRecipeId(), status);
-            if(RecipeBussConstant.RECIPEMODE_NGARIHEALTH.equals(recipe.getRecipeMode())) {
-                //增加药师首页待处理任务---创建任务
-                if (status == RecipeStatusConstant.READY_CHECK_YS) {
-                    RecipeBean recipeBean = ObjectCopyUtils.convert(recipe, RecipeBean.class);
-                    ApplicationUtils.getBaseService(IAsynDoBussService.class).fireEvent(new BussCreateEvent(recipeBean, BussTypeConstant.RECIPE));
+            Integer checkMode = recipe.getCheckMode();
+            //卫宁审方环境不推送药师消息
+            if (!new Integer(3).equals(checkMode)) {
+                //发送消息--待审核或者待处理消息
+                RecipeMsgService.batchSendMsg(recipe.getRecipeId(), status);
+                if (RecipeBussConstant.RECIPEMODE_NGARIHEALTH.equals(recipe.getRecipeMode())) {
+                    //增加药师首页待处理任务---创建任务
+                    if (status == RecipeStatusConstant.READY_CHECK_YS) {
+                        RecipeBean recipeBean = ObjectCopyUtils.convert(recipe, RecipeBean.class);
+                        ApplicationUtils.getBaseService(IAsynDoBussService.class).fireEvent(new BussCreateEvent(recipeBean, BussTypeConstant.RECIPE));
+                    }
                 }
             }
             //保存至电子病历
@@ -67,20 +81,24 @@ public abstract class AbstractAuidtMode implements IAuditMode{
     }
 
     @Override
-    public int afterAuditRecipeChange() {return RecipeStatusConstant.CHECK_PASS_YS;}
+    public int afterAuditRecipeChange() {
+        return RecipeStatusConstant.CHECK_PASS_YS;
+    }
 
     @Override
-    public void afterCheckPassYs(Recipe recipe) {}
+    public void afterCheckPassYs(Recipe recipe) {
+    }
 
     @Override
-    public void afterCheckNotPassYs(Recipe recipe) {}
+    public void afterCheckNotPassYs(Recipe recipe) {
+    }
 
     @Override
     public void afterPayChange(Boolean saveFlag, Recipe recipe, RecipeResultBean result, Map<String, Object> attrMap) {
         //默认待处理
         Integer status = RecipeStatusConstant.CHECK_PASS;
-        Integer giveMode = null == MapValueUtil.getInteger(attrMap,"giveMode") ? recipe.getGiveMode() : MapValueUtil.getInteger(attrMap,"giveMode");
-        Integer payMode = null == MapValueUtil.getInteger(attrMap, "payMode") ? recipe.getPayMode() : MapValueUtil.getInteger(attrMap,"payMode");
+        Integer giveMode = null == MapValueUtil.getInteger(attrMap, "giveMode") ? recipe.getGiveMode() : MapValueUtil.getInteger(attrMap, "giveMode");
+        Integer payMode = null == MapValueUtil.getInteger(attrMap, "payMode") ? recipe.getPayMode() : MapValueUtil.getInteger(attrMap, "payMode");
         Integer payFlag = MapValueUtil.getInteger(attrMap, "payFlag");
         //根据传入的方式来处理, 因为供应商列表，钥世圈提供的有可能是多种方式都支持，当时这2个值是保存为null的
         if (saveFlag) {
@@ -115,7 +133,7 @@ public abstract class AbstractAuidtMode implements IAuditMode{
             }
         }
 
-        updateRecipeInfoByRecipeId(recipe.getRecipeId(),status,attrMap,result);
+        updateRecipeInfoByRecipeId(recipe.getRecipeId(), status, attrMap, result);
         LOGGER.info("AbstractAuidtMode.afterPayChange saveFlag:{}, payFlag:{}.", saveFlag, payFlag);
         if (saveFlag && new Integer(PayConstant.PAY_FLAG_PAY_SUCCESS).equals(payFlag)) {
             //处方推送到药企
@@ -125,7 +143,7 @@ public abstract class AbstractAuidtMode implements IAuditMode{
 
     }
 
-    protected void updateRecipeInfoByRecipeId(Integer recipeId,Integer status, Map<String, Object> attrMap,RecipeResultBean result){
+    protected void updateRecipeInfoByRecipeId(Integer recipeId, Integer status, Map<String, Object> attrMap, RecipeResultBean result) {
         try {
             RecipeDAO recipeDAO = getDAO(RecipeDAO.class);
             boolean flag = recipeDAO.updateRecipeInfoByRecipeId(recipeId, status, attrMap);
@@ -138,7 +156,21 @@ public abstract class AbstractAuidtMode implements IAuditMode{
         } catch (Exception e) {
             result.setCode(RecipeResultBean.FAIL);
             result.setError("更新处方失败，" + e.getMessage());
-            LOGGER.error("更新处方失败",e);
+            LOGGER.error("更新处方失败", e);
         }
+    }
+
+    /**
+     * 卫宁审方通知
+     *
+     * @param recipe
+     */
+    protected void winningRecipeAudit(Recipe recipe) {
+        IRecipeAuditService recipeAuditService = AppContextHolder.getBean("recipeaudit.recipeAuditService", IRecipeAuditService.class);
+        RecipeDetailDAO recipeDetailDAO = DAOFactory.getDAO(RecipeDetailDAO.class);
+        List<Recipedetail> recipeDetails = recipeDetailDAO.findByRecipeId(recipe.getRecipeId());
+        RecipeBean recipeBean = ObjectCopyUtils.convert(recipe, RecipeBean.class);
+        List<RecipeDetailBean> recipeDetailBeans = ObjectCopyUtils.convert(recipeDetails, RecipeDetailBean.class);
+        recipeAuditService.winningRecipeAudit(recipeBean, recipeDetailBeans);
     }
 }
