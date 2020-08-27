e6c7ebe8ccada282c09a5e68e2457381f4495043
yinsheng <yins@ngarihealth.com>
2020-08-27 14:19:57 +0800

    BUG#50592 【实施】【上海市奉贤区中心医院】【A】查询线下处方缴费提示系统繁忙

--- ehealth-recipe-core/src/main/java/recipe/service/HisRecipeService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/HisRecipeService.java
@@ -1,6 +1,8 @@
 package recipe.service;
 
 import com.ngari.base.property.service.IConfigurationCenterUtilsService;
+import com.ngari.bus.appoint.model.AppointDepartBean;
+import com.ngari.bus.appoint.service.IAppointDepartService;
 import com.ngari.common.mode.HisResponseTO;
 import com.ngari.consult.ConsultAPI;
 import com.ngari.consult.common.model.ConsultExDTO;
@@ -525,7 +527,15 @@ public class HisRecipeService {
         recipe.setOrganName(hisRecipe.getOrganName());
         recipe.setRecipeCode(hisRecipe.getRecipeCode());
         recipe.setRecipeType(hisRecipe.getRecipeType());
-        recipe.setDepart(Integer.parseInt(hisRecipe.getDepartCode()));
+        //recipe.setDepart(Integer.parseInt(hisRecipe.getDepartCode()));
+        //BUG#50592 【实施】【上海市奉贤区中心医院】【A】查询线下处方缴费提示系统繁忙
+        IAppointDepartService appointDepartService = ApplicationUtils.getBaseService(IAppointDepartService.class);
+        AppointDepartBean appointDepartBean = appointDepartService.getByOrganIDAndAppointDepartCode(hisRecipe.getClinicOrgan(), hisRecipe.getDepartCode());
+        if (appointDepartBean != null) {
+            recipe.setDepart(appointDepartBean.getDepartId());
+        } else {
+            LOGGER.info("HisRecipeService saveRecipeFromHisRecipe 无法查询到挂号科室:{}.", hisRecipe.getDepartCode());
+        }
         EmploymentService employmentService = BasicAPI.getService(EmploymentService.class);
         if (StringUtils.isNotEmpty(hisRecipe.getDoctorCode())) {
             EmploymentDTO employmentDTO = employmentService.getByJobNumberAndOrganId(hisRecipe.getDoctorCode(), hisRecipe.getClinicOrgan());
