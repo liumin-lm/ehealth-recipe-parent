764d324703bf8cbf44e3475bb35a2f118488fec5
xy <xy18043124001@gmail.com>
2020-09-22 17:43:41 +0800

    task#77375 [ 电子处方 ] 处方调用电子病历

--- ehealth-recipe-core/src/main/java/recipe/service/RecipeService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/RecipeService.java
@@ -2392,6 +2392,7 @@ public class RecipeService extends RecipeBaseService {
 
         //记录日志
         RecipeLogService.saveRecipeLog(dbRecipeId, beforeStatus, beforeStatus, "修改处方单");
+        emrRecipeManager.saveMedicalInfo(recipe, recipeBean.getRecipeExtend());
         return dbRecipeId;
     }
 
