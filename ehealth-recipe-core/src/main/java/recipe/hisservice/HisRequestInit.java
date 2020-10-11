e08187a16b9fd6ca73dd54e8a256b6a737813e59
liuxiaofeng <lxf@winning.com.cn>
2020-10-11 10:53:47 +0800

    task#74226 [电子处方] 黄河医院商保对接   推送his增加患者渠道id

--- ehealth-recipe-core/src/main/java/recipe/hisservice/HisRequestInit.java
+++ ehealth-recipe-core/src/main/java/recipe/hisservice/HisRequestInit.java
@@ -1,5 +1,6 @@
 package recipe.hisservice;
 
+import com.alibaba.fastjson.JSONObject;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
@@ -24,6 +25,9 @@ import com.ngari.platform.recipe.mode.RecipeExtendBean;
 import com.ngari.platform.recipe.mode.RecipeOrderBean;
 import com.ngari.recipe.entity.*;
 import com.ngari.recipe.recipe.constant.RecipeSendTypeEnum;
+import com.ngari.revisit.RevisitAPI;
+import com.ngari.revisit.common.model.RevisitExDTO;
+import com.ngari.revisit.common.service.IRevisitExService;
 import ctd.account.thirdparty.entity.ThirdPartyMappingEntity;
 import ctd.controller.exception.ControllerException;
 import ctd.dictionary.Dictionary;
@@ -318,6 +322,20 @@ public class HisRequestInit {
             }catch(Exception e){
                 LOGGER.error("initRecipeSendRequestTO recipeid:{} error :{}",recipe.getRecipeId(),e );
             }
+        }
+        // 从复诊获取患者渠道id
+        try {
+            if (recipe.getClinicId() != null) {
+                IRevisitExService exService = RevisitAPI.getService(IRevisitExService.class);
+                LOGGER.info("queryPatientChannelId req={}", recipe.getClinicId());
+                RevisitExDTO revisitExDTO = exService.getByConsultId(recipe.getClinicId());
+                if (revisitExDTO != null) {
+                    LOGGER.info("queryPatientChannelId res={}", JSONObject.toJSONString(revisitExDTO));
+                    requestTO.setPatientChannelId(revisitExDTO.getProjectChannel());
+                }
+            }
+        } catch (Exception e) {
+            LOGGER.error("queryPatientChannelId error:",e);
         }
         //设置挂号序号---如果有
         if (recipe.getClinicId() != null) {
