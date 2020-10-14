23cc0bcf60888ccc51d052e1f0534d5bb6bb69d5
shiyuping <shiyp@ngarihealth.com>
2020-10-14 09:33:09 +0800

    task#79400 【后台】[电子处方]【重庆医科大学附属大学城医院】处方支付结算信息回写

--- ehealth-recipe-core/src/main/java/recipe/hisservice/HisRequestInit.java
+++ ehealth-recipe-core/src/main/java/recipe/hisservice/HisRequestInit.java
@@ -579,6 +579,7 @@ public class HisRequestInit {
                 requestTO.setCertID(patient.getCertificate());
                 requestTO.setPatientName(patient.getPatientName());
             }
+            requestTO.setPatId(recipe.getPatientID());
 
             if (null != card) {
                 requestTO.setCardType(card.getCardType());
@@ -880,11 +881,11 @@ public class HisRequestInit {
         List<RecipeAuditDetailReqTO> detailList = Lists.newArrayList();
         request.setRecipeAuditDetailReqTO(detailList);
         List<RecipeCheckDetail> recipeCheckDetailList = resutlBean.getCheckDetailList();
+        RecipeDetailDAO detailDAO = DAOFactory.getDAO(RecipeDetailDAO.class);
+        List<Recipedetail> recipeDetailList = detailDAO.findByRecipeId(recipe.getRecipeId());
         if (CollectionUtils.isNotEmpty(recipeCheckDetailList)) {
-            RecipeDetailDAO detailDAO = DAOFactory.getDAO(RecipeDetailDAO.class);
-            DrugListDAO drugListDAO = DAOFactory.getDAO(DrugListDAO.class);
 
-            List<Recipedetail> recipeDetailList = detailDAO.findByRecipeId(recipe.getRecipeId());
+            DrugListDAO drugListDAO = DAOFactory.getDAO(DrugListDAO.class);
             List<Integer> drugIds = detailDAO.findDrugIdByRecipeId(recipe.getRecipeId());
             List<DrugList> drugList = drugListDAO.findByDrugIds(drugIds);
             Map<Integer, DrugList> drugInfo = Maps.newHashMap();
@@ -922,6 +923,8 @@ public class HisRequestInit {
             } catch (Exception e) {
                 LOGGER.warn("recipeAudit create his data error. recipeId={}", recipe.getRecipeId(), e);
             }
+        } else {
+            //审核成功后
             //创建处方新增请求体
             try {
                 RecipeSendRequestTO recipeInfo = HisRequestInit.initRecipeSendRequestTO(recipe, recipeDetailList, patientBean, null);
@@ -929,7 +932,6 @@ public class HisRequestInit {
             } catch (Exception e) {
                 LOGGER.warn("recipeAudit create recipeSendInfo error. recipeId={}", recipe.getRecipeId(), e);
             }
-
         }
 
         return request;
