35a16189f16733023d1473019d22b5f237791024
lm <liu_m@winning.com.cn>
2020-10-28 17:28:23 +0800

    task# 81305【后台】【上海互联网】【A】处方笺pdf增加水印

--- ehealth-recipe-core/src/main/java/recipe/thread/UpdateReceiverInfoRecipePdfRunable.java
+++ ehealth-recipe-core/src/main/java/recipe/thread/UpdateReceiverInfoRecipePdfRunable.java
@@ -4,6 +4,7 @@ import com.google.common.collect.ImmutableMap;
 import com.ngari.recipe.entity.DrugsEnterprise;
 import com.ngari.recipe.entity.Recipe;
 import com.ngari.recipe.entity.RecipeOrder;
+import ctd.dictionary.DictionaryController;
 import ctd.persistence.DAOFactory;
 import ctd.util.AppContextHolder;
 import org.apache.commons.lang3.StringUtils;
@@ -23,7 +24,7 @@ import java.math.BigDecimal;
 /**
  * 支付成功后修改pdf 添加收货人信息
  *
- * @author fuzi
+ * @author liumin
  */
 public class UpdateReceiverInfoRecipePdfRunable implements Runnable {
 
@@ -52,6 +53,10 @@ public class UpdateReceiverInfoRecipePdfRunable implements Runnable {
             RecipeOrder order = orderDAO.getRelationOrderByRecipeId(recipeId);
             CommonRemoteService commonRemoteService = AppContextHolder.getBean("commonRemoteService", CommonRemoteService.class);
             logger.info("UpdateReceiverInfoRecipePdfRunable recipeid:{},order:{}", recipeId, JSONUtils.toString(order));
+            String organ = DictionaryController.instance().get("eh.base.dictionary.Organ").getText(recipe.getClinicOrgan());
+            if (StringUtils.isNotEmpty(organ)&&organ.contains("福建省立医院")){
+                return;
+            }
             //存在收货人信息
             if(order!=null&&(StringUtils.isNotEmpty(order.getReceiver()) || StringUtils.isNotEmpty(order.getRecMobile()) || StringUtils.isNotEmpty(commonRemoteService.getCompleteAddress(order)))){
                 logger.info("UpdateReceiverInfoRecipePdfRunable recipeid:{} 添加收货人信息", recipeId);
