b2af6ec9e9a1d03b94147e8ba0ee02caad2c21c1
shiyuping <shiyp@ngarihealth.com>
2020-11-07 02:04:21 +0800

    bug#55517 【正式环境】【电子处方】：通过到院取药/下载处方，处方单详情页，药品金额显示为0

--- ehealth-recipe-core/src/main/java/recipe/service/RecipeServiceSub.java
+++ ehealth-recipe-core/src/main/java/recipe/service/RecipeServiceSub.java
@@ -1801,8 +1801,10 @@ public class RecipeServiceSub {
             if (null != order) {
                 //合并处方这里要改--得重新计算药品费用不能从order里取
                 //actualPrice = order.getRecipeFee();
-                RecipeOrderService recipeOrderService = ApplicationUtils.getRecipeService(RecipeOrderService.class);
-                actualPrice = recipeOrderService.reCalculateRecipeFee(order.getEnterpriseId(), Arrays.asList(recipeId), null);
+                if (order.getEnterpriseId() != null) {
+                    RecipeOrderService recipeOrderService = ApplicationUtils.getRecipeService(RecipeOrderService.class);
+                    actualPrice = recipeOrderService.reCalculateRecipeFee(order.getEnterpriseId(), Arrays.asList(recipeId), null);
+                }
                 recipe.setDiscountAmount(order.getCouponName());
             } else {
                 // couponId = -1有优惠券  不使用 显示“不使用优惠券”
