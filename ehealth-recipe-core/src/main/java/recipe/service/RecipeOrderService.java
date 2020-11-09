47720c2663ad7ca896105eb5d9028841b5b62dc7
liuxiaofeng <lxf@winning.com.cn>
2020-11-09 16:31:54 +0800

    task#83583  处方记账功能开发

--- ehealth-recipe-core/src/main/java/recipe/service/RecipeOrderService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/RecipeOrderService.java
@@ -1980,7 +1980,7 @@ public class RecipeOrderService extends RecipeBaseService {
         wnSplitParam.setOutTradeNo(order.getOutTradeNo());
         Recipe recipe = recipes.get(0);
         // 交易金额 总的需要分账的金额
-        BigDecimal payAmount = new BigDecimal(order.getActualPrice());
+        BigDecimal payAmount = new BigDecimal(order.getActualPrice().toString());
         wnSplitParam.setAmount(payAmount+"");
         // 业务类型 5-处方
         wnSplitParam.setBusType("5");
@@ -2054,7 +2054,7 @@ public class RecipeOrderService extends RecipeBaseService {
         // 分账明细 : 处方无法确定各项金额对应的分账比例金额,所以收款方=参与方=分账方 分账金额=总支付金额
         List<WnAccountDetail> splitList = new ArrayList<>();
         WnAccountDetail splitDTO = new WnAccountDetail();
-        splitDTO.setAmount(new BigDecimal(order.getActualPrice()) + "");
+        splitDTO.setAmount(new BigDecimal(order.getActualPrice().toString()) + "");
         splitDTO.setAccountName(splitName);
         splitDTO.setAccountNo(splitNumber);
         splitDTO.setAccountType(splitType + "");
@@ -2070,7 +2070,7 @@ public class RecipeOrderService extends RecipeBaseService {
      * @param order
      */
     private List<JSONObject> getSplitFeeInfo(RecipeOrder order) {
-        BigDecimal payAmount = new BigDecimal(order.getActualPrice());
+        BigDecimal payAmount = new BigDecimal(order.getActualPrice().toString());
         List<JSONObject> feeList = new ArrayList<>();
         // 审方费
         BigDecimal auditFee = order.getAuditFee();
