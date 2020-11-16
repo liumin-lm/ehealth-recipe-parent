85b8dcc1264d013bd5c52c0f3e2deb9de205514f
fands@winning.com.cn <fands@winning.com.cn>
2020-11-16 11:37:23 +0800

    fix # 快递信息更新成功才发送消息提醒用户

--- ehealth-recipe-core/src/main/java/recipe/atop/RecipeOrderAtop.java
+++ ehealth-recipe-core/src/main/java/recipe/atop/RecipeOrderAtop.java
@@ -2,6 +2,7 @@ package recipe.atop;
 
 import com.alibaba.fastjson.JSON;
 import com.ngari.recipe.service.IRecipeOrderTwoService;
+import com.ngari.recipe.vo.CodeEnum;
 import com.ngari.recipe.vo.ResultBean;
 import com.ngari.recipe.vo.UpdateOrderStatusVO;
 import ctd.persistence.exception.DAOException;
@@ -33,7 +34,9 @@ public class RecipeOrderAtop extends BaseAtop {
         logger.info("RecipeOrderAtop updateRecipeOrderStatus updateOrderStatusVO = {}", JSON.toJSONString(updateOrderStatusVO));
         try {
             ResultBean result = recipeOrderTwoService.updateRecipeOrderStatus(updateOrderStatusVO);
-            this.sendExpressMsg(updateOrderStatusVO);
+            if(result.getCode() == CodeEnum.SERVICE_SUCCEED.getCode()) {
+                this.sendExpressMsg(updateOrderStatusVO);
+            }
             logger.info("RecipeOrderAtop updateRecipeOrderStatus result = {}", JSON.toJSONString(result));
             return result;
         } catch (DAOException e1) {
