2336bca9609eb35ca729ac245607795c1e30ac25
liuxiaofeng <lxf@winning.com.cn>
2020-12-15 10:22:06 +0800

    task#88476 上传监管平台增加发票号增加挂号序号

--- ehealth-recipe-core/src/main/java/recipe/hisservice/syncdata/HisSyncSupervisionService.java
+++ ehealth-recipe-core/src/main/java/recipe/hisservice/syncdata/HisSyncSupervisionService.java
@@ -510,13 +510,7 @@ public class HisSyncSupervisionService implements ICommonSyncSupervisionService 
                 invoiceDTO.setId(recipe.getRecipeId());
                 invoiceDTO.setCardId(req.getCardNo());
                 invoiceDTO.setCardType(req.getCardType());
-                if (recipe.getClinicId() != null) {
-                    IRevisitExService iRevisitExService = RevisitAPI.getService(IRevisitExService.class);
-                    RevisitExDTO consultExDTO = iRevisitExService.getByConsultId(recipe.getClinicId());
-                    if (consultExDTO != null) {
-                        invoiceDTO.setGhxh(consultExDTO.getRegisterNo());
-                    }
-                }
+                invoiceDTO.setGhxh(req.getPatientNumber());
                 invoiceDTO.setMpiid(recipe.getMpiid());
                 invoiceDTO.setOrganId(Integer.parseInt(req.getOrganID()));
                 invoiceDTO.setType("1");
