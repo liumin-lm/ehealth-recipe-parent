553b314997eb635be31032aabe9c72565208fae5
yinsheng <yins@ngarihealth.com>
2020-12-16 21:55:12 +0800

    BUG#58072 【电子处方】列表配置跳转第三方，点击列表处方、处方卡片、消息推送，没有跳转到第三方页面

--- ehealth-recipe-core/src/main/java/recipe/service/RecipeOrderService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/RecipeOrderService.java
@@ -2404,6 +2404,15 @@ public class RecipeOrderService extends RecipeBaseService {
         return result;
     }
 
+    @RpcService
+    public String getThirdUrl(Integer recipeId) {
+        SkipThirdBean skipThirdBean = getThirdUrlNew(recipeId);
+        if (skipThirdBean != null) {
+            return skipThirdBean.getUrl();
+        }
+        return "";
+
+    }
     /**
      * 从微信模板消息跳转时 先获取一下是否需要跳转第三方地址
      * 或者处方审核成功后推送处方卡片消息时点击跳转(互联网)
@@ -2411,7 +2420,7 @@ public class RecipeOrderService extends RecipeBaseService {
      * @return
      */
     @RpcService
-    public SkipThirdBean getThirdUrl(Integer recipeId) {
+    public SkipThirdBean getThirdUrlNew(Integer recipeId) {
         SkipThirdBean skipThirdBean = new SkipThirdBean();
         if (null == recipeId) {
             return new SkipThirdBean();
