3e51c98fb40c0527e3b5149d6543548e57a33802
xlx <xlx@ngarihealth.com>
2020-12-16 14:37:49 +0800

    task#90496【电子处方】上海市胸科医院公众号首页加入处方待处理业务提醒

---
+++ ehealth-recipe-api/src/main/java/com/ngari/recipe/recipe/serviceprovider/service/IPatientTaskService.java
@@ -0,0 +1,28 @@
+package com.ngari.recipe.recipe.serviceprovider.service;
+
+import com.ngari.bus.task.module.PatientTask;
+import ctd.util.annotation.RpcService;
+
+import java.util.List;
+
+/**
+ * @author xlx
+ * @Date: 2020/12/14
+ * @Description:com.ngari.recipe.recipe.serviceprovider.service
+ * @version:1.0
+ */
+public interface IPatientTaskService {
+
+    /**
+     * 首页获取任务栏，初诊，复诊，处方，检查等
+     *
+     * @param mpiId   患者id
+     * @param organId 机构id
+     * @param start
+     * @param limit
+     * @return
+     */
+    @RpcService
+    List<PatientTask> findPatientTask(String mpiId, Integer organId, Integer start, Integer limit);
+
+}
---
+++ ehealth-recipe-core/src/main/java/recipe/serviceprovider/recipeorder/service/PatientTaskServiceImpl.java
@@ -0,0 +1,112 @@
+package recipe.serviceprovider.recipeorder.service;
+
+import com.ngari.bus.task.module.ModuleInfo;
+import com.ngari.bus.task.module.PatientTask;
+import com.ngari.recipe.entity.Recipe;
+import com.ngari.recipe.entity.RecipeExtend;
+import com.ngari.recipe.entity.RecipeOrder;
+import com.ngari.recipe.recipe.serviceprovider.service.IPatientTaskService;
+import ctd.persistence.DAOFactory;
+import ctd.util.JSONUtils;
+import ctd.util.annotation.RpcBean;
+import ctd.util.annotation.RpcService;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import recipe.dao.RecipeDAO;
+import recipe.dao.RecipeExtendDAO;
+import recipe.factory.status.constant.RecipeTaskEnum;
+import recipe.service.HisRecipeService;
+import recipe.service.manager.EmrRecipeManager;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @author xlx
+ * @Date: 2020/12/14
+ * @Description:recipe.serviceprovider.recipeorder.service
+ * @version:1.0
+ */
+
+@RpcBean("patientTaskService")
+public class PatientTaskServiceImpl implements IPatientTaskService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HisRecipeService.class);
+    private RecipeDAO recipeDAO = DAOFactory.getDAO(RecipeDAO.class);
+    private RecipeExtendDAO recipeExtendDAO = DAOFactory.getDAO(RecipeExtendDAO.class);
+
+
+    /**
+     * 首页获取用户需要处理的处方单
+     *
+     * @param mpiId
+     * @param organId
+     * @param start
+     * @param limit
+     * @return
+     */
+    @Override
+    @RpcService
+    public List<PatientTask> findPatientTask(String mpiId, Integer organId, Integer start, Integer limit) {
+        //打印日志， 校验参数
+        LOGGER.info("PatientTaskServiceImpl findPatientTask mpiId={},organId={},start={},limit={}", mpiId, organId, start, limit);
+        if (StringUtils.isEmpty(mpiId) && organId == null) {
+            LOGGER.error("PatientTaskServiceImpl findPatientTask mpiId,organId is null");
+            return null;
+        }
+
+        List<PatientTask> patientTaskArrayList = new ArrayList<>();
+        // 获取对应的处方单
+        List<Recipe> recipes = recipeDAO.queryRecipeInfoByMpiIdAndOrganId(mpiId, organId, start, limit);
+        //查询出未支付的订单
+        List<RecipeOrder> recipeOrders = recipeDAO.queryOrderCodeUnpaid(mpiId, organId);
+        //将list转变为map
+        Map<String, RecipeOrder> recipeOrderMap = recipeOrders.stream().collect(Collectors.toMap(RecipeOrder::getOrderCode, a -> a, (k1, k2) -> k1));
+        for (Recipe recipe : recipes) {
+            PatientTask patientTask = new PatientTask();
+            Map<String, Object> params = new HashMap<>();
+            ModuleInfo moduleInfo = new ModuleInfo();
+            //调用电子病历进行获取病情名
+            RecipeExtend recipeExtend = recipeExtendDAO.getByRecipeId(recipe.getRecipeId());
+            EmrRecipeManager.getMedicalInfo(recipe, recipeExtend);
+            patientTask.setDiseaseName(recipe.getOrganDiseaseName());
+            patientTask.setBusType("recipe");
+            patientTask.setBusId(recipe.getRecipeId());
+            patientTask.setDoctorId(recipe.getDoctor());
+            patientTask.setDoctorName(recipe.getDoctorName());
+            patientTask.setBusDate(recipe.getSignDate());
+            params.put("cid", recipe.getRecipeId());
+            params.put("organId", organId);
+            params.put("mpiId", mpiId);
+            moduleInfo.setParams(params);
+            moduleInfo.setInitFn("doHandle");
+            moduleInfo.setUrl("eh.wx.health.patientRecipe.RecipeDetail");
+            patientTask.setModuleInfo(moduleInfo);
+            //判断处方状态
+            RecipeOrder recipeOrder = recipeOrderMap.get(recipe.getOrderCode());
+            if (null != recipeOrder) {
+                //待支付
+                patientTask.setTaskName(RecipeTaskEnum.RECIPE_TASK_STATUS_UNPAID.getTaskName());
+                patientTask.setBusStatusName(RecipeTaskEnum.RECIPE_TASK_STATUS_UNPAID.getBusStatusName());
+                patientTask.setButtonName(RecipeTaskEnum.RECIPE_TASK_STATUS_UNPAID.getButtonName());
+                patientTaskArrayList.add(patientTask);
+                continue;
+
+            }
+            //处理剩下的状态
+            RecipeTaskEnum recipeStatusEnum = RecipeTaskEnum.getRecipeStatusEnum(recipe.getStatus());
+            if (RecipeTaskEnum.NONE != recipeStatusEnum) {
+                patientTask.setTaskName(recipeStatusEnum.getTaskName());
+                patientTask.setBusStatusName(recipeStatusEnum.getBusStatusName());
+                patientTask.setButtonName(recipeStatusEnum.getButtonName());
+                patientTaskArrayList.add(patientTask);
+            }
+        }
+
+        LOGGER.info("PatientTaskServiceImpl findPatientTask List<PatientTask>:{}", JSONUtils.toString(patientTaskArrayList));
+        return patientTaskArrayList;
+    }
+}
