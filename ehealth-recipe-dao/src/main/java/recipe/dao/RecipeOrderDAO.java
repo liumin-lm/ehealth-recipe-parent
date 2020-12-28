f4ac170fac2f2e7a01a5ae5b939e1ff65c9171a8
yhq <y_hq@ngarihealth.com>
2020-12-28 15:32:51 +0800

    bug#59038【运营平台-对账平台】处方业务对账表药企配送订单明细筛选退款成功的数据显示了支付和退款两条记录；

--- ehealth-recipe-dao/src/main/java/recipe/dao/RecipeOrderDAO.java
+++ ehealth-recipe-dao/src/main/java/recipe/dao/RecipeOrderDAO.java
@@ -503,7 +503,9 @@ public abstract class RecipeOrderDAO extends HibernateSupportDelegateDAO<RecipeO
                     sqlRefund.append(" and o.payFlag = :payType ");
                 }
                 //退款的处方单需要展示两条记录，所以要在取一次
-                sql.append("SELECT * from ( ").append(sqlPay).append(" UNION ALL ").append(sqlRefund).append(" ) a");
+                if(payType==1)sql=sqlPay;
+                else if(payType==3)sql=sqlRefund;
+                else sql.append("SELECT * from ( ").append(sqlPay).append(" UNION ALL ").append(sqlRefund).append(" ) a");
                 if (orderColumn != null) {
                     sql.append(" order by " + orderColumn + " ");
                 }
