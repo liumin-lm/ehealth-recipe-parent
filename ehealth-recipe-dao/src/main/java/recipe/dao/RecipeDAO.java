f1fd835c44b26e138dfd481011875f30325df249
叶修 <ye@ngarihealth.com>
2021-01-13 15:55:58 +0800

    bug#60011 [后台]【在线复诊】医生已撤销处方单，用户依然无法申请复诊退费；

--- ehealth-recipe-dao/src/main/java/recipe/dao/RecipeDAO.java
+++ ehealth-recipe-dao/src/main/java/recipe/dao/RecipeDAO.java
@@ -3323,7 +3323,7 @@ public abstract class RecipeDAO extends HibernateSupportDelegateDAO<Recipe> impl
      * @param Status
      * @return
      */
-    @DAOMethod(sql = "from Recipe where bussSource=:bussSource and clinicId=:clinicId and status not in(13,14)")
+    @DAOMethod(sql = "from Recipe where bussSource=:bussSource and clinicId=:clinicId and status not in(9,13,14)")
     public abstract List<Recipe> findRecipeStatusLoseByBussSourceAndClinicId(@DAOParam("bussSource")Integer bussSource,@DAOParam("clinicId") Integer clinicId,@DAOParam("status") Integer Status);
 
     @DAOMethod
