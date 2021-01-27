a1e919412ebc42aa46a45c8c17bc8818c0152abc
xlx <xlx@ngarihealth.com>
2021-01-27 17:58:34 +0800

    fix--常用方代码调整

--- ehealth-recipe-core/src/main/java/recipe/service/CommonRecipeService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/CommonRecipeService.java
@@ -540,7 +540,7 @@ public class CommonRecipeService extends BaseService<CommonRecipeDTO> {
         drugList.forEach(a -> {
             OrganDrugList organDrugList = null;
             List<OrganDrugList> organDrugs = organDrugListsGroup.get(a.getDrugId());
-            if (CollectionUtils.isNotEmpty(organDrugs)) {
+            if (CollectionUtils.isEmpty(organDrugs)) {
                 throw new DAOException(ErrorCode.SERVICE_ERROR, "机构药品错误");
             }
             for (OrganDrugList organDrug : organDrugs) {
