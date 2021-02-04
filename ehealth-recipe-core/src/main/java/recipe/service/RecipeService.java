6b80b1c5b8c6c6044d3dbabe74b4e523b3cf330f
renfuhao <rfh@ngarihealth.com>
2021-02-04 09:55:01 +0800

    fix# 手动同步 药房字段更改

--- ehealth-recipe-core/src/main/java/recipe/service/RecipeService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/RecipeService.java
@@ -4086,13 +4086,13 @@ public class RecipeService extends RecipeBaseService {
             drugListMatch.setHisFormCode(drug.getHisFormCode());
         }
 
-        /*if (!ObjectUtils.isEmpty(drug.getPharmacyCode())) {
+        if (!ObjectUtils.isEmpty(drug.getPharmacyCode())) {
             String pharmacyCode = drug.getPharmacyCode();
             PharmacyTcm byPharmacyAndOrganId = pharmacyTcmDAO.getByPharmacyAndOrganId(pharmacyCode, organId);
             if (byPharmacyAndOrganId != null){
                 drugListMatch.setPharmacy(drug.getPharmacyCode());
             }
-        }*/
+        }
         if (!ObjectUtils.isEmpty(drug.getRegulationDrugCode())) {
             drugListMatch.setRegulationDrugCode(drug.getRegulationDrugCode());
         }
@@ -4158,14 +4158,14 @@ public class RecipeService extends RecipeBaseService {
         if (!ObjectUtils.isEmpty(drug.getRetrievalCode())) {
             organDrug.setRetrievalCode(drug.getRetrievalCode());
         }
-       /* //药房编码
+        //药房编码
         if (!ObjectUtils.isEmpty(drug.getPharmacyCode())) {
             String pharmacyCode = drug.getPharmacyCode();
             PharmacyTcm byPharmacyAndOrganId = pharmacyTcmDAO.getByPharmacyAndOrganId(pharmacyCode, organId);
             if (byPharmacyAndOrganId != null){
                 organDrug.setPharmacy(drug.getPharmacyCode());
             }
-        }*/
+        }
         //监管平台药品编码
         if (!ObjectUtils.isEmpty(drug.getRegulationDrugCode())) {
             organDrug.setRegulationDrugCode(drug.getRegulationDrugCode());
