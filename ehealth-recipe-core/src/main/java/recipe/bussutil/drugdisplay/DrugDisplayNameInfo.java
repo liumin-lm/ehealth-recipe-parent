39422d892c2f619eeceb17cf894563de1333888c
shiyuping <shiyp@ngarihealth.com>
2021-03-12 16:45:59 +0800

    task#105272 【后台】统一药品取值（西药）

--- ehealth-recipe-core/src/main/java/recipe/bussutil/drugdisplay/DrugDisplayNameInfo.java
+++ ehealth-recipe-core/src/main/java/recipe/bussutil/drugdisplay/DrugDisplayNameInfo.java
@@ -3,7 +3,6 @@ package recipe.bussutil.drugdisplay;
 import ctd.schema.annotation.ItemProperty;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.Map;
 
@@ -11,7 +10,6 @@ import java.util.Map;
  * created by shiyuping on 2021/3/10
  */
 @Data
-@NoArgsConstructor
 @Builder
 public class DrugDisplayNameInfo {
 
