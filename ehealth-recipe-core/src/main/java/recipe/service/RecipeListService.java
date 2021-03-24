881e64ad32c8cec6153b5d5ab1d001a4313ee2fa
shiyuping <shiyp@ngarihealth.com>
2021-03-24 10:32:14 +0800

    bug#64434 【电子处方】暂存的处方在处方列表不显示剂型

--- ehealth-recipe-core/src/main/java/recipe/service/RecipeListService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/RecipeListService.java
@@ -1669,6 +1669,8 @@ public class RecipeListService extends RecipeBaseService {
                 orderStatus = recipeOrders.stream().collect(Collectors.toMap(RecipeOrder::getOrderCode, RecipeOrder::getStatus));
             }
 
+            List<OrganDrugList> organDrugLists;
+            Map<String, Integer> configDrugNameMap;
             for (Recipe recipe : recipeList) {
                 if (StringUtils.isNotEmpty(recipe.getMpiid())) {
                     patientIds.add(recipe.getMpiid());
@@ -1683,15 +1685,14 @@ public class RecipeListService extends RecipeBaseService {
                             recipe.setRecipeDrugName(recipedetails.get(0).getDrugName());
                         } else {
                             //剂型获取---暂存重新获取配置药品名由于Recipedetail没有剂型要重新获取一遍
-                            OrganDrugListDAO organDrugListDAO = DAOFactory.getDAO(OrganDrugListDAO.class);
-                            List<OrganDrugList> organDrugLists = organDrugListDAO.findByOrganIdAndOrganDrugCodeAndDrugIdWithoutStatus(recipe.getClinicOrgan(), recipedetails.get(0).getOrganDrugCode(), recipedetails.get(0).getDrugId());
+                            organDrugLists = organDrugListDAO.findByOrganIdAndOrganDrugCodeAndDrugIdWithoutStatus(recipe.getClinicOrgan(), recipedetails.get(0).getOrganDrugCode(), recipedetails.get(0).getDrugId());
                             if (CollectionUtils.isNotEmpty(organDrugLists)) {
                                 if (StringUtils.isNotEmpty(organDrugLists.get(0).getDrugForm())) {
                                     recipedetails.get(0).setDrugForm(organDrugLists.get(0).getDrugForm());
                                 }
                             }
                             //药品名拼接配置
-                            Map<String, Integer> configDrugNameMap = MapValueUtil.strArraytoMap(DrugNameDisplayUtil.getDrugNameConfigByDrugType(recipe.getClinicOrgan(), recipe.getRecipeType()));
+                            configDrugNameMap = MapValueUtil.strArraytoMap(DrugNameDisplayUtil.getDrugNameConfigByDrugType(recipe.getClinicOrgan(), recipe.getRecipeType()));
                             recipe.setRecipeDrugName(DrugDisplayNameProducer.getDrugName(ObjectCopyUtils.convert(recipedetails.get(0), RecipeDetailBean.class), configDrugNameMap, DrugNameDisplayUtil.getDrugNameConfigKey(recipe.getRecipeType())));
                         }
                     } else {
