342c5eb00e6596929283e1b11e96b0d44109d1c0
carrding <nagrihealth@123>
2021-03-25 17:22:35 +0800

    fixBug#64630 【天津ca】药师审方签名时，ca签名时中断，将生成一个审方未签名的处方单，医生端的状态此时是待审核，该处方单详情页不应该有药师的手签

--- ehealth-recipe-core/src/main/java/recipe/service/RecipeServiceSub.java
+++ ehealth-recipe-core/src/main/java/recipe/service/RecipeServiceSub.java
@@ -1812,9 +1812,9 @@ public class RecipeServiceSub {
             map.put("doctorSignImgToken", FileAuth.instance().createToken(signInfo.get("doctorSignImg"), 3600L));
         }
         //设置药师手签图片id-----药师撤销审核结果/CA签名中/签名失败/未签名 不应该显示药师手签
-        LOGGER.info("checker test {}", StringUtils.isNotEmpty(signInfo.get("checkerSignImg")) && recipe.getStatus() != RecipeStatusConstant.READY_CHECK_YS);
+        LOGGER.info("checker test1 {}", StringUtils.isNotEmpty(signInfo.get("checkerSignImg")) && recipe.getStatus() != RecipeStatusConstant.READY_CHECK_YS);
         if (StringUtils.isNotEmpty(signInfo.get("checkerSignImg")) && recipe.getStatus() != RecipeStatusConstant.READY_CHECK_YS) {
-            LOGGER.info("checker test {}, recipeStatus{}" , !(recipe.getStatus() == RecipeStatusConstant.SIGN_ERROR_CODE_PHA ||
+            LOGGER.info("checker test2 {}, recipeStatus{}" , !(recipe.getStatus() == RecipeStatusConstant.SIGN_ERROR_CODE_PHA ||
                     recipe.getStatus() == RecipeStatusConstant.SIGN_ING_CODE_PHA ||
                     recipe.getStatus() == RecipeStatusConstant.SIGN_NO_CODE_PHA), recipe.getStatus());
             if (!(recipe.getStatus() == RecipeStatusConstant.SIGN_ERROR_CODE_PHA ||
