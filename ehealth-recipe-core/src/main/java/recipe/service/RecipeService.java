b7bbd6a90378c11d56dc1b47404ebcd1c9074630
whf <whf@winning.com.cn>
2021-04-02 17:29:59 +0800

    bug#65161 【电子处方】药企没有库存，医院有库存，处方开出来后，患者处方单详情没有到院取药按钮

--- ehealth-recipe-core/src/main/java/recipe/service/RecipeService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/RecipeService.java
@@ -2100,9 +2100,6 @@ public class RecipeService extends RecipeBaseService {
                     rMap.put("scanDrugStock", true);
 
                 }
-                HashedMap hashedMap = new HashedMap();
-                hashedMap.put("distributionFlag",1);
-                recipeDAO.updateRecipeInfoByRecipeId(recipeId,hashedMap);
                 return rMap;
             }
         }
