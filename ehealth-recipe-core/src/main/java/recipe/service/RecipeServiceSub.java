24b55e0a1bdc4abca2cb3c8a723fadb6e37f365c
whf <whf@winning.com.cn>
2021-04-02 09:20:16 +0800

    bug#65091 【实施】【重庆医科大学附属第二医院】【A类】【BUG】在线复诊——医生开国谈药后患者选择方式只能选择国谈药药店取药

--- ehealth-recipe-core/src/main/java/recipe/service/RecipeServiceSub.java
+++ ehealth-recipe-core/src/main/java/recipe/service/RecipeServiceSub.java
@@ -2135,6 +2135,7 @@ public class RecipeServiceSub {
                 map.put("showSendToHos", 0);
                 map.put("supportTFDS", 0);
                 map.put("supportOnline", 0);
+                map.put("supportToHos", 0);
             }
         }
         //date 20200724 北京互联网按钮展示根据HIS进行判断
