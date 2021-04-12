822cd1b39de69322dd54b29e89270d71823d4734
叶修 <ye@ngarihealth.com>
2021-04-12 11:53:00 +0800

    task#109178【电子处方】【上海市中医医院】药品增加默认嘱托内容

--- ehealth-recipe-entity/src/main/java/com/ngari/recipe/entity/Recipedetail.java
+++ ehealth-recipe-entity/src/main/java/com/ngari/recipe/entity/Recipedetail.java
@@ -110,8 +110,11 @@ public class Recipedetail implements java.io.Serializable {
 	
 	@ItemProperty(alias="药物金额")
 	private BigDecimal drugCost;
-	
-	@ItemProperty(alias="备注信息")
+
+	@ItemProperty(alias="药品嘱托Id")
+	private String entrustmentId;
+
+	@ItemProperty(alias="药品嘱托信息")
 	private String memo;
 
 	@ItemProperty(alias="药品效期")
@@ -242,7 +245,16 @@ public class Recipedetail implements java.io.Serializable {
     @ItemProperty(alias = "前端展示的商品拼接名")
     private String drugDisplaySplicedSaleName;
 
-    public void setUseDaysB(String useDaysB) {
+	@Column(name = "entrustmentId")
+	public String getEntrustmentId() {
+		return entrustmentId;
+	}
+
+	public void setEntrustmentId(String entrustmentId) {
+		this.entrustmentId = entrustmentId;
+	}
+
+	public void setUseDaysB(String useDaysB) {
 		this.useDaysB = useDaysB;
 	}
 	public void setDrugType(Integer drugType) {
@@ -576,7 +588,7 @@ public class Recipedetail implements java.io.Serializable {
         this.drugCost = drugCost;
     }
 
-	@Column(name = "Memo", length = 50)
+	@Column(name = "Memo", length = 200)
 	public String getMemo() {
 		return this.memo;
 	}
