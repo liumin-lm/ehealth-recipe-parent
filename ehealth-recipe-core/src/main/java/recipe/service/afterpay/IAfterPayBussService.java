a03f2619ec5508a15047a0c4003bec1831617ba6
yinsheng <yins@ngarihealth.com>
2021-04-13 17:23:04 +0800

    TASK#109157 【后台】【电子处方】处方支付状态同步通知提示医生

--- ehealth-recipe-core/src/main/java/recipe/service/afterpay/IAfterPayBussService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/afterpay/IAfterPayBussService.java
@@ -1,11 +1,5 @@
 package recipe.service.afterpay;
 
-import com.ngari.recipe.common.RecipeResultBean;
-import com.ngari.recipe.entity.Recipe;
-import com.ngari.recipe.entity.RecipeOrder;
-
-import java.util.List;
-
 /**
  * @author yinsheng
  * @date 2021\4\12 0012 17:43
@@ -20,6 +14,4 @@ public interface IAfterPayBussService {
     Integer RECIPE_SOURCE_ONLINE = 1;
     //正在进行中的复诊状态
     Integer REVISIT_STATUS_IN = 4;
-
-    void handle(RecipeResultBean result, RecipeOrder recipeOrder, List<Recipe> recipes, Integer payFlag);
 }
