d219b2b2815348c1d731b3365bbdad9a6ea738c9
yinsheng <yins@ngarihealth.com>
2021-04-21 15:07:39 +0800

    BUG#65962 【实施】【白银市第三人民医院】【B】【BUG】电子处方只支持下载处方签模式是，一直提示无库存

--- ehealth-recipe-core/src/main/java/recipe/service/DrugListExtService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/DrugListExtService.java
@@ -78,6 +78,7 @@ public class DrugListExtService extends BaseService<DrugListBean> {
     private static final String SUPPORT_SEND_TO_ENTERPRISES = "showSendToEnterprises";
     private static final String SUPPORT_SEND_TO_HOS = "showSendToHos";
     private static final String SUPPORT_TFDS = "supportTFDS";
+    private static final String SUPPORT_DOWNLOAD = "supportDownload";
 
     private static Pattern p = Pattern.compile("(?<=<em>).+?(?=</em>)");
 
@@ -1053,9 +1054,14 @@ public class DrugListExtService extends BaseService<DrugListBean> {
      * @return
      */
     private List<DrugListBean> filterInventoriesData(Integer organId, List<DrugListBean> drugListBeans){
-        LOGGER.info("filterInventoriesData drugListBeans:{}", JSONUtils.toString(drugListBeans));
+        LOGGER.info("filterInventoriesData 入参 drugListBeans:{}", JSONUtils.toString(drugListBeans));
         Iterator iterator = drugListBeans.iterator();
         boolean inventoryFlag = false;
+        boolean supportToHosFlag = getOrganGiveMode(organId, SUPPORT_TO_HOS);
+        boolean supportSendToEnterprises = getOrganGiveMode(organId, SUPPORT_SEND_TO_ENTERPRISES);
+        boolean supportToSendHos = getOrganGiveMode(organId, SUPPORT_SEND_TO_HOS);
+        boolean supportTFDS = getOrganGiveMode(organId, SUPPORT_TFDS);
+        boolean supportDownLoad = getOrganGiveMode(organId, SUPPORT_DOWNLOAD);
         while (iterator.hasNext()) {
             DrugListBean drugListBean = (DrugListBean)iterator.next();
             if (drugListBean != null) {
@@ -1063,9 +1069,7 @@ public class DrugListExtService extends BaseService<DrugListBean> {
                 Iterator drugIterator = drugInventoryInfos.iterator();
                 while (drugIterator.hasNext()) {
                     DrugInventoryInfo drugInventoryInfo = (DrugInventoryInfo)drugIterator.next();
-                    LOGGER.info("filterInventoriesData drugInventoryInfo:{}", JSONUtils.toString(drugInventoryInfo));
                     List<DrugPharmacyInventoryInfo> drugPharmacyInventoryInfos = drugInventoryInfo.getPharmacyInventories();
-                    LOGGER.info("filterInventoriesData drugPharmacyInventoryInfos:{}", JSONUtils.toString(drugPharmacyInventoryInfos));
                     if (CollectionUtils.isEmpty(drugPharmacyInventoryInfos)) {
                         continue;
                     }
@@ -1073,28 +1077,27 @@ public class DrugListExtService extends BaseService<DrugListBean> {
                     while (drugPharmacyIterator.hasNext()) {
                         int acc = 0;
                         DrugPharmacyInventoryInfo drugPharmacyInventoryInfo = (DrugPharmacyInventoryInfo)drugPharmacyIterator.next();
-                        LOGGER.info("filterInventoriesData drugPharmacyInventoryInfo:{}", JSONUtils.toString(drugPharmacyInventoryInfo));
-                        if (getOrganGiveMode(organId, SUPPORT_TO_HOS) && 3 == drugPharmacyInventoryInfo.getType()) {
+                        if (supportToHosFlag && 3 == drugPharmacyInventoryInfo.getType()) {
                             //说明运营平台没有配置到院取药
                             drugPharmacyIterator.remove();
                             acc++;
                         }
-                        if (getOrganGiveMode(organId, SUPPORT_SEND_TO_ENTERPRISES) && 2 == drugPharmacyInventoryInfo.getType()) {
+                        if (supportSendToEnterprises && 2 == drugPharmacyInventoryInfo.getType()) {
                             //说明运营平台没有配置药企配送
                             drugPharmacyIterator.remove();
                             acc++;
                         }
-                        if (getOrganGiveMode(organId, SUPPORT_SEND_TO_HOS) && 1 == drugPharmacyInventoryInfo.getType()) {
+                        if (supportToSendHos && 1 == drugPharmacyInventoryInfo.getType()) {
                             //说明运营平台没有配置医院配送
                             drugPharmacyIterator.remove();
                             acc++;
                         }
-                        if (getOrganGiveMode(organId, SUPPORT_TFDS) && 4 == drugPharmacyInventoryInfo.getType()) {
+                        if (supportTFDS && 4 == drugPharmacyInventoryInfo.getType()) {
                             //说明运营平台没有配置药店取药
                             drugPharmacyIterator.remove();
                             acc++;
                         }
-                        if (5 == drugPharmacyInventoryInfo.getType() && getOrganGiveMode(organId, SUPPORT_TFDS) && getOrganGiveMode(organId, SUPPORT_SEND_TO_ENTERPRISES)) {
+                        if (5 == drugPharmacyInventoryInfo.getType() && supportTFDS && supportSendToEnterprises) {
                             //说明运营平台没有配置药店取药和配送到家
                             drugPharmacyIterator.remove();
                             acc++;
@@ -1114,10 +1117,15 @@ public class DrugListExtService extends BaseService<DrugListBean> {
                     }
                 }
             }
+            //如果运营平台配置了下载处方签则显示
+            if (!supportDownLoad) {
+                inventoryFlag = true;
+            }
             if (inventoryFlag && drugListBean != null) {
                 drugListBean.setInventoriesFlag(inventoryFlag);
             }
         }
+        LOGGER.info("filterInventoriesData 出参 drugListBeans:{}", JSONUtils.toString(drugListBeans));
         return drugListBeans;
     }
 
