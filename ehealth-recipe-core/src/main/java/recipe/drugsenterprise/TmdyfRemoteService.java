7a6ec16b5b45ad778b9cf694e82a7499b4632f9a
carrding <nagrihealth@123>
2021-04-21 11:40:22 +0800

    fix 65969 【实施】【浙江互联网】【B】【BUG】浙江衢化医院医生开方选择药品时都提示无库存

--- ehealth-recipe-core/src/main/java/recipe/drugsenterprise/TmdyfRemoteService.java
+++ ehealth-recipe-core/src/main/java/recipe/drugsenterprise/TmdyfRemoteService.java
@@ -85,7 +85,7 @@ public class TmdyfRemoteService extends AccessDrugEnterpriseService{
 
     @Override
     public String getDrugInventory(Integer drugId, DrugsEnterprise drugsEnterprise, Integer organId) {
-        return "暂不支持库存查询";
+        return "有库存";
     }
 
     @Override
