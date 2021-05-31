b85a566ceb24e31de32896eece8c47662e7f7f6d
yinsheng <yins@ngarihealth.com>
2021-05-31 14:23:42 +0800

    BUG#68879 【电子处方】独立机构预发布：线下合并处方，只有一个处方的时候，处方单详情页顶部出现去合并处方的提示

--- ehealth-recipe-core/src/main/java/recipe/service/RecipeServiceSub.java
+++ ehealth-recipe-core/src/main/java/recipe/service/RecipeServiceSub.java
@@ -1745,6 +1745,7 @@ public class RecipeServiceSub {
                     if (mergeRecipeFlag) {
                         String mergeRecipeWay = (String) configService.getConfiguration(recipe.getClinicOrgan(), "mergeRecipeWay");
                         Integer numCanMergeRecipe = recipeDAO.getNumCanMergeRecipeByMergeRecipeWay(recipe.getMpiid(), recipeExtend.getRegisterID(), recipe.getClinicOrgan(), mergeRecipeWay, recipeExtend.getChronicDiseaseName());
+                        LOGGER.info("RecipeServiceSub getRecipeAndDetailByIdImpl recipeId:{},numCanMergeRecipe:{}.", recipe.getRecipeId(), numCanMergeRecipe);
                         //获取能合并处方的单数大于1的时候才能跳转列表页
                         if (numCanMergeRecipe <= 1) {
                             mergeRecipeFlag = false;
