5a5cbbc25808b108945c38f78d8b82a7ecd86c75
xy <xy18043124001@gmail.com>
2021-06-16 09:52:57 +0800

    fix # 优化代码

--- ehealth-recipe-entity/src/main/java/com/ngari/recipe/entity/CommonRecipe.java
+++ ehealth-recipe-entity/src/main/java/com/ngari/recipe/entity/CommonRecipe.java
@@ -6,7 +6,6 @@ import ctd.schema.annotation.Schema;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.Date;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
@@ -40,12 +39,12 @@ public class CommonRecipe implements Serializable{
     @ItemProperty(alias = "处方类型")
     @Dictionary(id = "eh.cdr.dictionary.RecipeType")
     private Integer recipeType;
-
-    @ItemProperty(alias = "创建时间")
-    private Date createDt;
-
-    @ItemProperty(alias = "最后修改时间")
-    private Date lastModify;
+//
+//    @ItemProperty(alias = "创建时间")
+//    private Date createDt;
+//
+//    @ItemProperty(alias = "最后修改时间")
+//    private Date lastModify;
 
     @ItemProperty(alias = "机构代码")
     private Integer organId;
@@ -129,24 +128,24 @@ public class CommonRecipe implements Serializable{
     public void setCommonRecipeName(String commonRecipeName) {
         this.commonRecipeName = commonRecipeName;
     }
-
-    @Column(name = "CreateDt", length = 19)
-    public Date getCreateDt() {
-        return createDt;
-    }
-
-    public void setCreateDt(Date createDt) {
-        this.createDt = createDt;
-    }
-
-    @Column(name = "LastModify", length = 19)
-    public Date getLastModify() {
-        return lastModify;
-    }
-
-    public void setLastModify(Date lastModify) {
-        this.lastModify = lastModify;
-    }
+//
+//    @Column(name = "CreateDt", length = 19)
+//    public Date getCreateDt() {
+//        return createDt;
+//    }
+//
+//    public void setCreateDt(Date createDt) {
+//        this.createDt = createDt;
+//    }
+//
+//    @Column(name = "LastModify", length = 19)
+//    public Date getLastModify() {
+//        return lastModify;
+//    }
+//
+//    public void setLastModify(Date lastModify) {
+//        this.lastModify = lastModify;
+//    }
 
     public String getRecipeJsonConfig() {
         return recipeJsonConfig;
