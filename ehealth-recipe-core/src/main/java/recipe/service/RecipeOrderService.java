b124425fe72b792dc1d40eea8e8f83b26b5187f8
yinsheng <yins@ngarihealth.com>
2021-06-24 14:18:42 +0800

    fix#代码review  代码优化 日志优化

--- ehealth-recipe-core/src/main/java/recipe/service/RecipeOrderService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/RecipeOrderService.java
@@ -2310,7 +2310,7 @@ public class RecipeOrderService extends RecipeBaseService {
     }
 
     public void uploadRecipeInfoToThird(SkipThirdReqVO skipThirdReqVO) {
-        LOGGER.info("RecipeOrderService uploadRecipeInfoToThird:{}.", JSONUtils.toString(skipThirdReqVO));
+        LOGGER.info("RecipeOrderService uploadRecipeInfoToThird skipThirdReqVO:{}.", JSONUtils.toString(skipThirdReqVO));
         Boolean pushToHisAfterChoose = configurationClient.getValueBooleanCatch(skipThirdReqVO.getOrganId(), "pushToHisAfterChoose", false);
         if (!pushToHisAfterChoose) {
             return ;
@@ -2320,6 +2320,7 @@ public class RecipeOrderService extends RecipeBaseService {
         recipes.forEach(recipe -> {
             recipe.setGiveMode(GiveModeTextEnum.getGiveMode(skipThirdReqVO.getGiveMode()));
             DrugEnterpriseResult result = recipeServiceSub.pushRecipeForThird(recipe, 1);
+            LOGGER.info("RecipeOrderService uploadRecipeInfoToThird result:{}.", JSONUtils.toString(result));
             if (new Integer(0).equals(result.getCode())) {
                 //表示上传失败
                 throw new DAOException(ErrorCode.SERVICE_ERROR, result.getMsg());
@@ -2328,7 +2329,7 @@ public class RecipeOrderService extends RecipeBaseService {
     }
 
     /**
-     * 跳转第三方订单或者处方购药页面  新接口：recipeOrderPatientAtop skipThirdPage
+     * todo 过期方法新调用 使用： recipeOrderPatientAtop skipThirdPage
      * @param recipeId
      * @return
      */
