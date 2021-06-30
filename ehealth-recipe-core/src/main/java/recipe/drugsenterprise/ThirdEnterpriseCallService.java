8b30ecedaf26e32e02dcd0795075c3b063e232d8
yinsheng <yins@ngarihealth.com>
2021-06-30 15:11:53 +0800

    BUG#71556 【电子处方】合并支付，药企通知配送处方状态，通知一张处方后状态改变，再通知另一张处方时，另一张处方状态未改变

--- ehealth-recipe-core/src/main/java/recipe/drugsenterprise/ThirdEnterpriseCallService.java
+++ ehealth-recipe-core/src/main/java/recipe/drugsenterprise/ThirdEnterpriseCallService.java
@@ -56,6 +56,7 @@ import recipe.purchase.CommonOrder;
 import recipe.service.*;
 import recipe.service.manager.EmrRecipeManager;
 import recipe.service.manager.GroupRecipeManager;
+import recipe.service.manager.RecipeManager;
 import recipe.serviceprovider.BaseService;
 import recipe.third.IFileDownloadService;
 import recipe.third.IWXServiceInterface;
@@ -111,6 +112,8 @@ public class ThirdEnterpriseCallService extends BaseService<DrugsEnterpriseBean>
     private RecipeOrderDAO recipeOrderDAO;
     @Autowired
     private DrugEnterpriseLogisticsDAO drugEnterpriseLogisticsDAO;
+    @Autowired
+    private RecipeManager recipeManager;
     /**
      * 待配送状态
      *
@@ -359,12 +362,16 @@ public class ThirdEnterpriseCallService extends BaseService<DrugsEnterpriseBean>
         attrMap.put("sender", sender);
         //以免进行处方失效前提醒
         attrMap.put("remindFlag", 1);
-        String recipeFeeStr = MapValueUtil.getString(paramMap, "recipeFee");
+        /*String recipeFeeStr = MapValueUtil.getString(paramMap, "recipeFee");
         if (StringUtils.isNotEmpty(recipeFeeStr)) {
             attrMap.put("totalMoney", new BigDecimal(recipeFeeStr));
-        }
+        }*/
         //更新处方信息
         Boolean rs = recipeDAO.updateRecipeInfoByRecipeId(recipeId, RecipeStatusConstant.IN_SEND, attrMap);
+        List<Recipe> recipes = recipeManager.getRecipesByOrderCode(recipe.getOrderCode());
+        recipes.stream().filter(recipe1 -> !recipeId.equals(recipe1.getRecipeId())).forEach(recipe2 -> {
+            recipeDAO.updateRecipeInfoByRecipeId(recipe2.getRecipeId(), RecipeStatusConstant.IN_SEND, attrMap);
+        });
 
         if (rs) {
             updateRecipeDetainInfo(recipe, paramMap);
