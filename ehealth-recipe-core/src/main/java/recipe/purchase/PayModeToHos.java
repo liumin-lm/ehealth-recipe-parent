9b15d4c5a6f20194197b3b9764e9ee35251d86c5
carrding <nagrihealth@123>
2021-07-16 16:05:03 +0800

    task#124557 子 【后台】【电子处方】【上海市浦东医院】患者选择，预约取药时间，优化 / 124127 【后台】【电子处方】【上海市浦东医院】患者选择，预约取药时间，优化-2

--- ehealth-recipe-core/src/main/java/recipe/purchase/PayModeToHos.java
+++ ehealth-recipe-core/src/main/java/recipe/purchase/PayModeToHos.java
@@ -117,6 +117,9 @@ public class PayModeToHos implements IPurchaseService{
         order.setOrderCode(orderService.getOrderCode(order.getMpiId()));
         order.setExpectSendDate(MapValueUtil.getString(extInfo, "expectSendDate"));
         order.setExpectSendTime(MapValueUtil.getString(extInfo, "expectSendTime"));
+        //设置预约取药开始和结束时间
+        order.setExpectEndTakeTime(MapValueUtil.getString(extInfo, "expectStartTakeTime"));
+        order.setExpectStartTakeTime(MapValueUtil.getString(extInfo, "expectEndTakeTime"));
         //订单的状态统一到finishOrderPayWithoutPay中设置
         order.setStatus(OrderStatusConstant.READY_GET_DRUG);
         order.setRecipeIdList(JSONUtils.toString(recipeIdLists));
