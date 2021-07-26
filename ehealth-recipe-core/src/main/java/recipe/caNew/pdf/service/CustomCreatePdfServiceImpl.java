6492a08ed6b88ab63dde74f5d0c3fe17a43f85bb
xy <xy18043124001@gmail.com>
2021-07-26 11:23:58 +0800

    task # 124117 【后台】【实施】【广东医科大学附属医院】【A，类】【需求】线上处方样式需要跟医院线下一样

--- ehealth-recipe-core/src/main/java/recipe/caNew/pdf/service/CustomCreatePdfServiceImpl.java
+++ ehealth-recipe-core/src/main/java/recipe/caNew/pdf/service/CustomCreatePdfServiceImpl.java
@@ -88,6 +88,7 @@ public class CustomCreatePdfServiceImpl implements CreatePdfService {
             return null;
         }
         SignRecipePdfVO pdfEsign = new SignRecipePdfVO();
+        pdfEsign.setQrCodeSign(true);
         pdfEsign.setPosX(ordinateVO.getX().floatValue());
         pdfEsign.setPosY((float) ordinateVO.getY() - 20);
         pdfEsign.setWidth(150f);
@@ -155,6 +156,19 @@ public class CustomCreatePdfServiceImpl implements CreatePdfService {
             return CreateRecipePdfUtil.generateCoOrdinatePdf(recipe.getSignFile(), coords);
         }
         return null;
+    }
+
+    @Override
+    public byte[] updateCheckNamePdfEsign(Integer recipeId, SignRecipePdfVO pdfEsign) throws Exception {
+        CoOrdinateVO ordinateVO = redisManager.getPdfCoords(recipeId, RECIPE + OP_RECIPE_CHECKER);
+        if (null == ordinateVO) {
+            return null;
+        }
+        pdfEsign.setPosX(ordinateVO.getX().floatValue());
+        pdfEsign.setPosY((float) ordinateVO.getY() - 20);
+        byte[] data = esignService.signForRecipe2(pdfEsign);
+        logger.info("CustomCreatePdfServiceImpl updateCheckNamePdfEsign data:{}", data.length);
+        return data;
     }
 
     @Override
