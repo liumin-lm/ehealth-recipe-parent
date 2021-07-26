86dc7acf670922cc65155fb866f5cc0e197be822
liumin <632347603@qq.com>
2021-07-26 15:17:00 +0800

    fix -- 日志添加

--- ehealth-recipe-core/src/main/java/recipe/business/offlinetoonline/impl/BaseOfflineToOnlineService.java
+++ ehealth-recipe-core/src/main/java/recipe/business/offlinetoonline/impl/BaseOfflineToOnlineService.java
@@ -558,7 +558,7 @@ public class BaseOfflineToOnlineService {
         if (appointDepartDTO != null) {
             recipe.setDepart(appointDepartDTO.getDepartId());
         } else {
-            LOGGER.info("offlineToOnlineService saveRecipeFromHisRecipe 无法查询到挂号科室:{}.", hisRecipe.getDepartCode());
+            LOGGER.info("BaseOfflineToOnlineService saveRecipeFromHisRecipe 无法查询到挂号科室:{}.", hisRecipe.getDepartCode());
             throw new DAOException(ErrorCode.SERVICE_ERROR, "挂号科室维护错误");
         }
         EmploymentService employmentService = BasicAPI.getService(EmploymentService.class);
@@ -640,7 +640,7 @@ public class BaseOfflineToOnlineService {
             recipe.setRecipeSupportGiveMode(join);
         }
         Recipe res= recipeDAO.saveOrUpdate(recipe);
-        LOGGER.info("BaseOfflineToOnlineService saveRecipeFromHisRecipe res:{}.", JSONUtils.toString(recipe));
+        LOGGER.info("BaseOfflineToOnlineService saveRecipeFromHisRecipe res:{}", JSONUtils.toString(recipe));
         return res;
     }
 
