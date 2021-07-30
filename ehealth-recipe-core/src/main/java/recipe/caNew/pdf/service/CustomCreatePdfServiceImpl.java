470a298d5017747d488ba3c888d03b7954aeeeaa
xy <xy18043124001@gmail.com>
2021-07-30 14:42:19 +0800

    BUG # 73662 【电子处方】【个性化PDF】西药标准模板生成处方笺后展示问题

--- ehealth-recipe-core/src/main/java/recipe/caNew/pdf/service/CustomCreatePdfServiceImpl.java
+++ ehealth-recipe-core/src/main/java/recipe/caNew/pdf/service/CustomCreatePdfServiceImpl.java
@@ -268,7 +268,7 @@ public class CustomCreatePdfServiceImpl implements CreatePdfService {
             return null;
         }
         return new SignImgNode(recipeId.toString(), organSealId, fileId, null, 90F, 90F
-                , (float) ordinateVO.getX() + 40, (float) (ordinateVO.getY() - 40), false);
+                , (float) ordinateVO.getX() + 40, (float) ordinateVO.getY() - 40, false);
     }
 
     /**
