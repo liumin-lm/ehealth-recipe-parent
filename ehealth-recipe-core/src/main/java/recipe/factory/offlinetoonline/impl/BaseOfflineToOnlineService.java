754e0bd50420b1d4dbd9a2d4bb945289bcbec795
liumin <632347603@qq.com>
2021-08-02 17:32:32 +0800

    bug#74009 [电子处方】独立机构预发布：合并支付，选择药店取药，提示：处方单已被处理

--- ehealth-recipe-core/src/main/java/recipe/factory/offlinetoonline/impl/BaseOfflineToOnlineService.java
+++ ehealth-recipe-core/src/main/java/recipe/factory/offlinetoonline/impl/BaseOfflineToOnlineService.java
@@ -525,19 +525,22 @@ public class BaseOfflineToOnlineService {
         Recipe recipeDb = recipeDAO.getByHisRecipeCodeAndClinicOrgan(hisRecipe.getRecipeCode(), hisRecipe.getClinicOrgan());
         LOGGER.info("saveRecipeFromHisRecipe recipeDb:{}.", JSONUtils.toString(recipeDb));
         UserRoleToken userRoleToken = UserRoleToken.getCurrent();
-        if (recipeDb != null && !RecipeUtil.isAllowDeleteByPayFlag(recipeDb.getPayFlag())) {
-            //已支付状态下的处方不允许修改
+        if (recipeDb != null) {
             return recipeDb;
         }
         Recipe recipe = new Recipe();
-        if (recipeDb != null) {
-            //如果为已删除状态，则重新生成
-            if (RecipeStatusEnum.RECIPE_STATUS_DELETE.equals(recipeDb.getStatus())) {
-                recipe.setRecipeId(recipeDb.getRecipeId());
-            } else {
-                return recipeDb;
-            }
-        }
+//        if (recipeDb != null && !RecipeUtil.isAllowDeleteByPayFlag(recipeDb.getPayFlag())) {
+//            //已支付状态下的处方不允许修改
+//            return recipeDb;
+//        }
+//        if (recipeDb != null) {
+//            //如果为已删除状态，则重新生成
+//            if (RecipeStatusEnum.RECIPE_STATUS_DELETE.equals(recipeDb.getStatus())) {
+//                recipe.setRecipeId(recipeDb.getRecipeId());
+//            } else {
+//                return recipeDb;
+//            }
+//        }
         recipe.setBussSource(0);
         //通过挂号序号关联复诊
         try {
