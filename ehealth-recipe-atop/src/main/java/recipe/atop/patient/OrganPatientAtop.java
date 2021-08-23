80b7305a53063ba01d9699a60970dd4bd9aaa9cc
yinsheng <yins@ngarihealth.com>
2021-08-23 14:56:54 +0800

    TASK#129521 【后台】【电子处方】【洪泽区人民医院】在线复诊处方支付后需付款还显示到院支付

--- ehealth-recipe-atop/src/main/java/recipe/atop/patient/OrganPatientAtop.java
+++ ehealth-recipe-atop/src/main/java/recipe/atop/patient/OrganPatientAtop.java
@@ -1,5 +1,6 @@
 package recipe.atop.patient;
 
+import com.alibaba.fastjson.JSON;
 import ctd.persistence.exception.DAOException;
 import ctd.util.annotation.RpcBean;
 import ctd.util.annotation.RpcService;
@@ -9,6 +10,7 @@ import recipe.constant.ErrorCode;
 import recipe.core.api.IOrganBusinessService;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * 机构相关服务
@@ -30,6 +32,27 @@ public class OrganPatientAtop extends BaseAtop {
             throw new DAOException(ErrorCode.SERVICE_ERROR, e1.getMessage());
         } catch (Exception e) {
             logger.error("OrganAtop getOrganForWeb error e", e);
+            throw new DAOException(ErrorCode.SERVICE_ERROR, e.getMessage());
+        }
+    }
+
+    /**
+     * 获取机构购药方式配置
+     * @param organId organId
+     * @return 购药方式列表
+     */
+    @RpcService
+    public Set<String> getOrganGiveModeConfig(Integer organId){
+        logger.info("OrganAtop getOrganGiveModeConfig organId:{}.", organId);
+        try {
+            Set<String> result = organBusinessService.getOrganGiveModeConfig(organId);
+            logger.info("OrganAtop getOrganGiveModeConfig result:{}.", JSON.toJSONString(result));
+            return result;
+        } catch (DAOException e1) {
+            logger.error("OrganAtop getOrganGiveModeConfig error", e1);
+            throw new DAOException(ErrorCode.SERVICE_ERROR, e1.getMessage());
+        } catch (Exception e) {
+            logger.error("OrganAtop getOrganGiveModeConfig error e", e);
             throw new DAOException(ErrorCode.SERVICE_ERROR, e.getMessage());
         }
     }
--- ehealth-recipe-core/src/main/java/recipe/business/OrganBusinessService.java
+++ ehealth-recipe-core/src/main/java/recipe/business/OrganBusinessService.java
@@ -1,18 +1,25 @@
 package recipe.business;
 
+import com.alibaba.fastjson.JSON;
+import com.ngari.base.scratchable.model.ScratchableBean;
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import recipe.client.IConfigurationClient;
 import recipe.client.OrganClient;
 import recipe.core.api.IOrganBusinessService;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class OrganBusinessService extends BaseService implements IOrganBusinessService {
 
     @Autowired
     private OrganClient organClient;
+    @Autowired
+    private IConfigurationClient configurationClient;
 
     @Override
     public List<Integer> getOrganForWeb() {
@@ -21,5 +28,16 @@ public class OrganBusinessService extends BaseService implements IOrganBusinessS
             return organIds;
         }
         return organClient.findAllOrganIds();
+    }
+
+    @Override
+    public Set<String> getOrganGiveModeConfig(Integer organId){
+        Set<String> result = new HashSet<>();
+        List<ScratchableBean> scratchableBeans = configurationClient.getOrganGiveMode(organId);
+        scratchableBeans.forEach(giveMode->
+            result.add(giveMode.getBoxLink())
+        );
+        logger.info("OrganBusinessService getOrganGiveModeConfig result:{}.", JSON.toJSONString(result));
+        return result;
     }
 }
