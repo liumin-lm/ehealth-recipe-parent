a788ebcffc30bf9995719387afb18e15931ed8a4
yinsheng <yins@ngarihealth.com>
2021-08-27 15:22:48 +0800

    TASK#129518 【后台】【实施】【广东医科大学附属医院】【A，类】【需求】复诊功能中实现开具诊疗处方2

--- ehealth-recipe-api/src/main/java/recipe/vo/doctor/RecipeTherapyVO.java
+++ ehealth-recipe-api/src/main/java/recipe/vo/doctor/RecipeTherapyVO.java
@@ -38,7 +38,7 @@ public class RecipeTherapyVO implements Serializable {
     @ItemProperty(alias = "诊疗缴费时间")
     private Date therapyPayTime;
     @ItemProperty(alias = "诊疗作废类型，1:医生撤销，2:HIS作废，3:系统取消")
-    private String therapyCancellationType;
+    private Integer therapyCancellationType;
     @ItemProperty(alias = "诊疗作废信息")
     private String therapyCancellation;
     @ItemProperty(alias = "诊疗状态: 1：待提交，2:待缴费，3:已交费，4：已作废")
