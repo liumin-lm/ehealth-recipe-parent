b8acd1574aea291e662bd966eee9bd2bd7eea363
whf <whf@winning.com.cn>
2021-09-01 15:55:35 +0800

    bug# 76613 【电子处方】【医保预结算】平台和互联网环境点击医保支付后都没有走到测试桩的医保预结算，没有返回预结算金额 添加日志

--- ehealth-recipe-core/src/main/java/recipe/presettle/condition/MedicalInsuranceProgramHandler.java
+++ ehealth-recipe-core/src/main/java/recipe/presettle/condition/MedicalInsuranceProgramHandler.java
@@ -1,8 +1,11 @@
 package recipe.presettle.condition;
 
+import com.alibaba.fastjson.JSONArray;
 import com.ngari.consult.ConsultAPI;
 import com.ngari.consult.ConsultBean;
 import com.ngari.consult.common.service.IConsultService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import recipe.presettle.RecipeOrderTypeEnum;
 import recipe.presettle.model.OrderTypeCreateConditionRequest;
@@ -13,14 +16,21 @@ import recipe.presettle.model.OrderTypeCreateConditionRequest;
  */
 @Component
 public class MedicalInsuranceProgramHandler implements IOrderTypeConditionHandler {
+    /**
+     * logger
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(MedicalInsuranceProgramHandler.class);
     @Override
     public Integer getOrderType(OrderTypeCreateConditionRequest request) {
+        LOGGER.info("MedicalInsuranceProgramHandler.getOrderType req={}", JSONArray.toJSONString(request));
+
         // 根据咨询单特殊来源标识和处方单特殊来源标识设置处方订单orderType为省中，邵逸夫医保小程序
         if (request.getRecipe() == null || null == request.getRecipe().getClinicId()) {
             return null;
         }
         IConsultService consultService = ConsultAPI.getService(IConsultService.class);
         ConsultBean consultBean = consultService.getById(request.getRecipe().getClinicId());
+        LOGGER.info("MedicalInsuranceProgramHandler.getOrderType consultBean={}", JSONArray.toJSONString(consultBean));
         if (null != consultBean) {
             if (Integer.valueOf(1).equals(consultBean.getConsultSource()) && (Integer.valueOf(1).equals(request.getRecipe().getRecipeSource()))) {
                 return RecipeOrderTypeEnum.PROVINCIAL_MEDICAL_APPLETS.getType();
