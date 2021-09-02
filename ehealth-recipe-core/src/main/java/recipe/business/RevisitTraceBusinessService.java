a9e5322163936ff2b42ec995d75ecfe28d1f3e2e
whf <whf@winning.com.cn>
2021-09-02 16:36:41 +0800

    bug# 76537 【电子处方】【线上复诊追溯】处方单还没有发药不应该展示发药药师信息

--- ehealth-recipe-core/src/main/java/recipe/business/RevisitTraceBusinessService.java
+++ ehealth-recipe-core/src/main/java/recipe/business/RevisitTraceBusinessService.java
@@ -158,7 +158,7 @@ public class RevisitTraceBusinessService extends BaseService implements IRevisit
 
                             //发药药师审核
                             RevisitRecipeTraceVo.GiveUser giveUser = new RevisitRecipeTraceVo.GiveUser();
-                            ApothecaryDTO apothecaryDTO2 = doctorClient.getGiveUser(recipe);
+                            ApothecaryDTO apothecaryDTO2 = doctorClient.getGiveUserDefault(recipe);
                             if (apothecaryDTO2 != null) {
                                 if (null != recipeOrder.getDispensingTime()) {
                                     BeanUtils.copy(apothecaryDTO2, giveUser);
