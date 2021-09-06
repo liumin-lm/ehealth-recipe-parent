21754c88e031344ac905de95e899cd4040ebf179
yinsheng <yins@ngarihealth.com>
2021-09-06 17:22:46 +0800

    fix#打印当前线程池的工作状态

--- ehealth-recipe-core/src/main/java/recipe/thread/RecipeBusiThreadPool.java
+++ ehealth-recipe-core/src/main/java/recipe/thread/RecipeBusiThreadPool.java
@@ -1,10 +1,13 @@
 package recipe.thread;
 
 import ctd.util.AppContextHolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * 线程池管理
@@ -14,8 +17,11 @@ import java.util.concurrent.Callable;
  */
 public class RecipeBusiThreadPool {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(RecipeBusiThreadPool.class);
+
     public static void execute(Runnable runnable) {
         ThreadPoolTaskExecutor service = AppContextHolder.getBean("busTaskExecutor", ThreadPoolTaskExecutor.class);
+        printThreadPoolInfo(service);
         if (null != service) {
             service.execute(runnable);
         }
@@ -24,6 +30,7 @@ public class RecipeBusiThreadPool {
 
     public static void submit(Callable callable) {
         ThreadPoolTaskExecutor service = AppContextHolder.getBean("busTaskExecutor", ThreadPoolTaskExecutor.class);
+        printThreadPoolInfo(service);
         if (null != service) {
             service.submit(callable);
         }
@@ -31,8 +38,24 @@ public class RecipeBusiThreadPool {
 
     public static void submitList(List<? extends Callable<String>> callableList) throws InterruptedException {
         ThreadPoolTaskExecutor service = AppContextHolder.getBean("busTaskExecutor", ThreadPoolTaskExecutor.class);
+        printThreadPoolInfo(service);
         if (null != service) {
             service.getThreadPoolExecutor().invokeAll(callableList);
+        }
+    }
+
+    /**
+     * 打印当前线程池工作状态
+     * @param service
+     */
+    private static void printThreadPoolInfo(ThreadPoolTaskExecutor service){
+        try {
+            if (null != service) {
+                ThreadPoolExecutor threadPoolExecutor = service.getThreadPoolExecutor();
+                LOGGER.info("RecipeBusiThreadPool printThreadPoolInfo 当前线程池排队线程数:{},当前线程池活动线程数:{},当前线程池完成线程数:{},当前线程池总线程数:{}.", threadPoolExecutor.getQueue().size(), threadPoolExecutor.getActiveCount(), threadPoolExecutor.getCompletedTaskCount(), threadPoolExecutor.getTaskCount());
+            }
+        } catch (IllegalStateException e) {
+            LOGGER.error("RecipeBusiThreadPool printThreadPoolInfo error: ", e);
         }
     }
 
