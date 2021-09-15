c19883080d34632c9a2f421162cbf16c1416d2be
yinsheng <yins@ngarihealth.com>
2021-09-15 19:56:52 +0800

    TASK#133002 线程池优化第一阶段：优化定时任务提交到线程池的任务

--- ehealth-recipe-manager/src/main/java/recipe/manager/RecipeDetailManager.java
+++ ehealth-recipe-manager/src/main/java/recipe/manager/RecipeDetailManager.java
@@ -7,6 +7,7 @@ import com.ngari.recipe.entity.Recipe;
 import com.ngari.recipe.entity.Recipedetail;
 import org.joda.time.DateTime;
 import org.springframework.stereotype.Service;
+import recipe.util.ObjectCopyUtils;
 import recipe.util.ValidateUtil;
 
 import java.math.BigDecimal;
@@ -113,9 +114,13 @@ public class RecipeDetailManager extends BaseManager {
         return recipeDetailDAO.getCountByRecipeId(recipeId);
     }
 
-
+    /**
+     * 获取处方详情列表
+     * @param recipeId 处方ID
+     * @return 处方详情列表
+     */
     public List<RecipeDetailBean> findByRecipeId(Integer recipeId){
-        recipeDetailDAO.findByRecipeId(recipeId);
-        return null;
+        List<Recipedetail> recipeDetails = recipeDetailDAO.findByRecipeId(recipeId);
+        return ObjectCopyUtils.convert(recipeDetails, RecipeDetailBean.class);
     }
 }
