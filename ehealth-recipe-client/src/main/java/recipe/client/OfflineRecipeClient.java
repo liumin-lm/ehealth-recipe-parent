1554bcf375686a9642bec65895b5fb2e9ae1f9ea
yinsheng <yins@ngarihealth.com>
2021-09-24 17:51:37 +0800

    TASK#134825 子 【后台】【实施】【浙大邵逸夫医院】【A】【需求】支付到院复诊处方改造1 / 退费处理-审方费快递费药品费用分开退费

--- ehealth-recipe-client/src/main/java/recipe/client/OfflineRecipeClient.java
+++ ehealth-recipe-client/src/main/java/recipe/client/OfflineRecipeClient.java
@@ -385,7 +385,7 @@ public class OfflineRecipeClient extends BaseClient {
             medicineCodeInfoTO.setRecipeCode(recipeCode);
             logger.info("OfflineRecipeClient queryMedicineCode medicineCodeInfoTO:{}.", JSON.toJSONString(medicineCodeInfoTO));
             HisResponseTO<MedicineCodeResponseTO> medicineCodeResponseTO = recipeHisService.queryMedicineCode(medicineCodeInfoTO);
-            MedicineCodeResponseTO medicineCodeResponse = JSONUtils.parse(getResponse(medicineCodeResponseTO).toString(), MedicineCodeResponseTO.class);
+            MedicineCodeResponseTO medicineCodeResponse = getResponse(medicineCodeResponseTO);
             logger.info("OfflineRecipeClient queryMedicineCode medicineCodeResponse:{}.", JSONUtils.toString(medicineCodeResponse));
             return medicineCodeResponse.getMedicineCode();
         } catch (Exception e) {
