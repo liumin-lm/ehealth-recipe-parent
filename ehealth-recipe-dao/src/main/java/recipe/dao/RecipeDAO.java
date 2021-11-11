824b14d7c8a14660bcf3292a931cf7c0b8db92a7
zgy <zgy3@winning.com.cn>
2021-11-11 17:00:15 +0800

    task# 140885 【后台】【处方】药师端处方审核页，如果患者是儿童类型，该页面的处方类型应该为儿童处方，且颜色为淡绿色

--- ehealth-recipe-dao/src/main/java/recipe/dao/RecipeDAO.java
+++ ehealth-recipe-dao/src/main/java/recipe/dao/RecipeDAO.java
@@ -3881,7 +3881,7 @@ public abstract class RecipeDAO extends HibernateSupportDelegateDAO<Recipe> impl
             @Override
             public void execute(StatelessSession ss) throws Exception {
                 StringBuilder hql = new StringBuilder();
-                hql.append("select r from Recipe r where 1==1  ");
+                hql.append("select r from Recipe r where 1=1  ");
                 if (StringUtils.isNotEmpty(startTime)) {
                     hql.append(" and r.createDate >= :startTime");
                 }
@@ -3896,8 +3896,12 @@ public abstract class RecipeDAO extends HibernateSupportDelegateDAO<Recipe> impl
                 }
                 Query query = ss.createQuery(hql.toString());
                 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                query.setTimestamp("startTime", sdf.parse(startTime));
-                query.setTimestamp("endTime", sdf.parse(endTime));
+                if (StringUtils.isNotEmpty(startTime)) {
+                    query.setTimestamp("startTime", sdf.parse(startTime));
+                }
+                if (StringUtils.isNotEmpty(endTime)) {
+                    query.setTimestamp("endTime", sdf.parse(endTime));
+                }
                 if (organId != null) {
                     query.setParameter("organId", organId);
                 }
