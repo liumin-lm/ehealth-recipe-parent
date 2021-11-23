90c4db0c75cb445fdc98c2e0bccd404ba812e880
xy <xy18043124001@gmail.com>
2021-11-23 21:55:24 +0800

    task # 142989 【后台】【上海外服医院】库存批量查询功能优化2

--- ehealth-recipe-manager/src/main/java/recipe/manager/OrganDrugListManager.java
+++ ehealth-recipe-manager/src/main/java/recipe/manager/OrganDrugListManager.java
@@ -2,7 +2,6 @@ package recipe.manager;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import com.ngari.recipe.dto.*;
 import com.ngari.recipe.entity.OrganDrugList;
 import com.ngari.recipe.entity.PharmacyTcm;
@@ -127,9 +126,7 @@ public class OrganDrugListManager extends BaseManager {
         // 请求his
         List<OrganDrugList> organDrugList = organDrugListDAO.findByOrganIdAndDrugIds(recipe.getClinicOrgan(), drugIdList);
         List<PharmacyTcm> pharmacyTcmByIds = pharmacyTcmDAO.getPharmacyTcmByIds(pharmaIds);
-        drugStockAmountDTO = drugStockClient.scanDrugStock(detailList, recipe.getClinicOrgan(), organDrugList, pharmacyTcmByIds);
-        logger.info("OrganDrugListManager scanDrugStockByRecipeId 结果={}", JSONObject.toJSONString(drugStockAmountDTO));
-        return drugStockAmountDTO;
+        return drugStockClient.scanDrugStock(detailList, recipe.getClinicOrgan(), organDrugList, pharmacyTcmByIds);
     }
 
     /**
