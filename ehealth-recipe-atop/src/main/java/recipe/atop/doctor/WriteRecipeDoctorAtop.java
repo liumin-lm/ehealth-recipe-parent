57dafc9e6a324bee6e3976628ac0fd682f4f4920
xy <s.xy@ngarihealth.com>
2022-01-05 16:15:37 +0800

    fix # 代码结构调整

---
+++ ehealth-recipe-atop/src/main/java/recipe/atop/doctor/WriteRecipeDoctorAtop.java
@@ -0,0 +1,14 @@
+package recipe.atop.doctor;
+
+import ctd.util.annotation.RpcBean;
+import recipe.atop.BaseAtop;
+
+/**
+ * 开处方服务入口类
+ *
+ * @author fuzi
+ */
+@RpcBean("writeRecipeDoctorAtop")
+public class WriteRecipeDoctorAtop extends BaseAtop {
+
+}
