e6eaee617f787cc3a81a79f5d50e8bd4a71b2c77
yinsheng <yins@ngarihealth.com>
2022-01-09 16:12:10 +0800

    TASK#150775 子 【后台】【实施】【上海外服医院】【需求】【B】在线支付后，自提点可以选择到不同的站点，患者到站自提。 / 增加获取取药站点的接口+前置机接口 稳定排序

--- ehealth-recipe-atop/src/main/java/recipe/atop/patient/DrugEnterprisePatientAtop.java
+++ ehealth-recipe-atop/src/main/java/recipe/atop/patient/DrugEnterprisePatientAtop.java
@@ -79,7 +79,7 @@ public class DrugEnterprisePatientAtop extends BaseAtop {
         validateAtop(medicineStationVO.getLat(), medicineStationVO.getLng());
         List<MedicineStationVO> medicineStationList = iDrugEnterpriseBusinessService.getMedicineStationList(medicineStationVO);
         //对站点由近到远排序
-        Collections.sort(medicineStationList, (o1,o2)-> o1.getDistance() > o2.getDistance() ? 0 : -1);
+        Collections.sort(medicineStationList, (o1,o2)-> o1.getDistance() >= o2.getDistance() ? 0 : -1);
         return medicineStationList;
     }
 
