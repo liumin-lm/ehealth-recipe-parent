ad510475051ce1e374daae80fa8a59a2d4cb3c40
xy <s.xy@ngarihealth.com>
2022-01-10 14:42:08 +0800

    task # 150121 【后台】【药事服务】配合基础服务线parent拆除改造- recipe

--- ehealth-recipe-atop/src/main/java/recipe/core/api/IRecipeBusinessService.java
+++ ehealth-recipe-atop/src/main/java/recipe/core/api/IRecipeBusinessService.java
@@ -8,6 +8,7 @@ import com.ngari.recipe.offlinetoonline.model.FindHisRecipeDetailReqVO;
 import com.ngari.recipe.vo.*;
 import recipe.vo.doctor.PatientOptionalDrugVO;
 import recipe.vo.patient.PatientOptionalDrugVo;
+import recipe.vo.second.MedicalDetailVO;
 
 import java.util.Date;
 import java.util.List;
@@ -111,4 +112,12 @@ public interface IRecipeBusinessService {
 
     void offlineToOnlineForRecipe(FindHisRecipeDetailReqVO request);
 
+    /**
+     * 获取电子病历数据
+     * actionType，为查看：则先根据clinicId查电子病历，若没有则在根据docIndexId查电子病历信息，返回前端结果
+     * actionType，为copy：则recipeId不为空则根据recipeId查docIndexId再调用copy接口，否则用clinicId调用查看接口返回前端结果
+     *
+     * @param caseHistoryVO 电子病历查询对象
+     */
+    MedicalDetailVO getDocIndexInfo(CaseHistoryVO caseHistoryVO);
 }
