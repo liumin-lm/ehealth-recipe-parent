d7f03f19c25dfeb2c268ad5f3113f1e8ead58544
zgy <zgy3@winning.com.cn>
2022-01-11 14:01:10 +0800

    task# 150176 【后台】【实施】【岳阳市中心医院】【A类】【需求】线下号源可以在线上接诊开处方

--- ehealth-recipe-api/src/main/java/com/ngari/recipe/recipe/model/WriteDrugRecipeDTO.java
+++ ehealth-recipe-api/src/main/java/com/ngari/recipe/recipe/model/WriteDrugRecipeDTO.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 import java.io.Serializable;
 
 /**
+ * 院内门诊返回结果
  * @author zgy
  * @date 2022/1/10 17:19
  */
@@ -17,9 +18,14 @@ public class WriteDrugRecipeDTO implements Serializable {
 
     private static final long serialVersionUID = -1182841922613719429L;
 
+    //患者信息
     private PatientDTO patient;
+    //请求的患者信息
     private PatientDTO requestPatient;
+    //门诊信息
     private Consult consult;
+    //类型 默认复诊 2 复诊  5  门诊
     private Integer type;
+    //组装给前端返回的字段对象
     private WriteDrugRecipeBean writeDrugRecipeBean;
 }
