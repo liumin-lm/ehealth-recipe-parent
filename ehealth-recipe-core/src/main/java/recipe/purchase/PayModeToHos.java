8e7ebef864953f3cd1a11146ffa228ed1f0cd251
whf <whf@winning.com.cn>
2022-01-18 11:40:31 +0800

    task#P1-19937--【需求】到院自取也需要新增一个药企，用于控制到院自取模式下的配置

--- ehealth-recipe-core/src/main/java/recipe/purchase/PayModeToHos.java
+++ ehealth-recipe-core/src/main/java/recipe/purchase/PayModeToHos.java
@@ -5,12 +5,14 @@ import com.ngari.base.property.service.IConfigurationCenterUtilsService;
 import com.ngari.patient.dto.OrganDTO;
 import com.ngari.patient.service.OrganService;
 import com.ngari.recipe.common.RecipeResultBean;
+import com.ngari.recipe.drugsenterprise.model.DepListBean;
 import com.ngari.recipe.dto.EnterpriseStock;
 import com.ngari.recipe.entity.*;
 import com.ngari.recipe.recipeorder.model.OrderCreateResult;
 import ctd.persistence.DAOFactory;
 import ctd.persistence.exception.DAOException;
 import ctd.util.JSONUtils;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +22,7 @@ import recipe.bean.RecipePayModeSupportBean;
 import recipe.client.IConfigurationClient;
 import recipe.constant.OrderStatusConstant;
 import recipe.constant.RecipeBussConstant;
+import recipe.core.api.IStockBusinessService;
 import recipe.dao.*;
 import recipe.enumerate.status.RecipeStatusEnum;
 import recipe.enumerate.type.RecipeSupportGiveModeEnum;
@@ -31,6 +34,7 @@ import recipe.service.RecipeOrderService;
 import recipe.util.MapValueUtil;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -60,6 +64,12 @@ public class PayModeToHos implements IPurchaseService {
     private OrganDrugsSaleConfigDAO organDrugsSaleConfigDAO;
     @Autowired
     private EnterpriseManager enterpriseManager;
+    @Autowired
+    private PurchaseService purchaseService;
+    @Autowired
+    private DrugsEnterpriseDAO drugsEnterpriseDAO;
+    @Autowired
+    private IStockBusinessService stockBusinessService;
     /**
      * logger
      */
@@ -68,73 +78,24 @@ public class PayModeToHos implements IPurchaseService {
     @Override
     public RecipeResultBean findSupportDepList(Recipe dbRecipe, Map<String, String> extInfo) {
         RecipeResultBean resultBean = RecipeResultBean.getSuccess();
+        //判断是否是慢病医保患者------郑州人民医院
+        if (purchaseService.isMedicareSlowDiseasePatient(dbRecipe.getRecipeId())) {
+            resultBean.setCode(RecipeResultBean.FAIL);
+            resultBean.setMsg("抱歉，由于您是慢病医保患者，请到人社平台、医院指定药房或者到医院进行医保支付。");
+            return resultBean;
+        }
+
         // 到院自取是否采用药企管理模式
         Boolean drugToHosByEnterprise = configurationClient.getValueBooleanCatch(dbRecipe.getClinicOrgan(), "drugToHosByEnterprise", false);
         if (drugToHosByEnterprise) {
-
+            resultBean = newModeFindSupportDepList(dbRecipe);
         } else {
             resultBean = oldModeFindSupportDepList(dbRecipe);
         }
         return resultBean;
     }
 
-    /**
-     * 老模式到院取药获取药企列表
-     * @param dbRecipe
-     * @return
-     */
-    private RecipeResultBean oldModeFindSupportDepList(Recipe dbRecipe){
-        RecipeDetailDAO detailDAO = DAOFactory.getDAO(RecipeDetailDAO.class);
-        OrganService organService = ApplicationUtils.getBasicService(OrganService.class);
-        RecipeDAO recipeDAO = DAOFactory.getDAO(RecipeDAO.class);
-        RecipeResultBean resultBean = RecipeResultBean.getSuccess();
-        Integer recipeId = dbRecipe.getRecipeId();
-        List<Recipedetail> detailList = detailDAO.findByRecipeId(recipeId);
-        Recipe recipe = recipeDAO.getByRecipeId(recipeId);
-        OrganDTO organDTO = organService.getByOrganId(recipe.getClinicOrgan());
-        StringBuilder sb = new StringBuilder();
-        PurchaseService purchaseService = ApplicationUtils.getRecipeService(PurchaseService.class);
-        //todo---暂时写死上海六院---配送到家判断是否是自费患者
-        //到院取药非卫宁付
-        Integer takeOneselfPaymentChannel = configurationClient.getValueCatch(dbRecipe.getClinicOrgan(), "payModeToHosOnlinePayConfig", 1);
 
-        if (!new Integer(2).equals(takeOneselfPaymentChannel)) {
-            if (dbRecipe.getClinicOrgan() == 1000899 && !purchaseService.isMedicarePatient(1000899, dbRecipe.getMpiid())) {
-                resultBean.setCode(RecipeResultBean.FAIL);
-                resultBean.setMsg("自费患者不支持到院取药，请选择其他取药方式");
-                return resultBean;
-            }
-        }
-        //判断是否是慢病医保患者------郑州人民医院
-        if (purchaseService.isMedicareSlowDiseasePatient(recipeId)) {
-            resultBean.setCode(RecipeResultBean.FAIL);
-            resultBean.setMsg("抱歉，由于您是慢病医保患者，请到人社平台、医院指定药房或者到医院进行医保支付。");
-            return resultBean;
-        }
-
-        //点击到院取药再次判断库存--防止之前开方的时候有库存流转到此无库存
-        // 到院取药校验机构库存
-        EnterpriseStock organStock = organDrugListManager.organStock(recipe.getClinicOrgan(), detailList);
-        if (Objects.isNull(organStock) || !organStock.getStock()) {
-            resultBean.setCode(RecipeResultBean.FAIL);
-            resultBean.setMsg("抱歉，医院没有库存，无法到医院取药，请选择其他购药方式。");
-            return resultBean;
-        }
-
-        RecipeExtendDAO recipeExtendDAO = DAOFactory.getDAO(RecipeExtendDAO.class);
-        RecipeExtend recipeExtend = recipeExtendDAO.getByRecipeId(recipeId);
-        if (!Objects.isNull(recipeExtend) && StringUtils.isNotEmpty(recipeExtend.getPharmNo())) {
-            String pharmNo = recipeExtend.getPharmNo();
-            if (StringUtils.isNotEmpty(pharmNo)) {
-                sb.append("选择到院自取后需去医院取药窗口取药：[" + organDTO.getName() + pharmNo + "取药窗口]");
-            } else {
-                sb.append("选择到院自取后，需去医院取药窗口取药");
-            }
-        }
-        resultBean.setMsg(sb.toString());
-        return resultBean;
-
-    }
     @Override
     public OrderCreateResult order(List<Recipe> dbRecipes, Map<String, String> extInfo) {
         OrderCreateResult result = new OrderCreateResult(RecipeResultBean.SUCCESS);
@@ -283,5 +244,110 @@ public class PayModeToHos implements IPurchaseService {
         RecipeOrderDAO recipeOrderDAO = DAOFactory.getDAO(RecipeOrderDAO.class);
         recipeOrder.setPayMode(2);
         recipeOrderDAO.update(recipeOrder);
+    }
+
+    /**
+     * 新模式获取到院取药支持的药企
+     * @param dbRecipe
+     * @return
+     */
+    private RecipeResultBean newModeFindSupportDepList(Recipe dbRecipe) {
+        Integer recipeId = dbRecipe.getRecipeId();
+        RecipeResultBean resultBean = RecipeResultBean.getSuccess();
+        DepListBean depListBean = new DepListBean();
+
+        // 库存判断
+        // TODO 预留位置 需要his返回取药点
+        List<OrganAndDrugsepRelation> relation = organAndDrugsepRelationDAO.getRelationByOrganIdAndGiveMode(dbRecipe.getClinicOrgan(), RecipeSupportGiveModeEnum.SUPPORT_TO_HOS.getType());
+        if (CollectionUtils.isEmpty(relation)) {
+            resultBean.setCode(RecipeResultBean.FAIL);
+            resultBean.setMsg("抱歉，该机构无支持到院取药的药企，无法到医院取药，请选择其他购药方式。");
+            return resultBean;
+        }
+        List<Integer> depIds = relation.stream().map(OrganAndDrugsepRelation::getDrugsEnterpriseId).collect(Collectors.toList());
+        List<DrugsEnterprise> drugsEnterprises = drugsEnterpriseDAO.findByIds(depIds);
+        if (CollectionUtils.isEmpty(drugsEnterprises)) {
+            LOG.warn("findSupportDepList 处方[{}]没有任何药企可以进行到院取药！", recipeId);
+            resultBean.setCode(5);
+            resultBean.setMsg("抱歉，没有可选择的药企");
+            return resultBean;
+        }
+        List<Recipedetail> detailList = recipeDetailDAO.findByRecipeId(recipeId);
+        List<DrugsEnterprise> subDepList = new ArrayList<>(drugsEnterprises.size());
+        for (DrugsEnterprise dep : drugsEnterprises) {
+            EnterpriseStock enterpriseStock = stockBusinessService.enterpriseStockCheck(dbRecipe, detailList, dep.getId());
+            if (null != enterpriseStock && enterpriseStock.getStock()) {
+                subDepList.add(dep);
+            }
+        }
+
+        if (CollectionUtils.isEmpty(subDepList)) {
+            LOG.warn("findSupportDepList 该处方无法配送. recipeId=[{}]", recipeId);
+            resultBean.setCode(5);
+            resultBean.setMsg("抱歉，没有可选择的药企");
+            return resultBean;
+        }
+
+        LOG.info("findSupportDepList subDepList:{}", JSONUtils.toString(subDepList));
+        depListBean.setSigle(false);
+        if (CollectionUtils.isNotEmpty(subDepList) && subDepList.size() == 1) {
+            depListBean.setSigle(true);
+        }
+        resultBean.setObject(depListBean);
+        LOG.info("findSupportDepList 当前处方{}查询药企列表信息：{}", recipeId, JSONUtils.toString(resultBean));
+        return resultBean;
+    }
+
+    /**
+     * 老模式到院取药获取药企列表
+     *
+     * @param dbRecipe
+     * @return
+     */
+    private RecipeResultBean oldModeFindSupportDepList(Recipe dbRecipe) {
+        RecipeDetailDAO detailDAO = DAOFactory.getDAO(RecipeDetailDAO.class);
+        OrganService organService = ApplicationUtils.getBasicService(OrganService.class);
+        RecipeDAO recipeDAO = DAOFactory.getDAO(RecipeDAO.class);
+        RecipeResultBean resultBean = RecipeResultBean.getSuccess();
+        Integer recipeId = dbRecipe.getRecipeId();
+        List<Recipedetail> detailList = detailDAO.findByRecipeId(recipeId);
+        Recipe recipe = recipeDAO.getByRecipeId(recipeId);
+        OrganDTO organDTO = organService.getByOrganId(recipe.getClinicOrgan());
+        StringBuilder sb = new StringBuilder();
+        PurchaseService purchaseService = ApplicationUtils.getRecipeService(PurchaseService.class);
+        //todo---暂时写死上海六院---配送到家判断是否是自费患者
+        //到院取药非卫宁付
+        Integer takeOneselfPaymentChannel = configurationClient.getValueCatch(dbRecipe.getClinicOrgan(), "payModeToHosOnlinePayConfig", 1);
+
+        if (!new Integer(2).equals(takeOneselfPaymentChannel)) {
+            if (dbRecipe.getClinicOrgan() == 1000899 && !purchaseService.isMedicarePatient(1000899, dbRecipe.getMpiid())) {
+                resultBean.setCode(RecipeResultBean.FAIL);
+                resultBean.setMsg("自费患者不支持到院取药，请选择其他取药方式");
+                return resultBean;
+            }
+        }
+
+        //点击到院取药再次判断库存--防止之前开方的时候有库存流转到此无库存
+        // 到院取药校验机构库存
+        EnterpriseStock organStock = organDrugListManager.organStock(recipe.getClinicOrgan(), detailList);
+        if (Objects.isNull(organStock) || !organStock.getStock()) {
+            resultBean.setCode(RecipeResultBean.FAIL);
+            resultBean.setMsg("抱歉，医院没有库存，无法到医院取药，请选择其他购药方式。");
+            return resultBean;
+        }
+
+        RecipeExtendDAO recipeExtendDAO = DAOFactory.getDAO(RecipeExtendDAO.class);
+        RecipeExtend recipeExtend = recipeExtendDAO.getByRecipeId(recipeId);
+        if (!Objects.isNull(recipeExtend) && StringUtils.isNotEmpty(recipeExtend.getPharmNo())) {
+            String pharmNo = recipeExtend.getPharmNo();
+            if (StringUtils.isNotEmpty(pharmNo)) {
+                sb.append("选择到院自取后需去医院取药窗口取药：[" + organDTO.getName() + pharmNo + "取药窗口]");
+            } else {
+                sb.append("选择到院自取后，需去医院取药窗口取药");
+            }
+        }
+        resultBean.setMsg(sb.toString());
+        return resultBean;
+
     }
 }
