99bce9bc3c37c9d488ae2f78ab892e84ad83391a
whf <whf@winning.com.cn>
2022-02-17 18:24:34 +0800

    task#154100 【后台】【内部优化】归整订单作废状态（包括处方患者端状态修改）

--- ehealth-recipe-manager/src/main/java/recipe/manager/RecipeManager.java
+++ ehealth-recipe-manager/src/main/java/recipe/manager/RecipeManager.java
@@ -264,7 +264,10 @@ public class RecipeManager extends BaseManager {
         logger.info("getToHosProof recipe:{} order:{}" , JSONArray.toJSONString(recipe),JSONArray.toJSONString(order));
         String qrName = "";
         try {
-            OrganDrugsSaleConfig organDrugsSaleConfig = enterpriseManager.getOrganDrugsSaleConfig(recipe.getClinicOrgan(), null, recipe.getGiveMode());
+            if(Objects.isNull(order)){
+                return "";
+            }
+            OrganDrugsSaleConfig organDrugsSaleConfig = enterpriseManager.getOrganDrugsSaleConfig(recipe.getClinicOrgan(), order.getEnterpriseId(), recipe.getGiveMode());
             RecipeShowQrConfigEnum qrConfigEnum = RecipeShowQrConfigEnum.getEnumByType(organDrugsSaleConfig.getTakeDrugsVoucher());
             switch (qrConfigEnum) {
                 case CARD_NO:
