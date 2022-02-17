3f7c52d8f338293e0c1e436fc488a4112109b378
liumin <liu_m@winning.com.cn>
2022-02-17 17:02:22 +0800

    fix --错误解决

--- ehealth-recipe-dao/src/main/java/recipe/dao/OrganAndDrugsepRelationDAO.java
+++ ehealth-recipe-dao/src/main/java/recipe/dao/OrganAndDrugsepRelationDAO.java
@@ -11,6 +11,7 @@ import ctd.persistence.support.hibernate.template.AbstractHibernateStatelessResu
 import ctd.persistence.support.hibernate.template.HibernateSessionTemplate;
 import ctd.persistence.support.hibernate.template.HibernateStatelessResultAction;
 import ctd.util.annotation.RpcSupportDAO;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hibernate.Query;
@@ -18,10 +19,10 @@ import org.hibernate.SQLQuery;
 import org.hibernate.StatelessSession;
 import recipe.dao.comment.ExtendDao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
  * @author yu_yun
  */
 @RpcSupportDAO
@@ -42,6 +43,7 @@ public abstract class OrganAndDrugsepRelationDAO extends HibernateSupportDelegat
 
     /**
      * 根据id获取
+     *
      * @param id
      * @return
      */
@@ -50,6 +52,7 @@ public abstract class OrganAndDrugsepRelationDAO extends HibernateSupportDelegat
 
     /**
      * 根据机构id和药企id获取组织与药企间关系
+     *
      * @param organId
      * @param entId
      * @return
@@ -58,17 +61,18 @@ public abstract class OrganAndDrugsepRelationDAO extends HibernateSupportDelegat
     public abstract OrganAndDrugsepRelation getOrganAndDrugsepByOrganIdAndEntId(@DAOParam("organId") Integer organId, @DAOParam("entId") Integer entId);
 
 
-
     /**
      * 根据药企id获取组织与药企间关系
+     *
      * @param entId
      * @return
      */
     @DAOMethod(sql = "from OrganAndDrugsepRelation where  drugsEnterpriseId=:entId")
-    public abstract List<OrganAndDrugsepRelation> findByEntId( @DAOParam("entId") Integer entId);
+    public abstract List<OrganAndDrugsepRelation> findByEntId(@DAOParam("entId") Integer entId);
 
     /**
      * 根据机构id和状态获取
+     *
      * @param organId
      * @param status
      * @return
@@ -78,6 +82,7 @@ public abstract class OrganAndDrugsepRelationDAO extends HibernateSupportDelegat
 
     /**
      * 根据机构id和状态获取药企id
+     *
      * @param organId
      * @param status
      * @return
@@ -87,19 +92,32 @@ public abstract class OrganAndDrugsepRelationDAO extends HibernateSupportDelegat
 
     /**
      * 根据机构id 与 类型获取关联关系
+     *
      * @param clinicOrgan
      * @param type
      * @return
      */
-    public List<OrganAndDrugsepRelation> getRelationByOrganIdAndGiveMode(@DAOParam("clinicOrgan") Integer clinicOrgan, @DAOParam("type") Integer type){
+    public List<OrganAndDrugsepRelation> getRelationByOrganIdAndGiveMode(@DAOParam("clinicOrgan") Integer clinicOrgan, @DAOParam("type") Integer type) {
         HibernateStatelessResultAction<List<OrganAndDrugsepRelation>> action = new AbstractHibernateStatelessResultAction<List<OrganAndDrugsepRelation>>() {
             @Override
             public void execute(StatelessSession ss) throws Exception {
-                StringBuilder sql = new StringBuilder("SELECT * FROM cdr_organ_drugsep_relation WHERE OrganId = ");
+                StringBuilder sql = new StringBuilder("SELECT id,organid,DrugsEnterpriseId,drug_enterprise_support_give_mode FROM cdr_organ_drugsep_relation WHERE OrganId = ");
                 sql.append(clinicOrgan).append(" and drug_enterprise_support_give_mode LIKE '%").append(type).append("%'");
-                SQLQuery query = ss.createSQLQuery(String.valueOf(sql));
-                query.addEntity(OrganAndDrugsepRelation.class);
-                setResult(query.list());
+                Query query = ss.createSQLQuery(String.valueOf(sql));
+
+                List<Object[]> result = query.list();
+                List<OrganAndDrugsepRelation> vo = new ArrayList<>();
+                if (CollectionUtils.isNotEmpty(result)) {
+                    for (Object[] objects : result) {
+                        OrganAndDrugsepRelation organAndDrugsepRelation = new OrganAndDrugsepRelation();
+                        organAndDrugsepRelation.setDrugsEnterpriseId(Integer.valueOf(objects[2].toString()));
+                        organAndDrugsepRelation.setDrugsEnterpriseSupportGiveMode(objects[3].toString());
+                        organAndDrugsepRelation.setOrganId(Integer.valueOf(objects[1].toString()));
+                        organAndDrugsepRelation.setId(Integer.valueOf(objects[0].toString()));
+                        vo.add(organAndDrugsepRelation);
+                    }
+                }
+                setResult(vo);
             }
         };
         HibernateSessionTemplate.instance().executeReadOnly(action);
