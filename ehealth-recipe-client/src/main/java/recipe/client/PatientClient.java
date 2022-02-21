2766259e2921d2342d163bdaa2b7c90a9df1d939
yinsheng <yins@ngarihealth.com>
2022-02-21 17:11:00 +0800

    TASK#154367 【后台】【实施】【晋江市卫生和计划生育局】【B类】【需求】- 用药提醒功能优化

--- ehealth-recipe-client/src/main/java/recipe/client/PatientClient.java
+++ ehealth-recipe-client/src/main/java/recipe/client/PatientClient.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
 import com.ngari.base.currentuserinfo.model.SimpleWxAccountBean;
 import com.ngari.base.currentuserinfo.service.ICurrentUserInfoService;
+import com.ngari.base.dto.UsePathwaysDTO;
 import com.ngari.base.patient.model.HealthCardBean;
 import com.ngari.base.patient.service.IPatientService;
 import com.ngari.bus.op.service.IUsePathwaysService;
@@ -332,8 +333,17 @@ public class PatientClient extends BaseClient {
                     medicineRemindTO.setOrganId(recipeInfoDTO.getRecipe().getClinicOrgan());
                     medicineRemindTO.setMpiId(patient.getMpiId());
                     medicineRemindTO.setExplan("用法用量");
-                    String explan = "用法用量";
-                    medicineRemindTO.setExplan(explan);
+                    StringBuilder explan = new StringBuilder("用法用量");
+                    try {
+                        UsePathwaysDTO usePathwaysDTO = usePathwaysService.getUsePathwaysByOrganAndPlatformKey(recipeInfoDTO.getRecipe().getClinicOrgan(), recipedetail.getUsePathways());
+                        if (null != usePathwaysDTO) {
+                            explan.append(usePathwaysDTO.getRelatedPlatformText()).append(" ");
+                        }
+                    } catch (Exception e) {
+                        logger.error("PatientClient remindPatientTakeMedicine error", e);
+                    }
+                    explan.append(recipedetail.getUseDose()).append(recipedetail.getUseDoseUnit());
+                    medicineRemindTO.setExplan(explan.toString());
                     medicineRemindTO.setNum(recipedetail.getUseDays());
                     medicineRemindTO.setUnit(0);
                     medicineRemindTO.setEvery(0);
