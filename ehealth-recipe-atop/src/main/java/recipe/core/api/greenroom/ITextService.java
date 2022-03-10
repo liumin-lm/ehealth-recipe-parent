6775a4054187a36d6dee0bc5c5110f9d45e10dc9
xy <s.xy@ngarihealth.com>
2022-03-10 17:48:43 +0800

    fix # 后门代码

--- ehealth-recipe-atop/src/main/java/recipe/core/api/greenroom/ITextService.java
+++ ehealth-recipe-atop/src/main/java/recipe/core/api/greenroom/ITextService.java
@@ -1,15 +1,26 @@
 package recipe.core.api.greenroom;
 
 
+import com.ngari.base.esign.model.CoOrdinateVO;
+
 /**
  * 用于postman 后门接口调用
+ *
  * @author fuzi
  */
 public interface ITextService {
+    /**
+     * 手动录入坐标缓存
+     *
+     * @param recipeId
+     */
+    void coOrdinate(Integer recipeId, CoOrdinateVO ordinateVO);
+
     /**
      * 支付成功后修改pdf 添加收货人信息/煎法
      *
      * @param recipeId
      */
     void updateAddressPdfExecute(Integer recipeId);
+
 }
