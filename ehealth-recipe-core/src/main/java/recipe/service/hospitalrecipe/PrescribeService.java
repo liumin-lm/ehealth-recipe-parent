023997e32c59a9f6c2cabccdf47a748349a146c6
yinsheng <yins@ngarihealth.com>
2022-04-20 17:07:53 +0800

    BUG#94803 【实施】【武汉市武昌区卫生和计划生育局】【B类】【BUG】药事服务医师签名报错

--- ehealth-recipe-core/src/main/java/recipe/service/hospitalrecipe/PrescribeService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/hospitalrecipe/PrescribeService.java
@@ -1,5 +1,6 @@
 package recipe.service.hospitalrecipe;
 
+import com.alibaba.fastjson.JSON;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
@@ -89,6 +90,7 @@ public class PrescribeService {
      */
     @RpcService
     public HosRecipeResult createPrescription(HospitalRecipeDTO hospitalRecipeDTO) {
+        LOG.info("PrescribeService createPrescription hospitalRecipeDTO:{}", JSON.toJSONString(hospitalRecipeDTO));
         HosRecipeResult<RecipeBean> result = new HosRecipeResult();
         //重置为默认失败
         result.setCode(HosRecipeResult.FAIL);
