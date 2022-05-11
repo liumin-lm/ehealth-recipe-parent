ae6e187aa8d073eb1bdada64b7902dcefdbdf3bc
yinsheng <yins@ngarihealth.com>
2022-05-11 10:48:44 +0800

    Task#95902 【需求】【上海市第六人民医院】【A】【需求】更改纳里医生开具一张处方两个药企弹窗提示文案

--- ehealth-recipe-core/src/main/java/recipe/business/StockBusinessService.java
+++ ehealth-recipe-core/src/main/java/recipe/business/StockBusinessService.java
@@ -158,14 +158,6 @@ public class StockBusinessService extends BaseService implements IStockBusinessS
             enterpriseManager.doSignRecipe(doSignRecipe, null, "未找到满足库存要求的购药方式");
             return MapValueUtil.beanToMap(doSignRecipe);
         }
-        //配置下载处方签 或者 例外支付
-        String supportMedicalPaymentButton = RecipeSupportGiveModeEnum.getGiveModeName(giveModeButtonBeans, RecipeSupportGiveModeEnum.SUPPORT_MEDICAL_PAYMENT.getText());
-        String supportDownloadButton = organDrugListManager.organStockDownload(recipe.getClinicOrgan(), recipeDetails);
-        if (StringUtils.isNotEmpty(supportDownloadButton) || StringUtils.isNotEmpty(supportMedicalPaymentButton)) {
-            //保存药品购药方式
-            saveGiveMode(recipe, organStock, enterpriseStock, recipeDetails);
-            return MapValueUtil.beanToMap(doSignRecipe);
-        }
         //未配置药企 医院无库存
         if (CollectionUtils.isEmpty(enterpriseStock) && null != organStock && !organStock.getStock()) {
             enterpriseManager.doSignRecipe(doSignRecipe, organStock.getDrugName(), "未找到满足库存要求的购药方式");
@@ -184,8 +176,8 @@ public class StockBusinessService extends BaseService implements IStockBusinessS
         //校验医院和药企
         if (CollectionUtils.isNotEmpty(enterpriseStock) && null != organStock) {
             boolean stockEnterprise = enterpriseStock.stream().anyMatch(EnterpriseStock::getStock);
-            //医院有库存 药企无库存
-            if (!stockEnterprise && organStock.getStock()) {
+            //医院有库存
+            if (organStock.getStock()) {
                 List<List<String>> groupList = new LinkedList<>();
                 enterpriseStock.forEach(a -> groupList.add(a.getDrugName()));
                 List<String> enterpriseDrugName = ListValueUtil.minIntersection(groupList);
@@ -193,19 +185,32 @@ public class StockBusinessService extends BaseService implements IStockBusinessS
                 enterpriseManager.doSignRecipe(doSignRecipe, enterpriseDrugName, msg.toString());
                 doSignRecipe.setCanContinueFlag("2");
             }
-            //医院无库存 药企有库存
-            if (stockEnterprise && !organStock.getStock()) {
+            //药企有库存
+            if (stockEnterprise) {
                 List<EnterpriseStock> haveStockEnterpriseList = enterpriseStock.stream().filter(EnterpriseStock::getStock).collect(Collectors.toList());
                 haveStockEnterpriseList.forEach(a->{
-
+                    msg.append("【").append(a.getGiveModeButton().get(0).getShowButtonName()).append("】");
                 });
-                enterpriseManager.doSignRecipe(doSignRecipe, organStock.getDrugName(), "药品医院库存不足，该处方仅支持药企配送，无法到院取药，是否继续？");
+                enterpriseManager.doSignRecipe(doSignRecipe, organStock.getDrugName(), msg.toString());
                 doSignRecipe.setCanContinueFlag("1");
             }
             //医院无库存 药企无库存
             if (!stockEnterprise && !organStock.getStock()) {
                 enterpriseManager.doSignRecipe(doSignRecipe, organStock.getDrugName(), "未找到满足库存要求的购药方式");
             }
+        }
+        //配置下载处方签 或者 例外支付
+        String supportMedicalPaymentButton = RecipeSupportGiveModeEnum.getGiveModeName(giveModeButtonBeans, RecipeSupportGiveModeEnum.SUPPORT_MEDICAL_PAYMENT.getText());
+        String supportDownloadButton = organDrugListManager.organStockDownload(recipe.getClinicOrgan(), recipeDetails);
+        if (StringUtils.isNotEmpty(supportDownloadButton)) {
+            msg.append("【").append(supportMedicalPaymentButton).append("】");
+        }
+        if (StringUtils.isNotEmpty(supportMedicalPaymentButton)) {
+            msg.append("【").append(supportMedicalPaymentButton).append("】");
+        }
+        if (StringUtils.isNotEmpty(supportDownloadButton) || StringUtils.isNotEmpty(supportMedicalPaymentButton)) {
+            enterpriseManager.doSignRecipe(doSignRecipe, null, msg.toString());
+            doSignRecipe.setCanContinueFlag("1");
         }
         //保存药品购药方式
         saveGiveMode(recipe, organStock, enterpriseStock, recipeDetails);
