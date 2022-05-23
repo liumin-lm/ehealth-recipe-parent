4d4364d57fa74421cca98cd3c129e5d4915e99e2
yinsheng <yins@ngarihealth.com>
2022-05-23 10:24:44 +0800

    TASK#168768 【后台】【处方业务】【上海浦东医院】【B】【BUG】查询门诊记录接口有返回就诊信息，但患者无法发起复诊 提需求

--- ehealth-recipe-core/src/main/java/recipe/mq/MqEasyPayConsumer.java
+++ ehealth-recipe-core/src/main/java/recipe/mq/MqEasyPayConsumer.java
@@ -2,14 +2,17 @@ package recipe.mq;
 
 import com.alibaba.fastjson.JSON;
 import com.ngari.recipe.entity.RecipeExtend;
+import com.ngari.recipe.entity.RecipeOrder;
 import ctd.net.broadcast.Observer;
 import ctd.persistence.DAOFactory;
+import ctd.util.JSONUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import recipe.dao.RecipeExtendDAO;
+import recipe.dao.RecipeOrderDAO;
 
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -26,20 +29,26 @@ public class MqEasyPayConsumer implements Observer<Map<String,String>> {
         String busId = map.get("busId");
         String handleType = map.get("handleType");
         RecipeExtendDAO recipeExtendDAO = DAOFactory.getDAO(RecipeExtendDAO.class);
+        RecipeOrderDAO recipeOrderDAO = DAOFactory.getDAO(RecipeOrderDAO.class);
         if (RECIPE_TYPE.equals(busType)) {
             logger.info("MqEasyPayConsumer onMessage map:{}", JSON.toJSON(map));
             if (StringUtils.isEmpty(busId)) {
                 return;
             }
-            RecipeExtend recipeExtend = recipeExtendDAO.getByRecipeId(Integer.parseInt(busId));
-            if (null == recipeExtend) {
+            RecipeOrder recipeOrder = recipeOrderDAO.getByOrderId(Integer.parseInt(busId));
+            if (null == recipeOrder) {
                 return;
             }
-            if (StringUtils.isEmpty(handleType)) {
-                return;
-            }
-            recipeExtend.setHandleType(handleType);
-            recipeExtendDAO.updateNonNullFieldByPrimaryKey(recipeExtend);
+            List<Integer> recipeIdList = JSONUtils.parse(recipeOrder.getRecipeIdList(), List.class);
+            recipeIdList.forEach(recipeId ->{
+                try {
+                    RecipeExtend recipeExtend = recipeExtendDAO.getByRecipeId(recipeId);
+                    recipeExtend.setHandleType(handleType);
+                    recipeExtendDAO.updateNonNullFieldByPrimaryKey(recipeExtend);
+                } catch (NumberFormatException e) {
+                    logger.error("MqEasyPayConsumer onMessage recipeId:{} error", recipeId, e);
+                }
+            });
         }
     }
 }
