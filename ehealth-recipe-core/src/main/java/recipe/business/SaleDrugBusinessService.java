38d488d460c03d940ab0afcb5612b187491cb9c8
liumin <liu_m@winning.com.cn>
2022-05-24 18:06:21 +0800

    TASK #168597 【后台】【实施】【上海市第六人民医院】【A】【需求】互联网医院药品目录需支持拆零药品可以正常开具--运营平台

--- ehealth-recipe-core/src/main/java/recipe/business/SaleDrugBusinessService.java
+++ ehealth-recipe-core/src/main/java/recipe/business/SaleDrugBusinessService.java
@@ -1,7 +1,6 @@
 package recipe.business;
 
 import com.alibaba.fastjson.JSONObject;
-import com.ngari.recipe.entity.OrganAndDrugsepRelation;
 import com.ngari.recipe.entity.SaleDrugList;
 import com.ngari.recipe.entity.SaleDrugSalesStrategy;
 import ctd.util.JSONUtils;
@@ -9,13 +8,13 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import recipe.core.api.ISaleDrugBusinessService;
-import recipe.dao.OrganAndDrugsepRelationDAO;
+import recipe.dao.DrugsEnterpriseDAO;
 import recipe.dao.OrganDrugListDAO;
 import recipe.dao.SaleDrugListDAO;
+import recipe.manager.SaleDrugListManager;
 
 import java.util.ArrayList;
 import java.util.List;
-import recipe.manager.SaleDrugListManager;
 
 /**
  * @description： 药企药品
@@ -44,51 +43,23 @@ public class SaleDrugBusinessService extends BaseService implements ISaleDrugBus
         if (null == saleDrugListDb) {
             return res;
         }
-        DrugsEnterprise drugsEnterprise = drugsEnterpriseDAO.get(saleDrugListDb.getOrganId());
-        if (null == drugsEnterprise) {
-            return res;
-        }
-        List<OrganDrugList> organDrugListList = organDrugListDAO.findByDrugIdAndOrganId(saleDrugList.getDrugId(), drugsEnterprise.getOrganId());
-        if (CollectionUtils.isEmpty(organDrugListList)) {
-            return res;
-        }
-        OrganDrugList organDrugList = organDrugListList.get(0);
-
-        SaleDrugSalesStrategy saleDrugSalesStrategy = new SaleDrugSalesStrategy();
-        //默认销售策略没id
-        saleDrugSalesStrategy.setOrganDrugListSalesStrategyId(null);
-        if (StringUtils.isEmpty(saleDrugListDb.getEnterpriseSalesStrategy())) {
-            saleDrugSalesStrategy.setButtonIsOpen("true");
-        } else {
-            saleDrugSalesStrategy.setButtonIsOpen("false");
-        }
-        saleDrugSalesStrategy.setIsDefault("true");
-        saleDrugSalesStrategy.setButtonIsOpen(organDrugList.getUnit());
-
-        String enterpriseSalesStrategy = saleDrugListDb.getEnterpriseSalesStrategy();
-        List<SaleDrugSalesStrategy> saleDrugSalesStrategies = JSONObject.parseArray(enterpriseSalesStrategy, SaleDrugSalesStrategy.class);
-        saleDrugSalesStrategies.add(saleDrugSalesStrategy);
-        saleDrugListDb.setEnterpriseSalesStrategy(JSONUtils.toString(saleDrugSalesStrategies));
-        saleDrugListDAO.updateNonNullFieldByPrimaryKey(saleDrugListDb);
-        //根据药企药品找到对应的机构药品的默认销售策略（）
-        //取机构药品目录的默认销售策略
-        //organDrugListDAO
-        return saleDrugList1;
+        saleDrugListDb.setEnterpriseSalesStrategy(saleDrugListManager.getEnterpriseSalesStrategy(saleDrugList));
+        return saleDrugListDb;
     }
 
     @Override
     public void saveSaleDrugSalesStrategy(SaleDrugList saleDrugList) {
-        logger.info("saveSaleDrugSalesStrategy saleDrugList={}",JSONUtils.toString(saleDrugList));
+        logger.info("saveSaleDrugSalesStrategy saleDrugList={}", JSONUtils.toString(saleDrugList));
         //获取之前的药企药品目录
         SaleDrugList saleDrugList1 = saleDrugListDAO.getByDrugIdAndOrganId(saleDrugList.getDrugId(), saleDrugList.getOrganId());
         List<SaleDrugSalesStrategy> saleDrugSalesStrategyList = new ArrayList<>();
         //把前端传的默认的药企药品销售策略去除
-        if(StringUtils.isNotEmpty(saleDrugList.getEnterpriseSalesStrategy())){
+        if (StringUtils.isNotEmpty(saleDrugList.getEnterpriseSalesStrategy())) {
             saleDrugSalesStrategyList = JSONObject.parseArray(saleDrugList.getEnterpriseSalesStrategy(), SaleDrugSalesStrategy.class);
             saleDrugSalesStrategyList.removeIf(saleDrugSalesStrategy -> saleDrugSalesStrategy.getIsDefault().equals("true"));
         }
         saleDrugList1.setEnterpriseSalesStrategy(JSONUtils.toString(saleDrugSalesStrategyList));
-        logger.info("saveSaleDrugSalesStrategy saleDrugList1={}",JSONUtils.toString(saleDrugList1));
+        logger.info("saveSaleDrugSalesStrategy saleDrugList1={}", JSONUtils.toString(saleDrugList1));
         //最后进行更新
         saleDrugListDAO.update(saleDrugList1);
     }
