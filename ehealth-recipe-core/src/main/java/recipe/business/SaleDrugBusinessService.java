b533940b49935e776b725294307fcc1c8ca01708
liumin <liu_m@winning.com.cn>
2022-05-24 14:01:29 +0800

    TASK #168597 【后台】【实施】【上海市第六人民医院】【A】【需求】互联网医院药品目录需支持拆零药品可以正常开具--运营平台

--- ehealth-recipe-core/src/main/java/recipe/business/SaleDrugBusinessService.java
+++ ehealth-recipe-core/src/main/java/recipe/business/SaleDrugBusinessService.java
@@ -33,7 +33,7 @@ public class SaleDrugBusinessService extends BaseService implements ISaleDrugBus
         SaleDrugList saleDrugList1 = saleDrugListDAO.getByDrugIdAndOrganId(saleDrugList.getDrugId(), saleDrugList.getOrganId());
         List<OrganAndDrugsepRelation> organAndDrugsepRelations = organAndDrugsepRelationDAO.findByEntId(saleDrugList1.getOrganId());
         //取机构药品目录的默认销售策略
-        organDrugListDAO.
+        // organDrugListDAO.
         return saleDrugList1;
     }
 }
