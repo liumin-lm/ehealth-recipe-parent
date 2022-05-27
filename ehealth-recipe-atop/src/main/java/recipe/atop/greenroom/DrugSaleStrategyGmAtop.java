b11a51c49468bb1003bcb1ce1f6b29fafedb1f09
yinsheng <yins@ngarihealth.com>
2022-05-27 13:49:12 +0800

    TASK#168595 【后台】【实施】【上海市第六人民医院】【A】【需求】互联网医院药品目录需支持拆零药品可以正常开具--其他、患者端+同步到药企的订单接口

--- ehealth-recipe-atop/src/main/java/recipe/atop/greenroom/DrugSaleStrategyGmAtop.java
+++ ehealth-recipe-atop/src/main/java/recipe/atop/greenroom/DrugSaleStrategyGmAtop.java
@@ -1,15 +1,11 @@
 package recipe.atop.greenroom;
 
-import com.ngari.recipe.entity.DrugSaleStrategy;
-import com.ngari.recipe.entity.OrganDrugList;
 import com.ngari.recipe.vo.DrugSaleStrategyVO;
 import ctd.util.annotation.RpcBean;
 import ctd.util.annotation.RpcService;
 import org.springframework.beans.factory.annotation.Autowired;
 import recipe.atop.BaseAtop;
 import recipe.core.api.IDrugSaleStrategyBusinessService;
-import recipe.core.api.IDrugsEnterpriseBusinessService;
-import recipe.core.api.ISaleDrugBusinessService;
 
 @RpcBean(value = "drugSaleStrategyGmAtop")
 public class DrugSaleStrategyGmAtop extends BaseAtop {
