b2e72c98867eea12c9cf6ce80e553c8d23f11a28
yinsheng <yins@ngarihealth.com>
2022-05-27 11:01:15 +0800

    TASK#168595 【后台】【实施】【上海市第六人民医院】【A】【需求】互联网医院药品目录需支持拆零药品可以正常开具--其他、患者端+同步到药企的订单接口

--- ehealth-recipe-atop/src/main/java/recipe/atop/greenroom/DrugSaleStrategyGmAtop.java
+++ ehealth-recipe-atop/src/main/java/recipe/atop/greenroom/DrugSaleStrategyGmAtop.java
@@ -1,21 +1,21 @@
-package recipe.atop.greenroom;
-
-import com.ngari.recipe.entity.DrugSaleStrategy;
-import com.ngari.recipe.entity.OrganDrugList;
-import ctd.util.annotation.RpcBean;
-import ctd.util.annotation.RpcService;
-import recipe.atop.BaseAtop;
+//package recipe.atop.greenroom;
 //
-//@RpcBean(value = "drugSaleStrategyGmAtop")
-//public class DrugSaleStrategyGmAtop extends BaseAtop {
-////    /**
-////     * 添加药品销售策略
-////     * @param drugSaleStrategy 销售策略
-////     */
-////    @RpcService
-////    public void addDrugSaleStrategy(DrugSaleStrategy drugSaleStrategy) {
-////        validateAtop(drugSaleStrategy.getStrategyTitle(),drugSaleStrategy.get));
-////        organDrugBusinessService.addOrganDrugSalesStrategy(organDrugList);
-////    }
-//
-//}
+//import com.ngari.recipe.entity.DrugSaleStrategy;
+//import com.ngari.recipe.entity.OrganDrugList;
+//import ctd.util.annotation.RpcBean;
+//import ctd.util.annotation.RpcService;
+//import recipe.atop.BaseAtop;
+////
+////@RpcBean(value = "drugSaleStrategyGmAtop")
+////public class DrugSaleStrategyGmAtop extends BaseAtop {
+//////    /**
+//////     * 添加药品销售策略
+//////     * @param drugSaleStrategy 销售策略
+//////     */
+//////    @RpcService
+//////    public void addDrugSaleStrategy(DrugSaleStrategy drugSaleStrategy) {
+//////        validateAtop(drugSaleStrategy.getStrategyTitle(),drugSaleStrategy.get));
+//////        organDrugBusinessService.addOrganDrugSalesStrategy(organDrugList);
+//////    }
+////
+////}
