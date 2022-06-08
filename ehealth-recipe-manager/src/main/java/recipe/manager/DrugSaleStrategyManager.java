2cdad3b234b85e2916d289d4364538af63ee8813
yinsheng <yins@ngarihealth.com>
2022-06-08 13:44:43 +0800

    BUG#100394 【电子处方】不同的药企点击出售策略展示的出售策略不一致

--- ehealth-recipe-manager/src/main/java/recipe/manager/DrugSaleStrategyManager.java
+++ ehealth-recipe-manager/src/main/java/recipe/manager/DrugSaleStrategyManager.java
@@ -1,18 +1,10 @@
 package recipe.manager;
 
-import com.ngari.recipe.entity.DrugSaleStrategy;
-import com.ngari.recipe.entity.DrugsEnterprise;
-import com.ngari.recipe.entity.OrganDrugList;
-import com.ngari.recipe.entity.SaleDrugList;
-import org.apache.commons.collections.CollectionUtils;
+import com.ngari.recipe.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import recipe.dao.DrugSaleStrategyDAO;
-import recipe.dao.DrugsEnterpriseDAO;
-import recipe.dao.OrganDrugListDAO;
-import recipe.dao.SaleDrugListDAO;
+import recipe.dao.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -26,11 +18,7 @@ public class DrugSaleStrategyManager extends BaseManager {
     @Autowired
     private DrugSaleStrategyDAO drugSaleStrategyDAO;
     @Autowired
-    private DrugsEnterpriseDAO drugsEnterpriseDAO;
-    @Autowired
-    private SaleDrugListDAO saleDrugListDAO;
-    @Autowired
-    private OrganDrugListDAO organDrugListDAO;
+    private DrugListDAO drugListDAO;
 
     public DrugSaleStrategy getDrugSaleStrategyById(Integer id) {
         return drugSaleStrategyDAO.getDrugSaleStrategyById(id);
@@ -41,19 +29,17 @@ public class DrugSaleStrategyManager extends BaseManager {
     }
 
     public DrugSaleStrategy getDefaultDrugSaleStrategy(Integer depId, Integer drugId){
-        DrugsEnterprise drugsEnterprise = drugsEnterpriseDAO.getById(depId);
-        if (null == drugsEnterprise || null == drugsEnterprise.getOrganId()) {
-            return null;
+        if (null == drugId) {
+            return  null;
         }
-        List<OrganDrugList> organDrugListList = organDrugListDAO.findByDrugIdAndOrganId(drugId, drugsEnterprise.getOrganId());
-        if (CollectionUtils.isEmpty(organDrugListList)) {
-            return null;
+        DrugList drugList = drugListDAO.getById(drugId);
+        if (null == drugList) {
+            return  null;
         }
-        OrganDrugList organDrugList = organDrugListList.get(0);
         DrugSaleStrategy drugSaleStrategy = new DrugSaleStrategy();
         drugSaleStrategy.setDrugId(drugId);
         drugSaleStrategy.setDrugAmount(1);
-        drugSaleStrategy.setDrugUnit(organDrugList.getUnit());
+        drugSaleStrategy.setDrugUnit(drugList.getUnit());
         drugSaleStrategy.setStrategyTitle("默认出售策略");
         drugSaleStrategy.setStatus(1);
         drugSaleStrategy.setId(0);
