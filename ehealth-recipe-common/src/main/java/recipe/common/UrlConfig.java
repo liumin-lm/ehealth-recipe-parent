39da3627de252cf2ce33f35ee64c2f401f7e6226
liumin <liu_m@winning.com.cn>
2022-06-16 10:33:09 +0800

    TASK #171702 【后台】100399拆分-【实施】【常州市金坛第一人民医院】【B类】【需求】对接江苏监管平台华正CA签名

---
+++ ehealth-recipe-common/src/main/java/recipe/common/UrlConfig.java
@@ -0,0 +1,16 @@
+package recipe.common;
+
+/**
+ * liumin
+ * 20220616
+ * description:文件阿波罗配置
+ */
+public class UrlConfig {
+
+    public static String fileViewUrl;
+
+    public  void setFileViewUrl(String fileViewUrl) {
+        UrlConfig.fileViewUrl = fileViewUrl;
+    }
+
+}
