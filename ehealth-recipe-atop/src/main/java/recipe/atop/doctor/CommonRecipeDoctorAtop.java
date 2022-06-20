efb86d4402692320e5853533775da7ac551e62c8
yinsheng <yins@ngarihealth.com>
2022-06-20 23:17:50 +0800

    fix#协定方取值错误

--- ehealth-recipe-atop/src/main/java/recipe/atop/doctor/CommonRecipeDoctorAtop.java
+++ ehealth-recipe-atop/src/main/java/recipe/atop/doctor/CommonRecipeDoctorAtop.java
@@ -216,7 +216,7 @@ public class CommonRecipeDoctorAtop extends BaseAtop {
             detailBean.setDrugUnit(a.getDrugUnit());
             detailBean.setUsingRateText(a.getUsingRate());
             detailBean.setUsePathwaysText(a.getUsePathWays());
-            detailBean.setUseDays(a.getDays());
+            detailBean.setUseDays(a.getUseDays()+"");
             detailBean.setUseTotalDose(null != a.getUseTotalDose()?a.getUseTotalDose().doubleValue():0.0);
             if (StringUtils.isNotEmpty(commonRecipe.getPharmacyCode()) && StringUtils.isEmpty(a.getPharmacyCode())) {
                 detailBean.setPharmacyCode(commonRecipe.getPharmacyCode());
