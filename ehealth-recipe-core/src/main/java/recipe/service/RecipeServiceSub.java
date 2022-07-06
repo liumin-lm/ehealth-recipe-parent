380f4f7d55b8995d10e3f492ad02e62ed04084b9
yinsheng <yins@ngarihealth.com>
2022-07-06 14:58:55 +0800

    BUG#103532 【处方】开具中药处方，患者端打开报错

--- ehealth-recipe-core/src/main/java/recipe/service/RecipeServiceSub.java
+++ ehealth-recipe-core/src/main/java/recipe/service/RecipeServiceSub.java
@@ -1885,7 +1885,7 @@ public class RecipeServiceSub {
         //医生端/患者端获取处方扩展信息
         if (recipeExtend != null) {
             //医生选择煎法后的每贴待煎费
-            if (recipeExtend.getDecoctionId() != null) {
+            if (StringUtils.isNotEmpty(recipeExtend.getDecoctionId())) {
                 DrugDecoctionWayDao DecoctionWayDao = DAOFactory.getDAO(DrugDecoctionWayDao.class);
                 DecoctionWay decoctionWay = DecoctionWayDao.get(Integer.valueOf(recipeExtend.getDecoctionId()));
                 if (decoctionWay != null && decoctionWay.getDecoctionPrice() != null) {
