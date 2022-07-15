2f96a5bbac1de549898155c90a9a49d1822a74ef
xy <s.xy@ngarihealth.com>
2022-07-15 09:50:32 +0800

    task # 176217 【后台】【实施】【长春中医药大学附属医院】【A】【需求】医生开处方时当药品库存不足会提示具体哪个药品没有库存

--- ehealth-recipe-core/src/main/java/recipe/service/common/RecipeSignService.java
+++ ehealth-recipe-core/src/main/java/recipe/service/common/RecipeSignService.java
@@ -397,7 +397,8 @@ public class RecipeSignService {
             }
             //第三步校验库存
             Integer appointEnterpriseType = recipeBean.getRecipeExtend().getAppointEnterpriseType();
-            if ((continueFlag == 0 || continueFlag == 4) && ValidateUtil.integerIsEmpty(appointEnterpriseType)) {
+            if ((continueFlag == 0 || continueFlag == 4) && ValidateUtil.integerIsEmpty(appointEnterpriseType)
+                    && ValidateUtil.integerIsEmpty(recipeBean.getVersion())) {
                 rMap = drugEnterpriseBusinessService.enterpriseStockMap(recipeBean.getRecipeId());
                 boolean signResult = Boolean.parseBoolean(rMap.get("signResult").toString());
                 if (!signResult) {
