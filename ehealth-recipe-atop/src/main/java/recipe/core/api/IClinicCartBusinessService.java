2ab1a9121dc1417daf9bdc8a416680bf2b7ab3c3
yzl <yzl@ngarihealth.com>
2022-07-26 17:08:18 +0800

    other# 添加接口注释

--- ehealth-recipe-atop/src/main/java/recipe/core/api/IClinicCartBusinessService.java
+++ ehealth-recipe-atop/src/main/java/recipe/core/api/IClinicCartBusinessService.java
@@ -6,11 +6,36 @@ import java.util.List;
 
 public interface IClinicCartBusinessService {
 
+    /**
+     * 方便门诊购物车列表查询
+     *
+     * @param organId
+     * @param userId
+     * @return
+     */
     List<ClinicCartVO> findClinicCartsByOrganIdAndUserId(Integer organId, String userId);
 
+    /**
+     * 方便门诊购物车新增
+     *
+     * @param clinicCartVO
+     * @return
+     */
     Integer addClinicCart(ClinicCartVO clinicCartVO);
 
+    /**
+     * 方便门诊购物车删除
+     *
+     * @param id
+     * @return
+     */
     Boolean deleteClinicCartByIds(List<Integer> id);
 
+    /**
+     * 方便门诊购物车 修改某一条记录（目前只需要修改数量）
+     *
+     * @param clinicCartVO
+     * @return
+     */
     Boolean updateClinicCartById(ClinicCartVO clinicCartVO);
 }
