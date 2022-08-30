786e4e0f61613a3bf2f30b63c6a8031759d1a565
xy <s.xy@ngarihealth.com>
2022-08-30 22:49:18 +0800

    test # 异常捕获

--- ehealth-recipe-core/src/main/java/recipe/thread/SyncOrganDrugToEsCallable.java
+++ ehealth-recipe-core/src/main/java/recipe/thread/SyncOrganDrugToEsCallable.java
@@ -53,111 +53,117 @@ public class SyncOrganDrugToEsCallable implements Callable<String> {
     @Override
     public String call() throws Exception {
         LOG.info("SyncOrganDrugToEsCallable start");
-        long start = System.currentTimeMillis();
-        OrganDrugListDAO organDrugListDAO = DAOFactory.getDAO(OrganDrugListDAO.class);
-        int total = this.end - this.start;
-        if (total > 0) {
-            int time = (int) Math.ceil(total / Double.parseDouble(String.valueOf(ONCE_NUM)));
-            List<OrganDrugList> list = null;
-            for (int i = 0; i < time; i++) {
-                int subStart = this.start + i * ONCE_NUM;
-                list = organDrugListDAO.findAllForPage(subStart, ONCE_NUM);
-                if (CollectionUtils.isNotEmpty(list)) {
-                    List<Integer> drugIdList = new ArrayList<>(list.size());
-                    //获取base_druglist信息
-                    for (OrganDrugList organDrug : list) {
-                        drugIdList.add(organDrug.getDrugId());
-                    }
+        try {
 
-                    DrugListDAO drugListDAO = DAOFactory.getDAO(DrugListDAO.class);
-                    List<DrugList> drugList = drugListDAO.findByDrugIdsWithOutStatus(drugIdList);
 
-                    //基础数据为空的话则存在问题
-                    if (CollectionUtils.isEmpty(drugList)) {
-                        LOG.warn("SyncOrganDrugToEsCallable drugList is empty. drugIds={}",
-                                JSONUtils.toString(drugIdList));
-                        drugList = Lists.newArrayList();
-                    }
-
-                    //处理下基础药品数据信息
-                    Map<Integer, DrugList> drugMap = Maps.uniqueIndex(drugList, new Function<DrugList, Integer>() {
-                        @Override
-                        public Integer apply(DrugList input) {
-                            return input.getDrugId();
+            long start = System.currentTimeMillis();
+            OrganDrugListDAO organDrugListDAO = DAOFactory.getDAO(OrganDrugListDAO.class);
+            int total = this.end - this.start;
+            if (total > 0) {
+                int time = (int) Math.ceil(total / Double.parseDouble(String.valueOf(ONCE_NUM)));
+                List<OrganDrugList> list = null;
+                for (int i = 0; i < time; i++) {
+                    int subStart = this.start + i * ONCE_NUM;
+                    list = organDrugListDAO.findAllForPage(subStart, ONCE_NUM);
+                    if (CollectionUtils.isNotEmpty(list)) {
+                        List<Integer> drugIdList = new ArrayList<>(list.size());
+                        //获取base_druglist信息
+                        for (OrganDrugList organDrug : list) {
+                            drugIdList.add(organDrug.getDrugId());
                         }
-                    });
+
+                        DrugListDAO drugListDAO = DAOFactory.getDAO(DrugListDAO.class);
+                        List<DrugList> drugList = drugListDAO.findByDrugIdsWithOutStatus(drugIdList);
+
+                        //基础数据为空的话则存在问题
+                        if (CollectionUtils.isEmpty(drugList)) {
+                            LOG.warn("SyncOrganDrugToEsCallable drugList is empty. drugIds={}",
+                                    JSONUtils.toString(drugIdList));
+                            drugList = Lists.newArrayList();
+                        }
+
+                        //处理下基础药品数据信息
+                        Map<Integer, DrugList> drugMap = Maps.uniqueIndex(drugList, new Function<DrugList, Integer>() {
+                            @Override
+                            public Integer apply(DrugList input) {
+                                return input.getDrugId();
+                            }
+                        });
 
 
-                    DrugSearchService searchService = AppContextHolder.getBean("es.drugSearchService", DrugSearchService.class);
-                    DoctorDrugDetailVO detailVo;
-                    DrugList drug;
-                    List<DoctorDrugDetailVO> updateList = new ArrayList<>(list.size());
-                    String searchKey = "";
-                    for (OrganDrugList organDrug : list) {
-                        detailVo = new DoctorDrugDetailVO();
-                        BeanUtils.copyProperties(organDrug, detailVo);
-                        detailVo.setDeleteFlag(0);
-                        drug = drugMap.get(organDrug.getDrugId());
-                        //处理药品基础库相关信息
-                        if (null == drug) {
-                            detailVo.setStatus(0);
-                        } else {
-                            //药品用法用量默认使用机构的，无机构数据则使用平台的，两者都无数据则为空
-                            if (StringUtils.isEmpty(organDrug.getUsePathways())){
-                                detailVo.setUsePathways(drug.getUsePathways());
-                            }
-                            if (StringUtils.isEmpty(organDrug.getUsingRate())){
-                                detailVo.setUsingRate(drug.getUsingRate());
-                            }
-                            if (StringUtils.isEmpty(organDrug.getUsePathwaysId())){
-                                detailVo.setUsePathwaysId(drug.getUsePathwaysId());
-                            }
-                            if (StringUtils.isEmpty(organDrug.getUsingRateId())){
-                                detailVo.setUsingRateId(drug.getUsingRateId());
-                            }
-                            //重置searchKey
-                            //机构药品名+平台商品名+机构商品名+院内别名
-                            searchKey = organDrug.getDrugName() + ";" + drug.getSaleName() + ";" + organDrug.getSaleName() + ";" +
-                                    LocalStringUtil.toString(organDrug.getRetrievalCode()) + ";" +   LocalStringUtil.toString(organDrug.getChemicalName());
-                            detailVo.setSearchKey(searchKey.replaceAll(" ", ";"));
-                            detailVo.setPlatformSaleName(drug.getSaleName());
-                            detailVo.setDrugType(drug.getDrugType());
-                            //设置药房id列表
-                            if (org.apache.commons.lang3.StringUtils.isNotEmpty(organDrug.getPharmacy())) {
-                                try {
-                                    List<String> splitToList = Splitter.on(",").splitToList(organDrug.getPharmacy());
-                                    List<Integer> pharmacyIds = splitToList.stream().map(Integer::valueOf).collect(Collectors.toList());
-                                    detailVo.setPharmacyIds(pharmacyIds);
-                                } catch (Exception e) {
-                                    LOG.error("pharmacyId transform exception! updateList={}", JSONUtils.toString(organDrug), e);
-                                }
-                            }
-                            //status字段修改注意：先判断基础药品库，再处理机构药品库
-                            if (0 == drug.getStatus()) {
+                        DrugSearchService searchService = AppContextHolder.getBean("es.drugSearchService", DrugSearchService.class);
+                        DoctorDrugDetailVO detailVo;
+                        DrugList drug;
+                        List<DoctorDrugDetailVO> updateList = new ArrayList<>(list.size());
+                        String searchKey = "";
+                        for (OrganDrugList organDrug : list) {
+                            detailVo = new DoctorDrugDetailVO();
+                            BeanUtils.copyProperties(organDrug, detailVo);
+                            detailVo.setDeleteFlag(0);
+                            drug = drugMap.get(organDrug.getDrugId());
+                            //处理药品基础库相关信息
+                            if (null == drug) {
                                 detailVo.setStatus(0);
                             } else {
-                                detailVo.setStatus(organDrug.getStatus());
+                                //药品用法用量默认使用机构的，无机构数据则使用平台的，两者都无数据则为空
+                                if (StringUtils.isEmpty(organDrug.getUsePathways())) {
+                                    detailVo.setUsePathways(drug.getUsePathways());
+                                }
+                                if (StringUtils.isEmpty(organDrug.getUsingRate())) {
+                                    detailVo.setUsingRate(drug.getUsingRate());
+                                }
+                                if (StringUtils.isEmpty(organDrug.getUsePathwaysId())) {
+                                    detailVo.setUsePathwaysId(drug.getUsePathwaysId());
+                                }
+                                if (StringUtils.isEmpty(organDrug.getUsingRateId())) {
+                                    detailVo.setUsingRateId(drug.getUsingRateId());
+                                }
+                                //重置searchKey
+                                //机构药品名+平台商品名+机构商品名+院内别名
+                                searchKey = organDrug.getDrugName() + ";" + drug.getSaleName() + ";" + organDrug.getSaleName() + ";" +
+                                        LocalStringUtil.toString(organDrug.getRetrievalCode()) + ";" + LocalStringUtil.toString(organDrug.getChemicalName());
+                                detailVo.setSearchKey(searchKey.replaceAll(" ", ";"));
+                                detailVo.setPlatformSaleName(drug.getSaleName());
+                                detailVo.setDrugType(drug.getDrugType());
+                                //设置药房id列表
+                                if (org.apache.commons.lang3.StringUtils.isNotEmpty(organDrug.getPharmacy())) {
+                                    try {
+                                        List<String> splitToList = Splitter.on(",").splitToList(organDrug.getPharmacy());
+                                        List<Integer> pharmacyIds = splitToList.stream().map(Integer::valueOf).collect(Collectors.toList());
+                                        detailVo.setPharmacyIds(pharmacyIds);
+                                    } catch (Exception e) {
+                                        LOG.error("pharmacyId transform exception! updateList={}", JSONUtils.toString(organDrug), e);
+                                    }
+                                }
+                                //status字段修改注意：先判断基础药品库，再处理机构药品库
+                                if (0 == drug.getStatus()) {
+                                    detailVo.setStatus(0);
+                                } else {
+                                    detailVo.setStatus(organDrug.getStatus());
+                                }
+                            }
+                            updateList.add(detailVo);
+                        }
+
+                        if (CollectionUtils.isNotEmpty(updateList)) {
+                            boolean b = false;
+                            try {
+                                b = searchService.updateDoctorDrugDetail(updateList);
+                            } catch (Exception e) {
+                                LOG.error("SyncOrganDrugToEsCallable update exception! updateList={}", JSONUtils.toString(updateList), e);
+                            }
+                            if (!b) {
+                                LOG.warn("SyncOrganDrugToEsCallable update error! updateList={}", JSONUtils.toString(updateList));
+                            } else {
+                                LOG.info("SyncOrganDrugToEsCallable update success. start={}", subStart);
                             }
                         }
-                        updateList.add(detailVo);
-                    }
 
-                    if (CollectionUtils.isNotEmpty(updateList)) {
-                        boolean b = false;
-                        try {
-                            b = searchService.updateDoctorDrugDetail(updateList);
-                        } catch (Exception e) {
-                            LOG.error("SyncOrganDrugToEsCallable update exception! updateList={}", JSONUtils.toString(updateList), e);
-                        }
-                        if (!b) {
-                            LOG.warn("SyncOrganDrugToEsCallable update error! updateList={}", JSONUtils.toString(updateList));
-                        } else {
-                            LOG.info("SyncOrganDrugToEsCallable update success. start={}", subStart);
-                        }
                     }
-
                 }
             }
+        } catch (Exception e) {
+            LOG.info("SyncOrganDrugToEsCallable error ", e);
         }
         long elapsedTime = System.currentTimeMillis() - start;
         LOG.info("RecipeBusiThreadPool SyncOrganDrugToEsCallable ES-同步机构药品 执行时间:{}.", elapsedTime);
