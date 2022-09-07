743a931c96d4fe9aa61c00f6d9d71e123e066686
yinsheng <yins@ngarihealth.com>
2022-09-07 14:08:16 +0800

    TASK#183633 【后台】【实施/西南】【祥云县人民医院】【B】【需求】我的处方单、出院带药对接合理用药查询药品说明书

--- ehealth-recipe-atop/src/main/java/recipe/atop/patient/RecipePatientAtop.java
+++ ehealth-recipe-atop/src/main/java/recipe/atop/patient/RecipePatientAtop.java
@@ -122,6 +122,18 @@ public class RecipePatientAtop extends BaseAtop {
         }
     }
 
+    @RpcService
+    public List<DiseaseInfoDTO> getOutRecipeDiseaseV1(PatientInfoVO patientInfoVO) {
+        logger.info("OutPatientRecipeAtop getOutRecipeDisease patientInfoVO:{}.", JSON.toJSONString(patientInfoVO));
+        validateAtop(patientInfoVO, patientInfoVO.getOrganId(), patientInfoVO.getPatientName(), patientInfoVO.getPatientId(), patientInfoVO.getRegisterID());
+        try {
+            List<DiseaseInfoDTO> result = recipeBusinessService.getOutRecipeDisease(patientInfoVO);
+            return result;
+        } catch (Exception e) {
+            return new ArrayList<>();
+        }
+    }
+
     /**
      * 获取门诊处方详情信息
      *
