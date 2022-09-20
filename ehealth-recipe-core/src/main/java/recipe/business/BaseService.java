2cae0af346792b9c9e5244b6ef95768669af7d2a
xy <s.xy@ngarihealth.com>
2022-09-20 09:55:26 +0800

    task # 185246 【后台】【实施/上海】【上海市第六人民医院】【A】【需求】线上处方支付校验库存提示优化

--- ehealth-recipe-core/src/main/java/recipe/business/BaseService.java
+++ ehealth-recipe-core/src/main/java/recipe/business/BaseService.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import recipe.client.IConfigurationClient;
 import recipe.manager.OrganManager;
 
 import java.util.LinkedList;
@@ -19,6 +20,8 @@ import java.util.concurrent.TimeUnit;
 public class BaseService {
     @Autowired
     protected OrganManager organManager;
+    @Autowired
+    protected IConfigurationClient configurationClient;
 
     protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
