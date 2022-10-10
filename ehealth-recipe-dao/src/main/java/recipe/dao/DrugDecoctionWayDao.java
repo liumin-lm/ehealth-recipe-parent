6a320672033ec49e768b7a0473ef2ef65c2036d3
yinsheng <yins@ngarihealth.com>
2022-10-10 15:13:16 +0800

    TASK#187393 子 【后台】【实施】【浙江省中医院】【B】【需求】互联网医院开具配方颗粒剂处方的时候默认不展示是否代煎-医生端 / 运营平台煎法所属剂型 查询 修改接口

--- ehealth-recipe-dao/src/main/java/recipe/dao/DrugDecoctionWayDao.java
+++ ehealth-recipe-dao/src/main/java/recipe/dao/DrugDecoctionWayDao.java
@@ -102,7 +102,7 @@ public abstract class DrugDecoctionWayDao extends HibernateSupportDelegateDAO<De
     @DAOMethod(sql = "select count(*) from DecoctionWay where organId=:organId")
     public abstract Long getCountOfOrgan(@DAOParam("organId") Integer organId);
 
-    @DAOMethod(sql = "from DecoctionWay where organId =:organId and drugForm=:drugFormType order by sort", limit = 0)
+    @DAOMethod(sql = "from DecoctionWay where organId =:organId and drugForm like :drugForm order by sort", limit = 0)
     public abstract List<DecoctionWay> findAllDecoctionWayByOrganIdAndDrugFormType(@DAOParam("organId") Integer organId,
                                                                                    @DAOParam("drugForm") String drugForm);
 
