7792227e01efdde90aae0204a9f329f718988e9e
liumin <liu_m@winning.com.cn>
2022-11-04 18:39:13 +0800

    TASK #190319 【后台】【处方业务】【实施/上海】【上海市第七人民医院】【B】【BUG】在线复诊患者支付处方费用后未获取配送方式等信息

--- ehealth-recipe-manager/src/main/java/recipe/manager/HisRecipeManager.java
+++ ehealth-recipe-manager/src/main/java/recipe/manager/HisRecipeManager.java
@@ -498,12 +498,15 @@ public class HisRecipeManager extends BaseManager {
     public String obtainPayStatus(String recipeCode, Integer clinicOrgan) {
         String realPayFlag = "";
         Recipe recipe = recipeDAO.getByRecipeCodeAndClinicOrgan(recipeCode, clinicOrgan);
+        logger.info("recipe:{}",JSONUtils.toString(recipe));
         if (recipe != null && StringUtils.isNotEmpty(recipe.getOrderCode())) {
             RecipeOrder recipeOrder = recipeOrderDAO.getByOrderCode(recipe.getOrderCode());
+            logger.info("recipeOrder:{}",JSONUtils.toString(recipeOrder));
             if (recipeOrder != null&&!Objects.isNull(recipeOrder.getWxPayWay())) {
                 realPayFlag = payClient.orderQuery(recipeOrder);
             }
         }
+        logger.info("payflag:{}",JSONUtils.toString(realPayFlag));
         return realPayFlag;
 
     }
