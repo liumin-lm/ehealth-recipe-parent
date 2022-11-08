0784e6a302a3c446571d09cd265bd8ab05c60af4
yzl <yzl@ngarihealth.com>
2022-11-08 10:54:09 +0800

    other#加日志

--- ehealth-recipe-core/src/main/java/recipe/mq/Buss2SessionProducer.java
+++ ehealth-recipe-core/src/main/java/recipe/mq/Buss2SessionProducer.java
@@ -1,5 +1,6 @@
 package recipe.mq;
 
+import com.alibaba.fastjson.JSON;
 import com.ngari.common.dto.Buss2SessionMsg;
 import com.ngari.recipe.entity.Recipe;
 import ctd.net.broadcast.MQHelper;
@@ -26,11 +27,11 @@ public class Buss2SessionProducer {
         msg.setMpiId(recipe.getMpiid());
         msg.setSessionType(4);
         msg.setSessionId(sessionId);
+        LOGGER.info("Buss2SessionProducer sendMsgToMq send to MQ, msg = {}", JSON.toJSONString(msg));
         try {
             MQHelper.getMqPublisher().publish(OnsConfig.sessionTopic, msg, "tag_revisit");
-            LOGGER.info("Buss2SessionProducer sendMsgToMq send to MQ, sessionType:{}, recipeID:{}", msg.getSessionType(), recipe.getRecipeId());
         } catch (Exception e) {
-            LOGGER.error("Buss2SessionProducer sendMsgToMq can't send to MQ, sessionType:{}, recipeID:{}", msg.getSessionType(), recipe.getRecipeId(), e);
+            LOGGER.error("Buss2SessionProducer sendMsgToMq error, contentType:{}, recipeID:{}", msg.getContentType(), recipe.getRecipeId(), e);
         }
     }
 }
