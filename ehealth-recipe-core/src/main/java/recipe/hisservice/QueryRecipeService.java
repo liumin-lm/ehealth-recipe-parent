956a11a30ba6ad89b1b98d2703971ff8a0f844b2
xy <s.xy@ngarihealth.com>
2022-12-06 21:01:05 +0800

    test # 测试代码

--- ehealth-recipe-core/src/main/java/recipe/hisservice/QueryRecipeService.java
+++ ehealth-recipe-core/src/main/java/recipe/hisservice/QueryRecipeService.java
@@ -56,7 +56,6 @@ import recipe.bussutil.UsePathwaysFilter;
 import recipe.bussutil.UsingRateFilter;
 import recipe.caNew.pdf.CreatePdfFactory;
 import recipe.client.DocIndexClient;
-import recipe.constant.SyncDrugConstant;
 import recipe.dao.*;
 import recipe.enumerate.type.RecipeDrugFormTypeEnum;
 import recipe.hisservice.syncdata.HisSyncSupervisionService;
@@ -323,7 +322,7 @@ public class QueryRecipeService implements IQueryRecipeService {
                 if (consultExDTO != null) {
                     recipeDTO.setRegisterId(consultExDTO.getRegisterNo());
                     recipeDTO.setTreatmentId(consultExDTO.getTreatmentId());
-                    recipeDTO.setSerialNumberOfReception(consultExDTO.getSerialNumberOfReception());
+                    //recipeDTO.setSerialNumberOfReception(consultExDTO.getSerialNumberOfReception());
                 }
             }
             recipeDTO.setCopyNum(recipe.getCopyNum());
