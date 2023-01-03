7323adf88decc87688d3a6625cc1bb7cd0bbc09f
xy <s.xy@ngarihealth.com>
2023-01-03 10:06:19 +0800

    task # 197760 【后台】【内部优化】处方详情单页面新增续方按钮

--- ehealth-recipe-common/src/main/java/recipe/enumerate/status/RecipeStateEnum.java
+++ ehealth-recipe-common/src/main/java/recipe/enumerate/status/RecipeStateEnum.java
@@ -121,6 +121,11 @@ public enum RecipeStateEnum {
      */
     public static final List<Integer> RECIPE_REPEAT = Arrays.asList(PROCESS_STATE_AUDIT.type, PROCESS_STATE_ORDER.type, PROCESS_STATE_DISPENSING.type, PROCESS_STATE_DISTRIBUTION.type, PROCESS_STATE_MEDICINE.type, PROCESS_STATE_DONE.type);
 
+    /**
+     * 需方按钮
+     */
+    public static final List<Integer> continueRecipeFlag = Arrays.asList(PROCESS_STATE_AUDIT.type, PROCESS_STATE_ORDER.type, PROCESS_STATE_DISPENSING.type, PROCESS_STATE_DISTRIBUTION.type, PROCESS_STATE_MEDICINE.type, PROCESS_STATE_DONE.type, PROCESS_STATE_CANCELLATION.type);
+
 
     /**
      * 根据类型 获取枚举类型
