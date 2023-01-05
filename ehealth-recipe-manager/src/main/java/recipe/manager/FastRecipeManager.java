7ae95380f41e575e048cdd5cccd68d339b2f6d3c
yzl <yzl@ngarihealth.com>
2023-01-05 11:46:24 +0800

    task#197781 【后台】【便捷购药】平台库存数量优化

--- ehealth-recipe-manager/src/main/java/recipe/manager/FastRecipeManager.java
+++ ehealth-recipe-manager/src/main/java/recipe/manager/FastRecipeManager.java
@@ -102,4 +102,49 @@ public class FastRecipeManager extends BaseManager {
         }
     }
 
+    public void decreaseSaleNum(Integer recipeId) {
+        logger.info("decreaseSaleNum param: recipeId={}", recipeId);
+        try {
+            Recipe recipe = recipeDAO.getByRecipeId(recipeId);
+            RecipeExtend recipeExtend = recipeExtendDAO.getByRecipeId(recipeId);
+            if (Objects.isNull(recipe) || Objects.isNull(recipeExtend) ||
+                    !FastRecipeFlagEnum.FAST_RECIPE_FLAG_QUICK.getType().equals(recipe.getFastRecipeFlag())) {
+                return;
+            }
+            FastRecipe fastRecipe = fastRecipeDAO.get(recipeExtend.getMouldId());
+            if (Objects.isNull(fastRecipe)) {
+                return;
+            }
+            if (Objects.nonNull(fastRecipe.getSaleNum())) {
+                fastRecipeDAO.addSaleNumByMouldId(fastRecipe.getId(), - recipeExtend.getFastRecipeNum());
+            } else {
+                logger.info("decreaseSaleNum error! StockNum is null!");
+            }
+        } catch (Exception e) {
+            logger.error("decreaseSaleNum error! ", e);
+        }
+    }
+
+    public void addStockByRecipeId(Integer recipeId) {
+        Recipe recipe = recipeDAO.getByRecipeId(recipeId);
+        RecipeExtend recipeExtend = recipeExtendDAO.getByRecipeId(recipeId);
+        if (Objects.isNull(recipe) || Objects.isNull(recipeExtend) ||
+                !FastRecipeFlagEnum.FAST_RECIPE_FLAG_QUICK.getType().equals(recipe.getFastRecipeFlag())) {
+            return;
+        }
+        Boolean fastRecipeUsePlatStock = configurationClient.getValueBooleanCatch(recipe.getClinicOrgan(), "fastRecipeUsePlatStock", false);
+        if (!fastRecipeUsePlatStock) {
+            return;
+        }
+
+        FastRecipe fastRecipe = fastRecipeDAO.get(recipeExtend.getMouldId());
+        if (Objects.isNull(fastRecipe)) {
+            return;
+        }
+        if (Objects.nonNull(fastRecipe.getStockNum())) {
+            fastRecipeDAO.updateStockByMouldId(recipeExtend.getMouldId(), -recipeExtend.getFastRecipeNum());
+        } else {
+            logger.info("addStock error! StockNum is null!");
+        }
+    }
 }
