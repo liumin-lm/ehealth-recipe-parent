db4f91de21700daf24ab1de28296913fdf208a8c
liumin <liu_m@ngarihealth.com>
2023-01-09 09:42:01 +0800

    TASK #197775 【后台】【实施/中南】【鄂州卫健委】【A】【需求】健康鄂州在线咨询、复诊处方对接健康湖北

--- ehealth-recipe-api/src/main/java/com/ngari/recipe/recipe/model/RecipeInfoBean.java
+++ ehealth-recipe-api/src/main/java/com/ngari/recipe/recipe/model/RecipeInfoBean.java
@@ -2,6 +2,7 @@ package com.ngari.recipe.recipe.model;
 
 
 import com.ngari.recipe.recipeorder.model.RecipeOrderBean;
+import ctd.schema.annotation.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +14,7 @@ import java.util.List;
  */
 @Setter
 @Getter
+@Schema
 public class RecipeInfoBean implements Serializable {
     private static final long serialVersionUID = -9005230183318990440L;
     private RecipeBean recipe;
