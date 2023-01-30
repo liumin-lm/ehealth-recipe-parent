81e933ec97bf253f717d9d6e378180328878829a
xy <s.xy@ngarihealth.com>
2023-01-30 14:49:22 +0800

    task# 200027 【后台】【实施/上海】【上海市普陀区利群医院】【A】【需求】科室、医生对应药房

---
+++ ehealth-recipe-entity/src/main/java/com/ngari/recipe/dto/BasicsDTO.java
@@ -0,0 +1,15 @@
+package com.ngari.recipe.dto;
+
+import lombok.Data;
+
+/**
+ * 基础字段公用实体类
+ * @author fuzi
+ */
+@Data
+public class BasicsDTO {
+
+    private Integer id;
+
+    private String name;
+}
