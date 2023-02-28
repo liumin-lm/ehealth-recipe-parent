2bce7f4fb53dc2d3d024f82ce46696fe8d1df53b
yinsheng <yins@ngarihealth.com>
2023-02-28 15:01:45 +0800

    BUG#121485 【灰度-电子处方】退单处理退费成功后未收到退费成功消息提醒

--- ehealth-recipe-core/src/main/java/recipe/business/RecipeOrderBusinessService.java
+++ ehealth-recipe-core/src/main/java/recipe/business/RecipeOrderBusinessService.java
@@ -191,6 +191,8 @@ public class RecipeOrderBusinessService extends BaseService implements IRecipeOr
     private RecipeHisService recipeHisService;
     @Autowired
     private RecipeOrderPayFlowManager recipeOrderPayFlowManager;
+    @Autowired
+    private RecipeRefundDAO recipeRefundDAO;
 
 
     @Override
@@ -2091,6 +2093,14 @@ public class RecipeOrderBusinessService extends BaseService implements IRecipeOr
         if (Objects.isNull(refundResultDTO) || refundResultDTO.getStatus() != 0) {
             return false;
         }
+        RecipeRefund recipeRefund = new RecipeRefund();
+        recipeRefund.setTradeNo(recipeOrder.getTradeNo());
+        recipeRefund.setPrice(recipeOrder.getActualPrice());
+        recipeRefund.setNode(RecipeRefundRoleConstant.RECIPE_REFUND_ROLE_PATIENT);
+        recipeRefund.setStatus(0);
+        recipeRefund.setApplyTime(new Date());
+        recipeRefund.setCheckTime(new Date());
+        recipeRefundDAO.saveRefund(recipeRefund);
         RecipeMsgService.batchSendMsg(recipeIdList.get(0), RecipeStatusConstant.RECIPE_REFUND_SUCC);
         return true;
     }
