3ab96a05000367753435e1d461c86824d93214bf
whf <whf@winning.com.cn>
2023-03-07 17:11:50 +0800

    task#  204080 【后台】【内部优化】患者端我的处方入口列表优化

--- ehealth-recipe-api/src/main/java/recipe/vo/patient/PatientRecipeListResVo.java
+++ ehealth-recipe-api/src/main/java/recipe/vo/patient/PatientRecipeListResVo.java
@@ -39,12 +39,12 @@ public class PatientRecipeListResVo implements Serializable {
     private Integer recipeType;
 
     @ItemProperty(alias = "开方科室")
-    @Dictionary(id = "eh.base.dictionary.Depart")
     private Integer depart;
+    private String departText;
 
     @ItemProperty(alias = "开方医生（医生Id）")
-    @Dictionary(id = "eh.base.dictionary.Doctor")
     private Integer doctor;
+    private String doctorText;
 
     @ItemProperty(alias = "机构疾病名称")
     private String organDiseaseName;
@@ -68,6 +68,8 @@ public class PatientRecipeListResVo implements Serializable {
     @ItemProperty(alias = "线下处方/常用方/协定方名称")
     private String offlineRecipeName;
 
+    @ItemProperty(alias = "支付标志")
+    private Integer payFlag;
 
     /******************************** 以下数据来源 recipeExt ****************************/
     @ItemProperty(alias = "大病类型")
