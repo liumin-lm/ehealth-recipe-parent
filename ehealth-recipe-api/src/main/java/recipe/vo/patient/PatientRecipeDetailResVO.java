b4b61744c2f7bae882fe0a5fce2a0001f5cd4dbb
whf <whf@winning.com.cn>
2023-03-15 11:33:31 +0800

    task#  204870 【后台】【处方业务】【提前开发】【内部优化】患者端处方详情优化

---
+++ ehealth-recipe-api/src/main/java/recipe/vo/patient/PatientRecipeDetailResVO.java
@@ -0,0 +1,17 @@
+package recipe.vo.patient;
+
+import ctd.schema.annotation.Schema;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @description： 患者端 处方详情出参
+ * @author： whf
+ * @date： 2023-03-15 11:32
+ */
+@Data
+@Schema
+public class PatientRecipeDetailResVO implements Serializable {
+    private static final long serialVersionUID = 6997161672673473142L;
+}
