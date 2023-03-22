dc4c5be00e71b592c38910fcab296f317a568f39
liumin <liu_m@ngarihealth.com>
2023-03-22 16:10:19 +0800

    TASK #205689 【后台】【内部优化】患者端在线处方详情优化

--- ehealth-recipe-api/src/main/java/com/ngari/recipe/offlinetoonline/model/BatchOfflineToOnlineReqVO.java
+++ ehealth-recipe-api/src/main/java/com/ngari/recipe/offlinetoonline/model/BatchOfflineToOnlineReqVO.java
@@ -3,7 +3,6 @@ package com.ngari.recipe.offlinetoonline.model;
 
 import ctd.schema.annotation.Desensitizations;
 import ctd.schema.annotation.DesensitizationsType;
-import ctd.schema.annotation.ItemProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -19,7 +18,7 @@ import java.util.List;
 public class BatchOfflineToOnlineReqVO implements Serializable {
     private static final long serialVersionUID = -7727248592234567484L;
 
-    List<SubBatchOfflineToOnlineReqVO> subParams;
+    private List<SubBatchOfflineToOnlineReqVO> subParams;
 
     /**
      * 机构
@@ -50,21 +49,38 @@ public class BatchOfflineToOnlineReqVO implements Serializable {
         /**
          * 处方号
          */
-        private String recipeCode;
+        public String recipeCode;
 
-
-
-        @ItemProperty(alias = "开始时间")
+        /**
+         * 开始时间
+         */
         private Date startTime;
 
-        @ItemProperty(alias = "结束时间")
+        /**
+         * 结束时间
+         */
         private Date endTime;
 
+        public SubBatchOfflineToOnlineReqVO() {
+        }
+
+        public SubBatchOfflineToOnlineReqVO(String recipeCode, Date startTime, Date endTime) {
+            this.recipeCode = recipeCode;
+            this.startTime = startTime;
+            this.endTime = endTime;
+        }
     }
 
+    public BatchOfflineToOnlineReqVO() {
+    }
 
-
-
+    public BatchOfflineToOnlineReqVO(List<SubBatchOfflineToOnlineReqVO> subParams, Integer organId, String mpiId, String cardId, String processState) {
+        this.subParams = subParams;
+        this.organId = organId;
+        this.mpiId = mpiId;
+        this.cardId = cardId;
+        this.processState = processState;
+    }
 }
 
 
