276b7d454aeb298710664b1df5f9ef91b33058d0
whf <whf@winning.com.cn>
2023-03-23 16:29:56 +0800

    task#  205685 【后台】【内部优化】患者端在线处方详情优化2

--- ehealth-recipe-common/src/main/java/recipe/enumerate/status/RecipeStateEnum.java
+++ ehealth-recipe-common/src/main/java/recipe/enumerate/status/RecipeStateEnum.java
@@ -41,7 +41,7 @@ public enum RecipeStateEnum {
     /**
      * 处方子状态:待购药
      */
-    SUB_ORDER_READY_SUBMIT_ORDER(31, "等待患者前往下单支付", ""),
+    SUB_ORDER_READY_SUBMIT_ORDER(31, "等待下单支付", ""),
     SUB_ORDER_HAD_SUBMIT_ORDER(32, "已提交订单", ""),
     SUB_ORDER_CANCEL_ORDER(33, "取消订单", ""),
 
